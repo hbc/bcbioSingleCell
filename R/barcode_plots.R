47ad7ca9c99753db10965bdc1377b28ea4585b22
Michael Steinbaugh <mike@steinbaugh.com>
2017-03-21 15:18:39 -0400

    Simple wrapper function to loop barcode plots for a bcbio scRNA-seq project

---
+++ R/barcode_plots.R
@@ -0,0 +1,23 @@
+#' Plot all scRNA-Seq sample barcodes
+#'
+#' @author Michael Steinbaugh
+#' @keywords bcbio plot qc
+#'
+#' @importFrom basejump bcbioProject
+
+#' @param `bcbio-nextgen` project list
+#'
+#' @export
+barcode_plots <- function(project = NULL) {
+    if (is.null(project)) {
+        project <- basejump::bcbioProject("indrop_rnaseq")
+    }
+    files <- list.files(project$finalDir,
+                        pattern = "*-barcodes.tsv",
+                        recursive = TRUE,
+                        include.dirs = TRUE,
+                        full.names = TRUE)
+    lapply(seq_along(files), function(a) {
+        barcode_plot(files[a])
+    }) %>% invisible
+}
