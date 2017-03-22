a73f062b6d1933c05709ab5481be3d8f520aa67f
Michael Steinbaugh <mjsteinbaugh@users.noreply.github.com>
2017-03-22 07:18:49 -0400

    Update pipe.R

--- R/pipe.R
+++ R/pipe.R
@@ -1,12 +1,15 @@
 #' @importFrom utils globalVariables
+#' @keywords internal
 utils::globalVariables(".")
+
+
 
 #' Pipe operator
 #'
 #' @name %>%
 #' @rdname pipe
 #' @keywords internal
+#' @export
 #' @importFrom magrittr %>%
 #' @usage lhs \%>\% rhs
-#' @export
 NULL
