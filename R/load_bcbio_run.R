8d6134c2eb4793a41f61fc356665bae52b0539d3
Michael Steinbaugh <mike@steinbaugh.com>
2017-03-23 17:10:12 -0400

    Split out during early development

--- R/load_bcbio_run.R
+++ R/load_bcbio_run.R
@@ -6,14 +6,3 @@
 #' @export
 #' @importFrom bcbioRnaseq load_bcbio_run
 NULL
-
-
-
-#' Set rownames
-#'
-#' @name set_rownames
-#' @rdname set_rownames
-#' @keywords internal
-#' @export
-#' @importFrom bcbioRnaseq set_rownames
-NULL
---
+++ R/set_rownames.R
@@ -0,0 +1,8 @@
+#' Set rownames
+#'
+#' @name set_rownames
+#' @rdname set_rownames
+#' @keywords internal
+#' @export
+#' @importFrom bcbioRnaseq set_rownames
+NULL
