31501ff18ac7b6cbb1a35d8606c9bce712d089f3
Michael Steinbaugh <mike@steinbaugh.com>
2017-03-23 17:05:51 -0400

    Exports from bcbioRnaseq

---
+++ R/imports.R
@@ -0,0 +1,19 @@
+#' Load bcbio run
+#'
+#' @name load_bcbio_run
+#' @rdname load_bcbio_run
+#' @keywords internal
+#' @export
+#' @importFrom bcbioRnaseq load_bcbio_run
+NULL
+
+
+
+#' Set rownames
+#'
+#' @name set_rownames
+#' @rdname set_rownames
+#' @keywords internal
+#' @export
+#' @importFrom bcbioRnaseq set_rownames
+NULL
