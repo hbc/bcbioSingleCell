12078f941a441933bd84de3a24b57302fd3fc735
Michael Steinbaugh <mike@steinbaugh.com>
2017-03-25 08:51:53 -0400

    Fix namespace

---
+++ R/imports.R
@@ -0,0 +1,13 @@
+#' @importFrom magrittr set_names
+#' @keywords internal
+set_names <- function(...) {
+    magrittr::set_names(...)
+}
+
+
+
+#' importFrom basejump setRownames
+#' @keywords internal
+set_rownames <- function(...) {
+    basejump::setRownames(...)
+}
--- R/set_rownames.R
+++
@@ -1,8 +0,0 @@
-#' Set rownames
-#'
-#' @name set_rownames
-#' @rdname set_rownames
-#' @keywords internal
-#' @importFrom bcbioRnaseq set_rownames
-#' @export
-NULL
