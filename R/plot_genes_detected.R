567a34803c0b11efca7226e8b9f7ea547782205e
roryk <rory.kirchner@gmail.com>
2017-04-10 00:16:22 -0400

    Break genes detected into two plots and return plot objects.

--- R/plot_genes_detected.R
+++ R/plot_genes_detected.R
@@ -1,4 +1,4 @@
-#' Genes detected plot
+#' Genes detected boxplot
 #'
 #' @author Rory Kirchner
 #' @author Michael Steinbaugh
@@ -8,23 +8,9 @@
 #' @importFrom stats aggregate median
 #'
 #' @param metrics Barcode metrics data frame
-#'
+#' @return ggplot2 object
 #' @export
-plot_genes_detected <- function(metrics) {
-    histogram <- metrics %>%
-        ggplot2::ggplot(
-            ggplot2::aes_(x = ~genes_detected,
-                          fill = ~sample)
-        ) +
-        ggplot2::facet_wrap(~sample) +
-        ggplot2::geom_histogram() +
-        ggplot2::ggtitle("genes detected histogram") +
-        ggplot2::theme(
-            axis.text.x = ggplot2::element_text(angle = 90,
-                                                hjust = 1),
-            legend.position = "none") +
-        ggplot2::xlab("genes per cell")
-
+plot_genes_detected_boxplot <- function(metrics) {
     boxplot <- metrics %>%
         ggplot2::ggplot(
             ggplot2::aes_(x = ~sample,
@@ -41,7 +27,32 @@ plot_genes_detected <- function(metrics) {
         ggtitle("genes detected boxplot") +
         theme(legend.position = "none") +
         ylab("genes per cell")
+    return(boxplot)
+}
 
-    print(histogram)
-    print(boxplot)
+#' Genes detected histogram
+#'
+#' @author Rory Kirchner
+#' @author Michael Steinbaugh
+#'
+#' @import dplyr
+#' @import ggplot2
+#' @importFrom stats aggregate median
+#' @param metrics
+#' @return ggplot2 object
+plot_genes_detected_histogram <- function(metrics) {
+    histogram <- metrics %>%
+        ggplot2::ggplot(
+            ggplot2::aes_(x = ~genes_detected,
+                          fill = ~sample)
+        ) +
+        ggplot2::facet_wrap(~sample) +
+        ggplot2::geom_histogram() +
+        ggplot2::ggtitle("genes detected histogram") +
+        ggplot2::theme(
+            axis.text.x = ggplot2::element_text(angle = 90,
+                                                hjust = 1),
+            legend.position = "none") +
+        ggplot2::xlab("genes per cell")
+    return(histogram)
 }
