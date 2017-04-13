55645e9a918c9dbd5ef2110312a6c400af80d2d0
Michael Steinbaugh <mike@steinbaugh.com>
2017-04-13 12:48:10 -0400

    Initial commit

---
+++ R/deprecated.R
@@ -0,0 +1,11 @@
+create_local_project <- function() {
+    .Deprecated("create_new_project")
+}
+
+load_csv_with_rownames <- function() {
+    .Deprecated("read_csv_with_rownames")
+}
+
+load_sparsecounts <- function() {
+    .Deprecated("read_sparsecounts_gz")
+}
