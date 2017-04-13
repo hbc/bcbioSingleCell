63561b571a1560e1f19faec7c1b91a1a3103d275
Michael Steinbaugh <mike@steinbaugh.com>
2017-04-13 12:51:18 -0400

    Initial commit, works with inDrop v3

---
+++ R/read_sample_barcodes_metadata.R
@@ -0,0 +1,27 @@
+#' Read sample barcodes metadata
+#'
+#' @author Michael Steinbaugh
+#'
+#' @param file Sample barcodes metadata file
+#'
+#' @return Data frame
+#' @export
+read_sample_barcodes_metadata <- function(
+    file = file.path("meta", "sample_barcodes.xlsx")) {
+    if (file.exists(file)) {
+        if (grepl(".\\xlsx", file)) {
+            df <- read_excel(file)
+        } else {
+            df <- read_csv(file)
+        }
+        # Reverse complement matches
+        df$reverse_complement <- sapply(df$sequence, reverse_complement)
+        # Unique identifier for each barcode
+        df$sample_barcode <- paste(df$samplename,
+                                   df$reverse_complement,
+                                   sep = "-")
+        return(df)
+    } else {
+        return(NULL)
+    }
+}
