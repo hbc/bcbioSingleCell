93337a90c751b9585dff74f847083f14902b0a98
Michael Steinbaugh <mike@steinbaugh.com>
2017-04-13 15:01:37 -0400

    Declare Matrix::colSums to fix rendering issue. Updated to pass in bcbio object run object, to obtain annotations automatically

--- R/barcode_metrics.R
+++ R/barcode_metrics.R
@@ -3,16 +3,22 @@
 #' @author Rory Kirchner
 #' @author Michael Steinbaugh
 #'
-#' @param counts `bcbio-nextgen` scRNA-seq counts sparse matrix
-#' @param annotations Ensembl annotations data frame
-#' @param metadata Sample metadata data frame
+#' @param run \code{bcbio-nextgen} run
+#' @param sparsecounts Sparse counts matrix
+#' @param metadata Sample barcodes metadata
 #'
 #' @return Data frame
 #' @export
 barcode_metrics <- function(
-    counts,
-    annotations,
+    run,
+    sparsecounts,
     metadata = NULL) {
+    check_run(run)
+    check_sparse(sparsecounts)
+
+    # Ensembl annotations with broad class definitions
+    annotations <- ensembl_annotations(run)
+
     coding <- annotations %>%
         filter_(.dots = quote(broad_class == "coding")) %>%
         .$external_gene_name %>% unique %>% sort
@@ -20,13 +26,16 @@ barcode_metrics <- function(
         filter_(.dots = quote(broad_class == "mito")) %>%
         .$external_gene_name %>% unique %>% sort
 
+    # Calculate colSums
     metrics <- data.frame(
-        identifier = colnames(counts),
-        total_counts = colSums(counts),
-        genes_detected = colSums(counts > 0),
-        coding_counts = colSums(counts[rownames(counts) %in% coding, ]),
-        mito_counts = colSums(counts[rownames(counts) %in% mito, ])
-    ) %>%
+        identifier = colnames(sparsecounts),
+        total_counts = Matrix::colSums(sparsecounts),
+        genes_detected = Matrix::colSums(sparsecounts > 0),
+        coding_counts = Matrix::colSums(sparsecounts[rownames(sparsecounts) %in% coding, ]),
+        mito_counts = Matrix::colSums(sparsecounts[rownames(sparsecounts) %in% mito, ]))
+    
+    # dplyr operations
+    metrics <- metrics %>%
         separate_("identifier",
                   c("sample_barcode", "cellular_barcode"),
                   sep = ":",
@@ -35,13 +44,28 @@ barcode_metrics <- function(
             list(quote(log(genes_detected) / log(total_counts)),
                  quote(mito_counts / total_counts)),
             c("log_detected_per_count",
-              "percent_mito")
-        ))
-    if (!is.null(metadata)) {
-        metrics <- metrics %>%
-            left_join(metadata[, c("sample_barcode", "sample")],
-                      by = "sample_barcode")
+              "percent_mito")))
+
+    # Attempt to detect metadata saved manually for consult, if necessary
+    if (is.null(metadata)) {
+        metafile <- list.files("meta",
+                               pattern = "^sample_barcodes",
+                               full.names = TRUE)
+        if (length(metafile) == 1) {
+            metadata <- read_sample_barcodes_metadata(metafile)
+        } else if (length(metafile) > 1) {
+            stop("multiple sample barcode metadata files matched...
+                 need to clean the meta folder")
+        } else {
+            metadata <- NULL
+        }
     }
+
+    # Join metadata, if present
+    if (!is.null(metadata)) {
+        metrics <- left_join(metrics, metadata, by = "sample_barcode")
+    }
+
     metrics <- metrics %>%
         arrange_(.dots = "identifier") %>%
         set_rownames(.$identifier)
