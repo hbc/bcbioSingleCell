522def5940876f684fb26cc52ba750c5376090cd
Michael Steinbaugh <mike@steinbaugh.com>
2017-04-13 12:59:49 -0400

    Consolidate read functions that depend upon bcbio run object

--- R/read_bcbio.R
+++ R/read_bcbio.R
@@ -1,3 +1,43 @@
+#' Read data from bcbio-nextgen run
+#'
+#' @rdname read_bcbio
+#'
+#' @author Michael Steinbaugh
+#' @author Rory Kirchner
+#'
+#' @param run \code{bcbio-nextgen}
+#' @param save Save data
+
+
+
+#' @rdname read_bcbio
+#' @description Import metadata
+#'
+#' @return Metadata data frame
+#' @export
+read_metadata <- function(
+    run,
+    save = FALSE) {
+    check_run(run)
+    metadata <- list.files(run$config_dir,
+                           pattern = ".csv$",
+                           full.names = TRUE) %>%
+        read_csv(col_types = cols()) %>%
+        set_names_snake %>%
+        arrange_(.dots = "description") %>%
+        as.data.frame %>%
+        set_rownames(.$description)
+
+    if (isTRUE(save)) {
+        save(metadata, file = "data/metadata.rda")
+        write_csv(metadata, "meta/metadata.csv")
+    }
+
+    return(metadata)
+}
+
+
+
 #' Import scRNA-Seq count data
 #'
 #' Import transcript-level count data from a bcbio run into a sparse matrix
