3857af55688522e05d0950fbba836caceebdb942
Michael Steinbaugh <mike@steinbaugh.com>
2017-04-13 12:50:36 -0400

    Rename, for better consistency

--- R/read_csv_with_rownames.R
+++ R/read_csv_with_rownames.R
@@ -1,4 +1,4 @@
-#' Load a CSV file with readr, setting a given column as the rownames
+#' Read a CSV file with readr, setting a given column as the rownames
 #'
 #' @author Rory Kirchner
 #'
@@ -9,7 +9,7 @@
 #'
 #' @return Data frame
 #' @export
-load_csv_with_rownames <- function(filename, column) {
+read_csv_with_rownames <- function(filename, column) {
     dat <- read_csv(filename, progress = FALSE) %>%
         as.data.frame
     rownames(dat) <- dat[, column]
