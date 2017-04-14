277d9e0389a5180d5e739c503539283f09005445
roryk <rory.kirchner@gmail.com>
2017-04-14 13:31:27 -0400

    Add function to create a skeleteon bcbio run object.

--- R/integrity_checks.R
+++ R/integrity_checks.R
@@ -3,8 +3,6 @@
 #' @rdname integrity_checks
 #' @author Michael Steinbaugh
 
-
-
 #' @rdname integrity_checks
 #' @description Check \code{bcbio-nextgen} run
 #' @param run \code{bcbio-nextgen} run
@@ -13,27 +11,25 @@ check_run <- function(run) {
     if (!is.list(run)) {
         stop("bcbio run object not found")
     }
-    if (!length(dir(run$parent_dir))) {
+    if (!file.exists(run$parent_dir)) {
         stop("could not access parent_dir")
     }
-    if (!length(dir(run$run_dir))) {
+    if (!file.exists(run$run_dir)) {
         stop("could not access run_dir")
     }
-    if (!length(dir(run$config_dir))) {
+    if (!file.exists(run$config_dir)) {
         stop("could not access config_dir")
     }
-    if (!length(dir(run$final_dir))) {
+    if (!file.exists(run$final_dir)) {
         stop("could not access final_dir")
     }
     if (!length(run$sample_dirs)) {
         stop("no sample directories in run")
     }
-    if (!length(dir(run$project_dir))) {
+    if (!file.exists(run$project_dir)) {
         stop("could not access project_dir")
     }
 }
-
-
 
 #' @rdname integrity_checks
 #' @description Check sparse counts matrix
@@ -43,4 +39,28 @@ check_sparse <- function(sparsecounts) {
     if (class(sparsecounts)[1] != "dgCMatrix") {
         stop("sparse counts must be dgCMatrix")
     }
+}
+
+##' create a skeleton bcbio project
+##'
+##' @param basedir base directory in which to create skeleton bcbio structure
+##' @return a bcbio run object
+##' @author Rory Kirchner
+##' @author Michael Steinbaugh
+##' @export
+make_bcbio_skeleton <- function(basedir=getwd(), organism="hsapiens") {
+  parent_dir = file.path(basedir, "skeleton")
+  run <- list(parent_dir = file.path(parent_dir),
+              run_dir = file.path(parent_dir),
+              config_dir = file.path(parent_dir, "config"),
+              final_dir = file.path(parent_dir, "final"),
+              project_dir = file.path(parent_dir, "final"),
+              sample_dirs = c(NA),
+              organism = organism,
+              intgroup = NA)
+  dir.create(run$parent_dir, showWarnings = FALSE)
+  dir.create(run$config_dir, showWarnings = FALSE)
+  dir.create(run$final_dir, showWarnings = FALSE)
+  dir.create(run$project_dir, showWarnings = FALSE)
+  return(run)
 }
