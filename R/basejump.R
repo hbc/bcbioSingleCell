f8c1af4224fcb8f404c70fdf3965f95828411a50
Michael Steinbaugh <mike@steinbaugh.com>
2017-04-14 14:43:49 -0400

    Export case utility functions for working with consults

--- R/basejump.R
+++ R/basejump.R
@@ -1,10 +1,14 @@
-# snake_case versions of basejump utility functions
-# https://github.com/steinbaugh/basejump/blob/master/R/kable.R
-# https://github.com/steinbaugh/basejump/blob/master/R/makeNames.R
-# https://github.com/steinbaugh/basejump/blob/master/R/setNames.R
+#' Utility functions from basejump package
+#'
+#' @rdname basejump
+#'
+#' @author Michael Steinbaugh
+#'
+#' @param data Data type that supports name assignments
 
 
 
+#' @keywords internal
 kables <- function(list, captions = NULL) {
     output <- opts_knit$get("rmarkdown.pandoc.to")
     if (!is.null(output)) {
@@ -19,6 +23,7 @@ kables <- function(list, captions = NULL) {
 
 
 
+#' @keywords internal
 make_names <- function(character) {
     character %>%
         as.character %>%
@@ -44,6 +49,7 @@ make_names <- function(character) {
 
 
 
+#' @keywords internal
 make_names_snake <- function(character) {
     character %>%
         make_names %>%
@@ -52,12 +58,18 @@ make_names_snake <- function(character) {
 
 
 
+#' @rdname basejump
+#' @description Set names in dot notation
+#' @export
 set_names_dot <- function(data) {
     set_names(data, make_names(colnames(data)))
 }
 
 
 
+#' @rdname basejump
+#' @description Set names in snake_case
+#' @export
 set_names_snake <- function(data) {
     set_names(data, make_names_snake(colnames(data)))
 }
