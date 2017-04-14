e5b88f75d93e77c93f8d863b3b320f03065bb95d
Michael Steinbaugh <mike@steinbaugh.com>
2017-04-14 11:32:13 -0400

    Add set_names_dot for improved handling of Seurat names

--- R/basejump.R
+++ R/basejump.R
@@ -1,4 +1,10 @@
+# snake_case versions of basejump utility functions
 # https://github.com/steinbaugh/basejump/blob/master/R/kable.R
+# https://github.com/steinbaugh/basejump/blob/master/R/makeNames.R
+# https://github.com/steinbaugh/basejump/blob/master/R/setNames.R
+
+
+
 kables <- function(list, captions = NULL) {
     output <- opts_knit$get("rmarkdown.pandoc.to")
     if (!is.null(output)) {
@@ -13,7 +19,6 @@ kables <- function(list, captions = NULL) {
 
 
 
-# https://github.com/steinbaugh/basejump/blob/master/R/makeNames.R
 make_names <- function(character) {
     character %>%
         as.character %>%
@@ -39,7 +44,6 @@ make_names <- function(character) {
 
 
 
-# https://github.com/steinbaugh/basejump/blob/master/R/makeNames.R
 make_names_snake <- function(character) {
     character %>%
         make_names %>%
@@ -48,7 +52,12 @@ make_names_snake <- function(character) {
 
 
 
-# https://github.com/steinbaugh/basejump/blob/master/R/setNames.R
+set_names_dot <- function(data) {
+    set_names(data, make_names(colnames(data)))
+}
+
+
+
 set_names_snake <- function(data) {
     set_names(data, make_names_snake(colnames(data)))
 }
