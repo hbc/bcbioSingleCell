34f8f09ccc7aee5450e3f9f6540a84b0177c4ff1
Michael Steinbaugh <mike@steinbaugh.com>
2017-04-17 16:34:55 -0400

    Fix for featureids not matching rownames after subsetting

--- R/combine_features.R
+++ R/combine_features.R
@@ -1,14 +1,17 @@
-##' combine features of a sparse matrix
-##'
-##' this collapsed features with the same feature id by summing them
-##'
-##' @param sparse a sparse matrix of counts
-##' @param featureids new feature ids for the collapsed features
-##' @return sparse matrix
-##' @author Rory Kirchner
-##' @export
+#' Combine features of a sparse matrix
+#'
+#' This collapsed features with the same feature id by summing them
+#'
+#' @author Rory Kirchner
+#'
+#' @param sparse A sparse matrix of counts
+#' @param featureids New feature ids for the collapsed features
+#'
+#' @return Sparse matrix
+#' @export
 combine_features <- function(sparse, featureids) {
   rownames(sparse) <- featureids
   sparse <- sparse[!is.na(rownames(sparse)), ]
-  return(aggregate.Matrix(sparse, featureids, fun="sum"))
+  combine <- aggregate.Matrix(sparse, rownames(sparse), fun = "sum")
+  return(combine)
 }
