5094c927c41e898a8e31926f7074dbac34237221
Michael Steinbaugh <mike@steinbaugh.com>
2017-04-18 17:35:37 -0400

    Flipped grepl pattern match

--- R/detect_orchestra.R
+++ R/detect_orchestra.R
@@ -1,14 +1,14 @@
 #' Detect if R is running on Orchestra HPC cluster
-#' 
+#'
 #' Utility function to enable CPU intensive tasks
-#' 
+#'
 #' @author Michael Steinbaugh
-#' 
+#'
 #' @export
 detect_orchestra <- function() {
     if (Sys.info()[["sysname"]] == "Linux" &
         Sys.info()[["login"]] == "root" &
-        grepl(Sys.info()[["nodename"]], "^clarinet")) {
+        grepl("^clarinet", Sys.info()[["nodename"]])) {
         return(TRUE)
     } else {
         return(FALSE)
