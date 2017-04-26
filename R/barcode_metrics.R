2cb86f7ba5656cf68d8dec05d7e9a0195dbdb866
Michael Steinbaugh <mike@steinbaugh.com>
2017-04-26 12:49:15 -0400

    Remote attempt to detect barcode metadata file on disk. Update mutate function to dplyr 0.6.0.

--- R/barcode_metrics.R
+++ R/barcode_metrics.R
@@ -20,55 +20,36 @@ barcode_metrics <- function(
     annotations <- ensembl_annotations(run)
 
     coding <- annotations %>%
-        filter_(.dots = quote(broad_class == "coding")) %>%
+        .[.$broad_class == "coding", ] %>%
         .$external_gene_name %>% unique %>% sort
     mito <- annotations %>%
-        filter_(.dots = quote(broad_class == "mito")) %>%
+        .[.$broad_class == "mito", ] %>%
         .$external_gene_name %>% unique %>% sort
 
     # Calculate colSums
-    metrics <- data.frame(
+    metrics <- tibble(
         identifier = colnames(sparsecounts),
         total_counts = Matrix::colSums(sparsecounts),
         genes_detected = Matrix::colSums(sparsecounts > 0),
-        coding_counts = Matrix::colSums(sparsecounts[rownames(sparsecounts) %in% coding, ]),
-        mito_counts = Matrix::colSums(sparsecounts[rownames(sparsecounts) %in% mito, ]))
-    
-    # dplyr operations
-    metrics <- metrics %>%
+        coding_counts = Matrix::colSums(
+            sparsecounts[rownames(sparsecounts) %in% coding, ]),
+        mito_counts = Matrix::colSums(
+            sparsecounts[rownames(sparsecounts) %in% mito, ])) %>%
+        # Unique identifier
         separate_("identifier",
                   c("sample_barcode", "cellular_barcode"),
                   sep = ":",
                   remove = FALSE) %>%
-        mutate_(.dots = set_names(
-            list(quote(log(genes_detected) / log(total_counts)),
-                 quote(mito_counts / total_counts)),
-            c("log_detected_per_count",
-              "percent_mito")))
-
-    # Attempt to detect metadata saved manually for consult, if necessary
-    if (is.null(metadata)) {
-        metafile <- list.files("meta",
-                               pattern = "^sample_barcodes",
-                               full.names = TRUE)
-        if (length(metafile) == 1) {
-            metadata <- read_sample_barcodes_metadata(metafile)
-        } else if (length(metafile) > 1) {
-            stop("multiple sample barcode metadata files matched...
-                 need to clean the meta folder")
-        } else {
-            metadata <- NULL
-        }
-    }
+        # Summary statistics
+        mutate(log10_detected_per_count = log10(.data$genes_detected) /
+                   log10(.data$total_counts),
+               percent_mito = .data$mito_counts / .data$total_counts)
 
     # Join metadata, if present
     if (!is.null(metadata)) {
         metrics <- left_join(metrics, metadata, by = "sample_barcode")
     }
 
-    metrics <- metrics %>%
-        arrange_(.dots = "identifier") %>%
-        set_rownames(.$identifier)
-
+    metrics <- metrics[order(metrics$identifier), ]
     return(metrics)
 }
