5deb45ace04190fd208d806ea58db973a59e5726
Michael Steinbaugh <mike@steinbaugh.com>
2017-05-03 15:37:01 -0400

    Deprecated. Don't rely on the config directory.

--- R/read_bcbio.R
+++ R/read_bcbio.R
@@ -10,34 +10,6 @@
 
 
 
-#' @rdname read_bcbio
-#' @description Import metadata
-#'
-#' @return Metadata data frame
-#' @export
-read_bcbio_metadata <- function(
-    run,
-    save = FALSE) {
-    check_run(run)
-    metadata <- list.files(run$config_dir,
-                           pattern = ".csv$",
-                           full.names = TRUE) %>%
-        read_csv(col_types = cols()) %>%
-        set_names_snake %>%
-        arrange_(.dots = "description") %>%
-        as.data.frame %>%
-        set_rownames(.$description)
-
-    if (isTRUE(save)) {
-        save(metadata, file = "data/metadata.rda")
-        write_csv(metadata, "meta/metadata.csv")
-    }
-
-    return(metadata)
-}
-
-
-
 #' Import single-cell RNA-seq count data
 #'
 #' Import transcript-level count data from a bcbio run into a sparse matrix
