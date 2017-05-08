c5fae482ab1a1116e4409060c86b97698290f5f9
Michael Steinbaugh <mike@steinbaugh.com>
2017-05-08 18:23:03 -0400

    Fix integrity check on run object

--- R/integrity_checks.R
+++ R/integrity_checks.R
@@ -3,6 +3,8 @@
 #' @rdname integrity_checks
 #' @author Michael Steinbaugh
 
+
+
 #' @rdname integrity_checks
 #' @description Check \code{bcbio-nextgen} run
 #' @param run \code{bcbio-nextgen} run
@@ -11,16 +13,18 @@ check_run <- function(run) {
     if (!is.list(run)) {
         stop("Run object is not a list")
     }
-    if (!file.exists(run$final_dir)) {
-        stop("Could not access final_dir")
-    }
-    if (!length(run$sample_dirs)) {
-        stop("No sample directories in run")
+    if (!file.exists(run$upload_dir)) {
+        stop("Could not access upload dir")
     }
     if (!length(dir(run$project_dir))) {
         stop("Could not access project_dir")
     }
+    if (!length(run$sample_dirs)) {
+        stop("No sample directories in run")
+    }
 }
+
+
 
 #' @rdname integrity_checks
 #' @description Check sparse counts matrix
