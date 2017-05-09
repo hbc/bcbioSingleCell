6775ff7160d0f2061be245222cc44f74d279a636
Michael Steinbaugh <mike@steinbaugh.com>
2017-05-09 13:53:29 -0400

    Rename "input" to "counts" for better consistency across package

--- R/read_bcbio.R
+++ R/read_bcbio.R
@@ -17,19 +17,19 @@
 #'
 #' @return Sparse counts matrix
 #' @export
-read_bcbio_sparsecounts <- function(run, strip_version = TRUE) {
-    input <- file.path(run$project_dir, "tagcounts.mtx") %>% read_sparsecounts
+read_bcbio_counts <- function(run, strip_version = TRUE) {
+    counts <- file.path(run$project_dir, "tagcounts.mtx") %>% read_counts
     if (isTRUE(strip_version)) {
-        input <- strip_transcript_versions(input)
+        counts <- strip_transcript_versions(counts)
     }
     # Convert transcript-level counts to gene-level
     ensembl <- run$ensembl
     # Avoid setting rownames on data frames (tidy principle)
-    indexes <- match(rownames(input), ensembl$ensembl_transcript_id)
+    indexes <- match(rownames(counts), ensembl$ensembl_transcript_id)
     # collect(select(ensembl, external_gene_name))[[1]]
     gene_names <- ensembl %>%
         as.data.frame %>%
         .[indexes, "external_gene_name"]
-    sparsecounts <- combine_features(input, gene_names)
-    return(sparsecounts)
+    counts <- combine_features(counts, gene_names)
+    return(counts)
 }
