008dba68f47289f0e6298cacd567730401ca0855
Michael Steinbaugh <mike@steinbaugh.com>
2017-05-12 15:31:17 -0400

    Update to use run object

--- R/filter_cellular_barcodes.R
+++ R/filter_cellular_barcodes.R
@@ -1,27 +1,28 @@
-#' Filter cellular barcodes
+#' Filter cellular barcodes.
 #'
 #' Apply gene detection, mitochondrial abundance, and novelty score cutoffs to
 #' cellular barcodes.
 #'
 #' @author Michael Steinbaugh
 #'
-#' @param metrics Barcode metrics data frame
-#' @param min_genes Minimum number of genes detected
-#' @param max_genes Maximum number of genes detected
-#' @param percent_mito Maximum mitochondrial pct abundance (\code{0-1} scale)
-#' @param novelty Minimum novelty score
-#' @param plot Print relevant plots
+#' @param run bcbio-nextgen scRNA-seq run.
+#' @param min_genes Minimum number of genes detected.
+#' @param max_genes Maximum number of genes detected.
+#' @param percent_mito Maximum mitochondrial pct abundance (\code{0-1} scale).
+#' @param novelty Minimum novelty score.
+#' @param plot Print relevant plots.
 #'
-#' @return Filtered metrics data frame
+#' @return Filtered metrics data frame.
 #' @export
 filter_cellular_barcodes <- function(
-    metrics,
+    run,
     min_genes = 500,
     max_genes = 5000,
     percent_mito = 25,
     novelty = 0.75,
     plot = TRUE) {
-    filtered <- metrics %>%
+    check_run(run)
+    filtered <- run$metrics %>%
         .[.$genes_detected > min_genes, ] %>%
         .[.$genes_detected < max_genes, ] %>%
         .[.$percent_mito < percent_mito, ] %>%
