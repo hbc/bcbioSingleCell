598a5362f295aae6f2355c0e3ffd05a488c2fad3
Michael Steinbaugh <mike@steinbaugh.com>
2017-05-15 15:37:26 -0400

    Rework dplyr chain

--- R/ensembl_annotations.R
+++ R/ensembl_annotations.R
@@ -1,10 +1,9 @@
-#' Get transcript-to-gene annotations from
-#' \href{http://www.ensembl.org/}{Ensembl}.
+#' Ensembl annotations
 #'
-#' This function also defines broad classes, which are used in quality control
-#' analysis.
+#' Download transcript-to-gene annotations from
+#' [Ensembl](http://www.ensembl.org/). This function also defines broad classes,
+#' which are used in quality control analysis.
 #'
-#' @keywords internal
 #' @author Rory Kirchner
 #' @author Michael Steinbaugh
 #'
@@ -12,47 +11,35 @@
 #'
 #' @return Data frame.
 ensembl_annotations <- function(run) {
-    ensembl <- useEnsembl(
-        biomart = "ensembl",
-        dataset = paste(run$organism, "gene_ensembl", sep = "_"))
-
-    meta <- getBM(
-        mart = ensembl,
-        attributes = c("ensembl_gene_id",
-                       "ensembl_transcript_id",
-                       "external_gene_name",
-                       "gene_biotype",
-                       "chromosome_name")
-    ) %>%
-        as_tibble %>%
-        group_by(!!sym("ensembl_gene_id")) %>%
-        arrange(!!sym("ensembl_transcript_id"), .by_group = TRUE)
-
+    import_tidy_verbs()
     # Broad class definitions
     coding <- c("protein_coding")
     decaying <- c("non_stop_decay", "nonsense_mediated_decay")
     noncoding <- c("known_ncrna", "lincRNA", "non_coding")
-    srna <- c("miRNA",
-              "misc_RNA",
-              "ribozyme",
-              "rRNA",
-              "scaRNA",
-              "scRNA",
-              "snoRNA",
-              "snRNA",
-              "sRNA")
-    meta$broad_class <-
-        case_when(tolower(meta$chromosome_name) == "mt" ~ "mito",
-                  # Fix to match Drosophila genome (non-standard)
-                  grepl("mito", meta$chromosome_name) ~ "mito",
-                  grepl("pseudo", meta$gene_biotype) ~ "pseudo",
-                  grepl("TR_", meta$gene_biotype) ~ "TCR",
-                  grepl("IG_", meta$gene_biotype) ~ "IG",
-                  meta$gene_biotype %in% srna ~ "small",
-                  meta$gene_biotype %in% decaying ~ "decaying",
-                  meta$gene_biotype %in% noncoding ~ "noncoding",
-                  meta$gene_biotype %in% coding ~ "coding",
-                  TRUE ~ "other")
-
-    return(meta)
+    srna <- c("miRNA", "misc_RNA", "ribozyme", "rRNA", "scaRNA", "scRNA",
+              "snoRNA", "snRNA", "sRNA")
+    ensembl <- useEnsembl(
+        biomart = "ensembl",
+        dataset = paste(run$organism, "gene_ensembl", sep = "_"))
+    getBM(mart = ensembl,
+          attributes = c("ensembl_gene_id",
+                         "ensembl_transcript_id",
+                         "external_gene_name",
+                         "gene_biotype",
+                         "chromosome_name")) %>%
+        as_tibble %>%
+        group_by(!!sym("ensembl_gene_id")) %>%
+        arrange(!!sym("ensembl_transcript_id"), .by_group = TRUE) %>%
+        mutate(broad_class = case_when(
+            tolower(.data$chromosome_name) == "mt" ~ "mito",
+            # Fix to match Drosophila genome (non-standard)
+            grepl("mito", .data$chromosome_name) ~ "mito",
+            grepl("pseudo", .data$gene_biotype) ~ "pseudo",
+            grepl("TR_", .data$gene_biotype) ~ "TCR",
+            grepl("IG_", .data$gene_biotype) ~ "IG",
+            .data$gene_biotype %in% srna ~ "small",
+            .data$gene_biotype %in% decaying ~ "decaying",
+            .data$gene_biotype %in% noncoding ~ "noncoding",
+            .data$gene_biotype %in% coding ~ "coding",
+            TRUE ~ "other"))
 }
