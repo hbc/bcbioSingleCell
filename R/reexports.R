aa9c564e38f4c58efaf944f0dc8897178fa1ffd7
Michael Steinbaugh <mike@steinbaugh.com>
2017-06-12 10:33:42 -0400

    Update NAMESPACE for Seurat template

--- R/reexports.R
+++ R/reexports.R
@@ -1,3 +1,12 @@
+# basejump ====
+# This is used to format names according to Seurat conventions
+#' @export
+basejump::dot_notation
+
+
+
+# Matrix ====
+# Re-export, as to not confuse with base or BiocGeneric variants
 #' @export
 Matrix::colSums
 
