072b09d751c3f761e291a749a36860e1d3e0b8b5
Michael Steinbaugh <mike@steinbaugh.com>
2017-06-13 14:34:02 -0400

    Improve 10X import to handle multiple samples and aggregate into a single sparse matrix

--- R/read.R
+++ R/read.R
@@ -251,38 +251,59 @@ read_metadata <- function(
 #' @return Sparse counts matrix (dgCMatrix).
 #' @export
 read_10x <- function(data_dir) {
-    barcodes_file <- file.path(data_dir, "barcodes.tsv")
-    genes_file <- file.path(data_dir, "genes.tsv")
-    matrix_file <- file.path(data_dir, "matrix.mtx")
+    # Recurse through specific data directory and identify sample subdirectories
+    # by the presence of a `matrix.mtx` counts file.
+    sample_dirs <- list.files(
+        data_dir,
+        full.names = TRUE,
+        pattern = "matrix.mtx",
+        recursive = TRUE) %>%
+        normalizePath %>%
+        dirname %>%
+        set_names(basename(.))
+    message(paste(length(sample_dirs), "samples detected"))
+    message("Reading 10X Chromium samples...")
+    lst <- pbmclapply(seq_along(sample_dirs), function(a) {
+        name <- names(sample_dirs)[a]
+        sample_dir <- sample_dirs[a]
+        barcodes_file <- file.path(sample_dir, "barcodes.tsv")
+        if (!file.exists(barcodes_file)) {
+            stop("Barcodes file missing")
+        }
+        genes_file <- file.path(sample_dir, "genes.tsv")
+        if (!file.exists(genes_file)) {
+            stop("Gene names file missing")
+        }
+        matrix_file <- file.path(sample_dir, "matrix.mtx")
+        if (!file.exists(matrix_file)) {
+            stop("MatrixMart counts file missing")
+        }
 
-    if (!file.exists(barcodes_file)) {
-        stop("Barcodes file missing")
-    }
-    if (!file.exists(genes_file)) {
-        stop("Gene names file missing")
-    }
-    if (!file.exists(matrix_file)) {
-        stop("MatrixMart counts file missing")
-    }
+        # Read MatrixMart expression counts matrix
+        counts <- readMM(matrix_file)
 
-    # Read MatrixMart expression counts matrix
-    counts <- readMM(matrix_file)
+        # Assign barcodes to colnames
+        barcodes <- read_lines(barcodes_file)
+        # Check for `-1` suffix in barcode names, indicative of 10X data
+        pattern <- "\\-1$"
+        if (all(grepl(pattern, barcodes))) {
+            barcodes <- str_replace(barcodes, pattern, "")
+        }
+        # Append the sample name to the barcodes. This will be used after
+        # list is generated to [cBind()] all counts to a single sparse matrix.
+        colnames(counts) <- paste(name, barcodes, sep = "-")
 
-    # Assign barcodes to colnames
-    barcodes <- readLines(barcodes_file)
-    # Check for `-1` suffix in barcode names, indicative of 10X data
-    pattern <- "\\-1$"
-    if (all(grepl(pattern, barcodes))) {
-        barcodes <- gsub(pattern, "", barcodes)
-    }
-    colnames(counts) <- barcodes
+        # Assign gene names (symbols) to rownames
+        genes <- read_tsv(genes_file,
+                          col_names = c("ensembl_gene_id",
+                                        "external_gene_name"))
+        rownames(counts) <- make.unique(genes$external_gene_name)
 
-    # Assign gene names (symbols) to rownames
-    genes <- read_tsv(genes_file,
-                      col_names = c("ensembl_gene_id",
-                                    "external_gene_name"))
-    rownames(counts) <- make.unique(genes$external_gene_name)
-
-    # Coerce dgTMatrix to dgCMatrix
-    as(counts, "dgCMatrix")
+        # Coerce dgTMatrix to dgCMatrix (sparse)
+        as(counts, "dgCMatrix")
+    }) %>% set_names(names(sample_dirs))
+    # [fix] Any reason why we'd want to keep the files separate? Does this make
+    # individual sample analysis easier?
+    message("Aggregating samples to a single sparse matrix...")
+    do.call(cBind, lst)
 }
