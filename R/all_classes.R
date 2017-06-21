c79ac8acfd6b1a283195a311d13b1ee53b13f055
Michael Steinbaugh <mike@steinbaugh.com>
2017-06-21 15:06:19 -0400

    Use same class structure as bcbioRnaDataSet

--- R/all_classes.R
+++ R/all_classes.R
@@ -1,20 +1,14 @@
 #' bcbioSCDataSet
 #'
 #' `bcbioSCDataSet` is a subclass of [SummarizedExperiment] designed to store a
-#' single-cell RNA-seq analysis. This class contains experiment metadata, raw
-#' counts, and summary statistics for each sample analyzed.
-#'
-#' Methods for this objects ...
-#'
-#' `metadata` contains ...
+#' single-cell RNA-seq analysis. This class contains sparse counts saved as `dgCMatrix`, sample barcodes, other metadata, and summary statistics for each sample analyzed.
 #'
 #' @rdname bcbioSCDataSet
-#' @keywords internal
-#' @aliases bcbioSCDataSet-class
+#' @author Michael Steinbaugh
+#'
 #' @export
 bcbioSCDataSet <- setClass(
     "bcbioSCDataSet",
     contains = "SummarizedExperiment",
-    representation = representation(
-        callers = "SimpleList"))
+    representation = representation(callers = "SimpleList"))
 setValidity("bcbioSCDataSet", function(object) TRUE)
