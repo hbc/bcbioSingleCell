d8aea8ee104987fcd0b96736edf447e5395ba63b
Michael Steinbaugh <mike@steinbaugh.com>
2017-06-21 21:48:23 -0400

    Update NAMESPACE and documentation

--- R/bcbioSinglecell.R
+++ R/bcbioSinglecell.R
@@ -2,15 +2,12 @@
 #'
 #' Utility functions for analysis of bcbio-nextgen single-cell RNA-seq data.
 #'
-#' @keywords internal
-#'
-#' @import annotables
-#' @import basejump
-#' @import Biobase
-#' @import ggplot2
-#' @import methods
-#' @import SummarizedExperiment
-#' @import S4Vectors
+#' @import annotables basejump Biobase BiocGenerics methods SummarizedExperiment
+#'   S4Vectors
+#' @importFrom ggplot2 aes_ coord_flip element_text expand_limits facet_wrap
+#'   geom_bar geom_boxplot geom_histogram geom_hline geom_label geom_line
+#'   geom_point geom_smooth geom_text geom_violin geom_vline ggplot ggtitle labs
+#'   qplot scale_x_log10 scale_y_log10 scale_y_sqrt theme unit xlab xlim ylab
 #' @importFrom Matrix cBind readMM writeMM
 #' @importFrom Matrix.utils aggregate.Matrix
 #' @importFrom scales math_format trans_breaks trans_format
