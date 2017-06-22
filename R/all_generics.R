8fa71c25e636ae0d082ff533a63408033f690df8
Michael Steinbaugh <mike@steinbaugh.com>
2017-06-21 23:29:12 -0400

    S4 version of metadata_table

--- R/all_generics.R
+++ R/all_generics.R
@@ -5,3 +5,9 @@ setGeneric("bcbio", function(object, ...) standardGeneric("bcbio"))
 #' @rdname bcbio
 #' @export
 setGeneric("bcbio<-", function(object, ..., value) standardGeneric("bcbio<-"))
+
+#' @rdname metadata_table
+#' @export
+setGeneric("metadata_table", function(object, ...) {
+    standardGeneric("metadata_table")
+})
