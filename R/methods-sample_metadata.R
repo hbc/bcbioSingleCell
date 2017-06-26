b51deed84a0beda50f37da48e802f1b6b97595d3
Michael Steinbaugh <mike@steinbaugh.com>
2017-06-26 09:39:52 -0400

    Add support for bcbioSCSubset

--- R/methods-sample_metadata.R
+++ R/methods-sample_metadata.R
@@ -2,9 +2,17 @@
 #'
 #' @rdname sample_metadata
 #'
+#' @author Michael Steinbaugh
+#'
 #' @param object Object.
 #'
 #' @export
 setMethod("sample_metadata", "bcbioSCDataSet", function(object) {
     metadata(object)[["sample_metadata"]] %>% as.data.frame
+})
+
+#' @rdname sample_metadata
+#' @export
+setMethod("sample_metadata", "bcbioSCSubset", function(object) {
+    object@callers[["sample_metadata"]] %>% as.data.frame
 })
