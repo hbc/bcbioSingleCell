d605d7963254a5eabdafb056a435823bd7d8aa8c
Michael Steinbaugh <mike@steinbaugh.com>
2017-06-26 12:30:56 -0400

    Initial commit

---
+++ R/methods-counts.R
@@ -0,0 +1,14 @@
+#' Counts accessor
+#'
+#' @rdname counts
+#'
+#' @author Michael Steinbaugh
+#'
+#' @param object Object.
+#' @param ... Additional parameters.
+#'
+#' @return Sparse counts (`dgCMatrix`).
+#' @export
+setMethod("counts", "bcbioSCDataSet", function(object) {
+    assays(object)[["sparse_counts"]]
+})
