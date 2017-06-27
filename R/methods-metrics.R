534304b412c83b655a45fdec4fe7d154cf93d0c5
Michael Steinbaugh <mike@steinbaugh.com>
2017-06-27 18:22:15 -0400

    Include file name in metrics, for facet wrapping in ggplot

--- R/methods-metrics.R
+++ R/methods-metrics.R
@@ -9,16 +9,18 @@
 setMethod("metrics", "bcbioSCDataSet", function(object) {
     interesting_groups <- interesting_groups(object)
     meta <- sample_metadata(object) %>%
-        .[, unique(c("sample_name", interesting_groups))] %>%
-        rownames_to_column("sample_id")
-    colData(object) %>%
+        tidy_select(unique(c("file_name",
+                             "sample_id",
+                             "sample_name",
+                             interesting_groups)))
+    metrics <- colData(object) %>%
         as.data.frame %>%
         rownames_to_column %>%
         mutate(separate = .data[["rowname"]]) %>%
         separate_(col = "separate",
                   into = c("sample_id", "cellular_barcode"),
                   sep = ":") %>%
-        mutate(cellular_barcode = NULL) %>%
-        left_join(meta, by = "sample_id") %>%
+        mutate(cellular_barcode = NULL)
+    left_join(meta, metrics, by = "sample_id") %>%
         column_to_rownames
 })
