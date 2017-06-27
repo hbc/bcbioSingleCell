bbafdd74208f68b8105096a53569884b3f157960
Michael Steinbaugh <mike@steinbaugh.com>
2017-06-27 17:28:07 -0400

    Metrics metadata handling tweak

--- R/methods-metrics.R
+++ R/methods-metrics.R
@@ -7,17 +7,19 @@
 #' @return [data.frame].
 #' @export
 setMethod("metrics", "bcbioSCDataSet", function(object) {
-    interesting_groups <- metadata(object)[["interesting_groups"]]
-    metadata <- sample_metadata(object) %>%
-        .[, unique(c("sample_name", interesting_groups))]
+    interesting_groups <- interesting_groups(object)
+    meta <- sample_metadata(object) %>%
+        .[, unique(c("sample_name", interesting_groups))] %>%
+        rownames_to_column("sample_id")
     colData(object) %>%
         as.data.frame %>%
         rownames_to_column %>%
         mutate(separate = .data[["rowname"]]) %>%
         separate_(col = "separate",
-                  into = c("sample_name", "barcode"),
-                  sep = "-") %>%
-        mutate(barcode = NULL) %>%
-        left_join(metadata, by = "sample_name") %>%
+                  into = c("sample_id", "cellular_barcode"),
+                  sep = ":") %>%
+        left_join(meta, by = "sample_id") %>%
+        mutate(cellular_barcode = NULL,
+               sample_id = NULL) %>%
         column_to_rownames
 })
