8c521ff2b49b68877e1e87b728bba717f321ee20
Michael Steinbaugh <mike@steinbaugh.com>
2017-06-27 18:59:52 -0400

    Add support for unique sample names. This is useful for QC plot functions.

--- R/methods-sample_metadata.R
+++ R/methods-sample_metadata.R
@@ -5,8 +5,26 @@
 #' @author Michael Steinbaugh
 #'
 #' @param object Object.
+#' @param ... Additional parameters.
+#' @param unique_names Unique sample names.
 #'
 #' @export
-setMethod("sample_metadata", "bcbioSCDataSet", function(object) {
-    metadata(object)[["sample_metadata"]] %>% as.data.frame
+setMethod("sample_metadata", "bcbioSCDataSet", function(
+    object, unique_names = FALSE) {
+    meta <- metadata(object)[["sample_metadata"]] %>% as.data.frame
+    if (isTRUE(unique_names)) {
+        # Ensure unique sample names
+        if (any(duplicated(meta[["sample_name"]]))) {
+            meta[["sample_name"]] <-
+                paste0(meta[["sample_name"]],
+                       " (",
+                       meta[["file_name"]],
+                       ")")
+        }
+        # Double check
+        if (any(duplicated(meta[["sample_name"]]))) {
+            stop("Automatic generation of unique sample names failed")
+        }
+    }
+    meta
 })
