7cf71edf514c98476e0beb35eae98742cd441c99
Michael Steinbaugh <mike@steinbaugh.com>
2017-06-28 18:30:39 -0400

    Update template, lint tweaks

--- R/internal-read_file.R
+++ R/internal-read_file.R
@@ -68,8 +68,9 @@
 
     # Return
     if (is_tibble(data)) {
-        # Finally, strip all NA columns and rows, and sanitize column names
-        data %>% remove_na %>% snake(rownames = FALSE)
+        data %>%
+            remove_na %>%
+            snake(rownames = FALSE)
     } else {
         data
     }
