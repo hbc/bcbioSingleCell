f1c80a0698478ba62c343c81c10800625528a52a
Michael Steinbaugh <mike@steinbaugh.com>
2017-06-27 22:24:35 -0400

    Working individual barcode plot

--- R/plots-barcodes.R
+++ R/plots-barcodes.R
@@ -6,46 +6,49 @@
 .plot_cb_histogram <- function(bcb) {
     barcodes <- bcbio(bcb, "cellular_barcodes")
     message("Generating barcode histograms")
-    pbmclapply(seq_along(barcodes), function(a) {
-        name <- names(bcb[["barcodes"]][[a]])
-        sample_name <- bcb[["metadata"]][name, "sample_name"]
-        title <- paste(sample_name, name, sep = " : ")
+    pblapply(seq_along(barcodes), function(a) {
+        title <- paste0(
+            names(barcodes)[[a]],
+            " (",
+            sample_metadata(bcb)[name, "sample_name"],
+            ")")
         bcs <- barcodes[[a]] %>%
-            as.data.frame %>%
-            rownames_to_column %>%
-            set_colnames(c("cellular_barcode", "reads")) %>%
             mutate(log10_reads = log10(.data[["reads"]]))
-        bcs_hist <- hist(bcs[["log10_reads"]], plot = FALSE, n = 50L)
-        f_log <- bcs_hist[["count"]]
+        bcs_hist <- hist(bcs[["log10_reads"]], n = 50L, plot = FALSE)
+        f_log <- bcs_hist[["counts"]]
         x_log <- bcs_hist[["mids"]]
-        y <- f_log * (10L ^ x_log) / sum(f_log * (10L ^ x_log))
-        ggplot(
-            x = 10L ^ x_log,
-            y = y) +
+        qplot(x = 10L ^ x_log,
+              y = f_log * (10L ^ x_log) / sum(f_log * (10L ^ x_log))) +
             geom_point() +
             geom_line() +
             scale_x_log10(
                 breaks = trans_breaks("log10", function(x) 10L ^ x),
                 labels = trans_format("log10", math_format(~10L ^ .x))) +
             labs(title = title,
-                 x = "number of reads assigned to a cell",
+                 x = "reads per cell",
                  y = "proportion of cells")
     }) %>% invisible
 }
 
 .plot_cb_violin <- function(bcb) {
-    title <- deparse(substitute(bcb))
-    barcodes <- .unlist_barcodes(bcb)
-    message("Generating violin plot...")
+    barcodes <- bcbio(bcb, "cellular_barcodes") %>%
+        .bind_cellular_barcodes %>%
+        separate_(col = "rowname",
+                  into = c("sample_id", "cellular_barcode"),
+                  sep = ":") %>%
+        mutate(cellular_barcode = NULL,
+               log10_reads = log10(.data[["reads"]])) %>%
+        left_join(sample_metadata(bcb), by = "sample_id")
+    interesting_group <- interesting_groups(bcb)[[1L]]
     ggplot(barcodes,
            aes_(x = ~sample_name,
                 y = ~log10_reads,
-                fill = ~sample_name)) +
+                fill = as.name(interesting_group))) +
+        facet_wrap(~file_name) +
         geom_violin(scale = "width") +
-        labs(title = paste(title, "violin plot"),
+        labs(title = "cellular barcode violin plot",
              x = "sample name",
              y = "log10 reads per cellular barcode") +
-        theme(legend.position = "none") +
         coord_flip()
 }
 
