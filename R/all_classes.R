3ec07eef9a1b6ec187db7b003c35e2c887e137fa
Michael Steinbaugh <mike@steinbaugh.com>
2017-07-10 11:56:38 -0400

    Redefine filtered cellular barcodes as [SCSubset] class instead of general [SummarizedExperiment]. This way we can be sure that metrics and other required filtering parameter metadata are set.

--- R/all_classes.R
+++ R/all_classes.R
@@ -1,10 +1,9 @@
 #' bcbioSCDataSet
 #'
-#' [bcbioSCDataSet] is a subclass of
-#' [SummarizedExperiment::SummarizedExperiment] designed to store a single-cell
-#' RNA-seq analysis. This class contains read counts save as a sparse matrix
-#' (`dgCMatrix`), sample barcodes, run metadata, and barcode summary statistics
-#' for each sample analyzed.
+#' [bcbioSCDataSet] is a subclass of [SummarizedExperiment] designed to store a
+#' single-cell RNA-seq analysis. This class contains read counts save as a
+#' sparse matrix (`dgCMatrix`), sample barcodes, run metadata, and barcode
+#' summary statistics for each sample analyzed.
 #'
 #' @author Michael Steinbaugh
 #'
@@ -19,8 +18,7 @@ setValidity("bcbioSCDataSet", function(object) TRUE)
 
 #' SCSubset
 #'
-#' [SCSubset] is a subclass of
-#' [SummarizedExperiment::SummarizedExperiment] designed to store a subset
+#' [SCSubset] is a subclass of [SummarizedExperiment] designed to store a subset
 #' of single-cell RNA-seq counts filtered by quality control analysis.
 #'
 #' @author Michael Steinbaugh
