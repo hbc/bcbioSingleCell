69f07e66348b4f476d19786e1e1f067b393f789f
Michael Steinbaugh <mike@steinbaugh.com>
2017-07-11 13:44:46 -0400

    Improve consistency of sample metadata storage, rather than performing operations in S4 method

--- R/internal-sample_metadata_file.R
+++ R/internal-sample_metadata_file.R
@@ -8,18 +8,18 @@
 #'
 #' @author Michael Steinbaugh
 #'
-#' @param sample_metadata_file Sample metadata file.
+#' @param file Sample metadata file.
 #' @param sample_dirs Sample directory paths.
 #' @param pipeline Pipeline used to process the samples.
 #'
 #' @return [tibble].
 .sample_metadata_file <- function(
-    sample_metadata_file,
+    file,
     sample_dirs,
     pipeline) {
-    sample_metadata <- read_file_by_extension(sample_metadata_file)
+    meta <- read_file_by_extension(file) %>% snake
 
-    # Check sample_metadata file integrity
+    # Check file integrity
     if (pipeline == "bcbio") {
         required_cols <- c("index", "sequence", "sample_name")
     } else if (pipeline == "cellranger") {
@@ -27,49 +27,69 @@
     } else {
         stop("Unsupported pipeline")
     }
-    if (!all(required_cols %in% colnames(sample_metadata))) {
+    if (!all(required_cols %in% colnames(meta))) {
         stop(paste("Metadata file missing columns:",
                    toString(required_cols)))
     }
 
     # bcbio-nextgen sample_id construction
     if (pipeline == "bcbio") {
-        sample_metadata <- mutate(
-            sample_metadata,
-            revcomp = vapply(.data[["sequence"]], revcomp, character(1L)))
+        meta <- meta %>%
+            mutate(revcomp = vapply(.data[["sequence"]],
+                                    revcomp,
+                                    character(1L)))
         # Map sample names to revcomp barcode indexes
-        if ("file_name" %in% colnames(sample_metadata)) {
-            sample_metadata <- sample_metadata %>%
-                mutate(sample_id = paste(
-                    .data[["file_name"]], .data[["revcomp"]], sep = "_"))
+        if ("file_name" %in% colnames(meta)) {
+            meta <- meta %>%
+                mutate(sample_id = paste(.data[["file_name"]],
+                                         .data[["revcomp"]],
+                                         sep = "_"))
         } else {
-            revcomp_matches <- pull(sample_metadata, "revcomp")
+            revcomp_matches <- pull(meta, "revcomp")
             sample_matches <- basename(sample_dirs) %>%
                 str_match("(.*)_([ACGT]+)$") %>%
                 as_tibble %>%
                 set_colnames(c("sample_id", "file_name", "revcomp")) %>%
                 filter(.data[["revcomp"]] %in% syms(revcomp_matches))
-            sample_metadata <- suppressWarnings(
-                full_join(sample_metadata, sample_matches, by = "revcomp"))
+            meta <- suppressWarnings(
+                full_join(meta, sample_matches, by = "revcomp"))
         }
-        sample_metadata <- sample_metadata %>%
-            tidy_select(c("file_name",
-                          "index",
-                          "sequence",
-                          "revcomp",
-                          "sample_id",
-                          "sample_name"),
-                        everything())
     }
 
-    sample_metadata <- sample_metadata %>%
-        mutate(sample_id = snake(.data[["sample_id"]]))
+    # Ensure `sample_id` is valid name then arrange
+    meta <- meta %>%
+        mutate(sample_id = make.names(.data[["sample_id"]])) %>%
+        arrange(!!sym("sample_id"))
 
-    # Check that sample_ids match samples
-    if (!all(sample_metadata[["sample_id"]] %in% names(sample_dirs))) {
+    # Check that `sample_id` matches `sample_dirs`
+    if (!all(meta[["sample_id"]] %in% names(sample_dirs))) {
         stop("Sample directory names don't match the sample metadata file")
     }
 
-    sample_metadata %>%
-        arrange(!!sym("sample_id"))
+    # Set required columns, if empty
+    if (is.null(meta[["file_name"]])) {
+        meta[["file_name"]] <- "all_samples"
+    }
+    if (is.null(meta[["sample_name"]])) {
+        meta[["sample_name"]] <- meta[["sample_id"]]
+    }
+
+    # Ensure unique sample names
+    if (any(duplicated(meta[["sample_name"]]))) {
+        meta[["sample_name"]] <-
+            str_c(meta[["sample_name"]], " (", meta[["file_name"]], ")")
+        if (any(duplicated(meta[["sample_name"]]))) {
+            stop("Unique sample name generation failed")
+        }
+    }
+
+    priority_cols <- c("sample_id", "sample_name", "file_name")
+    if (pipeline == "bcbio") {
+        meta %>%
+            tidy_select(c(priority_cols, "index", "sequence", "revcomp"),
+                        everything())
+    } else {
+        meta %>%
+            tidy_select(priority_cols, everything())
+    }
 }
