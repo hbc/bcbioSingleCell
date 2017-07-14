e3e8976aa491ceef5f8ac841904efd6f936809fd
Michael Steinbaugh <mike@steinbaugh.com>
2017-07-14 06:23:24 -0600

    Metrics accessor update for snake_case barcodes

--- R/methods-metrics.R
+++ R/methods-metrics.R
@@ -13,13 +13,31 @@
 .metrics <- function(object) {
     meta <- sample_metadata(object) %>%
         tidy_select(unique(c(meta_priority_cols, interesting_groups(object))))
-    colData(object) %>%
+    col_data <- colData(object) %>%
         as.data.frame %>%
-        rownames_to_column %>%
-        mutate(
-            sample_id = str_replace(.data[["rowname"]], "_[ACGT_]+$", "")) %>%
-        left_join(meta, by = "sample_id") %>%
-        column_to_rownames
+        rownames_to_column("cellular_barcode")
+    # Sample identifier match fix
+    if (metadata(object)[["pipeline"]] == "bcbio") {
+        cb <- pull(col_data, "cellular_barcode")
+        pattern <- "^(.*)_([acgt]{8})_([acgt]{8})_([acgt]{8})$"
+        sample_ids <- str_match(cb, pattern) %>%
+            as_tibble %>%
+            set_colnames(c("cellular_barcode",
+                           "file_name",
+                           "revcomp",
+                           "cb1",
+                           "cb2")) %>%
+            mutate(sample_id = paste(
+                .data[["file_name"]],
+                .data[["revcomp"]],
+                sep = "_")) %>%
+            pull("sample_id")
+        col_data[["sample_id"]] <- sample_ids
+    }
+
+    left_join(col_data, meta, by = "sample_id") %>%
+        as.data.frame %>%
+        column_to_rownames("cellular_barcode")
 }
 
 
