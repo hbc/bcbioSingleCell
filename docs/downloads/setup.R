3d88d1c9775336a0002d47b04ec5a3984bebc9e7
Michael Steinbaugh <mike@steinbaugh.com>
2017-07-14 15:45:06 -0600

    Simplify setup.R and fix duplicate kables

--- docs/downloads/setup.R
+++ docs/downloads/setup.R
@@ -1,24 +1,10 @@
-# bcbioSinglecell ====
-library(bcbioSinglecell)
-if (file.exists("data/bcb.rda")) {
-    data(bcb)
-} else {
-    bcb <- load_run(
-        upload_dir = file.path("data", "singlecell"),
-        sample_metadata_file = file.path("meta", "sample_barcodes.xlsx"),
-        interesting_groups = "genotype")
-    save_data(bcb, compress = FALSE)
-}
-
-
-
-# knitr ====
 library(knitr)
+library(ggplot2)
+library(bcbioSinglecell)
 opts_chunk$set(
     audodep = TRUE,
     cache = TRUE,
     cache.lazy = FALSE,
-    dev = c("png", "pdf", "svg"),
     error = FALSE,
     fig.height = 8,
     fig.retina = 2,
@@ -26,9 +12,4 @@ opts_chunk$set(
     message = FALSE,
     tidy = TRUE,
     warning = FALSE)
-
-
-
-# ggplot2 ====
-library(ggplot2)
 theme_set(theme_light(base_size = 14))
