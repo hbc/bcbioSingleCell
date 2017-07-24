a2c7f23a8a666945042b8d3fd35e3fdac164f8fa
Michael Steinbaugh <mike@steinbaugh.com>
2017-07-24 14:04:14 -0400

    Update download files

--- docs/downloads/setup.R
+++ docs/downloads/setup.R
@@ -1,30 +1,17 @@
-# bcbioSinglecell ====
-library(bcbioSinglecell)
-if (file.exists("data/bcb.rda")) {
-    data(bcb)
-} else {
-    bcb <- load_run(
-        upload_dir = file.path("data", "singlecell"),
-        sample_metadata_file = file.path("meta", "sample_barcodes.xlsx"),
-        interesting_groups = "genotype")
-    save_data(bcb, compress = FALSE)
-}
-
-# knitr ====
 library(knitr)
+library(ggplot2)
+library(bcbioSinglecell)
+
 opts_chunk$set(
     audodep = TRUE,
     cache = TRUE,
     cache.lazy = FALSE,
-    dev = c("png", "pdf", "svg"),
     error = FALSE,
-    fig.height = 7,
+    fig.height = 8,
     fig.retina = 2,
-    fig.width = 7,
+    fig.width = 8,
     message = FALSE,
     tidy = TRUE,
     warning = FALSE)
 
-# ggplot2 ====
-library(ggplot2)
 theme_set(theme_light(base_size = 14))
