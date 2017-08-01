a12746c195a73df640b0d98512ab6e6d025f7111
Michael Steinbaugh <mike@steinbaugh.com>
2017-08-01 16:38:26 -0400

    TEMP fix for Seurat issue #111

--- R/methods-plotClusters.R
+++ R/methods-plotClusters.R
@@ -8,10 +8,22 @@
 #' @return No value, only graphical output.
 #' @export
 setMethod("plotClusters", "seurat", function(object, symbols) {
-    VlnPlot(object,
-            features.plot = symbols,
-            use.scaled = TRUE)
-    FeaturePlot(object,
-                features.plot = symbols,
-                cols.use = c("grey", "blue"))
+    # TEMP Fix for Seurat issue #111
+    if (any(grepl("\\-", symbols))) {
+        warning("Seurat v2 fails on symbols starting with hyphen or number",
+                call. = FALSE)
+        symbols <- symbols %>%
+            .[!grepl("\\-", .)]
+        if (!length(symbols)) return(NULL)
+    }
+    VlnPlot(
+        object,
+        features.plot = symbols,
+        nCol = 2L,
+        x.lab.rot = TRUE)
+    FeaturePlot(
+        object,
+        features.plot = symbols,
+        cols.use = c("grey", "blue"),
+        nCol = 2L)
 })
