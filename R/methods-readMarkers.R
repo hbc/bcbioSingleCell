9eac2649a853aaa5c970c11f773eee9bb39f9239
Michael Steinbaugh <mike@steinbaugh.com>
2017-08-09 14:28:21 -0400

    Rework `readMarkers()` function

--- R/methods-readMarkers.R
+++ R/methods-readMarkers.R
@@ -16,31 +16,68 @@ NULL
 #' @rdname readMarkers
 #' @export
 setMethod("readMarkers", "character", function(
-    object, genomeBuild, show = FALSE) {
-    annotable <- annotable(genomeBuild)
+    object,
+    gene2symbol,
+    show = FALSE) {
+    if (!is.data.frame(gene2symbol)) {
+        stop("gene2symbol must be data.frame")
+    }
+    if (length(dimnames(gene2symbol)[[2L]]) != 2L) {
+        stop("gene2symbol must only contain two columns")
+    }
+    if (!identical(
+        colnames(gene2symbol),
+        c("ensgene", "symbol"))) {
+        stop("gene2symbol colnames must be 'ensgene', 'symbol'")
+    }
+
     markers <- readFileByExtension(object) %>%
         camel
-    # Ensure Ensemble gene identifiers are not set
-    markers[["ensgene"]] <- NULL
-    markers <- markers %>%
-        # Remove rows that don't contain a symbol to map
-        .[!is.na(.[["symbol"]]), ] %>%
-        left_join(annotable, by = "symbol")
-    if (any(is.na(markers[["ensgene"]]))) {
-        missing <- markers %>%
-            .[is.na(.[["ensgene"]]), ] %>%
-            pull("symbol") %>%
-            sort %>%
-            unique
-        stop(paste("Unmapped symbols:", toString(missing)))
+
+    # Match the markers file by Ensembl gene identifier, otherwise symbol
+    if ("ensgene" %in% colnames(markers)) {
+        message("Matching by gene identifier")
+        markers <- markers %>%
+            .[, c("cellType", "ensgene")] %>%
+            .[!is.na(.[["ensgene"]]), ] %>%
+            left_join(gene2symbol, by = "ensgene")
+        # Check for bad identifiers
+        if (any(is.na(markers[["symbol"]]))) {
+            missing <- markers %>%
+                .[is.na(.[["symbol"]]), ] %>%
+                pull("ensgene") %>%
+                sort %>%
+                unique
+            stop(paste("Bad genes:", toString(missing)))
+        }
+    } else if ("symbol" %in% colnames(markers)) {
+        message("Matching by gene symbol")
+        markers <- markers %>?%
+            .[, c("cellType", "symbol")] %>%
+            .[!is.na(.[["symbol"]]), ] %>%
+            left_join(gene2symbol, by = "symbol")
+        # Check for bad identifiers
+        if (any(is.na(markers[["ensgene"]]))) {
+            missing <- markers %>%
+                .[is.na(.[["ensgene"]]), ] %>%
+                pull("symbol") %>%
+                sort %>%
+                unique
+            stop(paste("Bad symbols:", toString(missing)))
+        }
+    } else {
+        stop("Marker file must contain 'ensgene' or 'symbol'")
     }
+
     markers <- markers %>%
         .[!is.na(.[["ensgene"]]), ] %>%
         .[, c("cellType", "symbol", "ensgene")] %>%
         arrange(!!!syms(c("cellType", "symbol"))) %>%
         distinct
+
     if (isTRUE(show)) {
         kable(markers, caption = "Known markers") %>% show
     }
+
     markers
 })
