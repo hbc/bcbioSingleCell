bebd621dda340dd56457075bf893ab064517eba3
Michael Steinbaugh <mike@steinbaugh.com>
2017-08-10 08:49:12 -0400

    Include a Markdown header for each gene by default

--- R/methods-plotClusters.R
+++ R/methods-plotClusters.R
@@ -4,6 +4,7 @@
 #' @name plotClusters
 #'
 #' @param symbols Character vector of gene symbols.
+#' @param markdown Include a Markdown header for each gene.
 #'
 #' @return No value, only graphical output.
 NULL
@@ -13,15 +14,29 @@ NULL
 # Methods ====
 #' @rdname plotClusters
 #' @export
-setMethod("plotClusters", "seurat", function(object, symbols) {
-    VlnPlot(
-        object,
-        features.plot = symbols,
-        nCol = 2L,
-        x.lab.rot = TRUE)
-    FeaturePlot(
-        object,
-        features.plot = symbols,
-        cols.use = c("grey", "blue"),
-        nCol = 2L)
+setMethod("plotClusters", "seurat", function(
+    object,
+    symbols,
+    markdown = TRUE) {
+    sapply(seq_along(symbols), function(a) {
+        if (isTRUE(markdown)) {
+            mdHeader(symbols[a], level = 4L)
+        }
+
+        # Violin plots
+        VlnPlot(
+            object,
+            do.return = FALSE,
+            features.plot = symbols[a],
+            x.lab.rot = TRUE) %>%
+            show
+
+        # tSNE color plots
+        FeaturePlot(
+            object,
+            do.return = FALSE,
+            features.plot = symbols[a],
+            cols.use = c("grey", "blue"))
+    }) %>%
+        invisible
 })
