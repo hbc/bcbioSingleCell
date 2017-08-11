e3ccb1bb73ee71882d17232a6edf8d1dea1bc26d
roryk <rory.kirchner@gmail.com>
2017-08-11 09:29:11 -0400

    Fix memory blowing up issue when plotting zeroes vs depth.

--- R/methods-plotZeroesVsDepth.R
+++ R/methods-plotZeroesVsDepth.R
@@ -12,15 +12,18 @@ NULL
 .plotZerosVsDepth <- function(object) {
     counts <- assay(object)
     metrics <- metrics(object)
+    # using a logical matrix is much faster than a == 0L comparison
+    present <- as(counts, "lgCMatrix")
+
     # The `counts == 0L` operation here blows up memory on large datasets
     df <- data.frame(
-        dropout = Matrix::colSums(counts == 0L) / nrow(counts),
+        dropout = (nrow(present) - Matrix::colSums(present)) / nrow(present),
         depth = Matrix::colSums(counts),
         fileName = metrics[["fileName"]])
     p <- ggplot(df,
            aes_(x = ~depth,
                 y = ~dropout * 100L)) +
-        geom_point(size = 0.8) +
+        geom_point(size = 0.8, alpha=0.3) +
         scale_x_log10() +
         labs(x = "library size",
              y = "% genes zero")
