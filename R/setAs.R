960b20fb68fc2057ca6a99a4db786f2b43567591
Michael Steinbaugh <mike@steinbaugh.com>
2017-08-24 10:40:38 -0400

    Initial support for monocle CellDataSet class coercion from bcbioSCFiltered class

--- R/setAs.R
+++ R/setAs.R
@@ -1,10 +1,39 @@
-# Seurat ====
+# monocle: CellDataSet class ====
+setAs("bcbioSCFiltered", "CellDataSet", function(from) {
+    # phenoData
+    pd <- colData(from) %>%
+        as.data.frame %>%
+        as("AnnotatedDataFrame")
+    # featureData
+    fd <- rowData(from) %>%
+        as.data.frame %>%
+        # monocle requires `gene_short_name` column for gene symbols
+        mutate(gene_short_name = .data[["symbol"]]) %>%
+        set_rownames(rownames(from)) %>%
+        as("AnnotatedDataFrame")
+    cds <- newCellDataSet(
+        cellData = assay(from),
+        phenoData = pd,
+        featureData = fd)
+
+    # Check to make sure cells or genes aren't dropped. Use `as.numeric()`
+    # conversion here because CellDataSet names the dimensions.
+    if (!identical(as.numeric(dim(from)), as.numeric(dim(cds)))) {
+        stop("Coercion to 'CellDataSet' class changed dimensions")
+    }
+
+    cds
+})
+
+
+
+# Seurat: seurat class ====
 # [test_seurat_object.R](https://goo.gl/GMQAdC)
-setAs("bcbioSCSubset", "seurat", function(from) {
+setAs("bcbioSCFiltered", "seurat", function(from) {
     project <- deparse(substitute(from))
     counts <- counts(from, gene2symbol = TRUE)
 
-    object <- Seurat::CreateSeuratObject(
+    object <- CreateSeuratObject(
         raw.data = counts,
         project = project,
         # We already prefiltered by gene level
@@ -15,7 +44,7 @@ setAs("bcbioSCSubset", "seurat", function(from) {
 
     # Integrity checks
     if (!identical(dim(counts), dim(object@data))) {
-        stop("Dimension mismatch between bcbioSCSubset and Seurat object")
+        stop("Dimension mismatch between bcbioSCFiltered and Seurat object")
     }
     if (!identical(colnames(counts), rownames(metrics))) {
         stop("Count matrix and metrics mismatch")
@@ -35,10 +64,10 @@ setAs("bcbioSCSubset", "seurat", function(from) {
     }
 
     # Rows must correspond to `object@cell.names`
-    object <- Seurat::AddMetaData(object, metrics)
+    object <- AddMetaData(object, metrics)
 
     # Normalize
-    object <- Seurat::NormalizeData(
+    object <- NormalizeData(
         object,
         normalization.method = "LogNormalize",
         scale.factor = 10000L)
