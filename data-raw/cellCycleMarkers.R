1675b75abbb538b9e4767e5af8a9bff71c1df38d
Michael Steinbaugh <mike@steinbaugh.com>
2017-09-09 11:31:38 -0400

    Finish marker migration to Google Sheets

--- data-raw/cellCycleMarkers.R
+++ data-raw/cellCycleMarkers.R
@@ -1,8 +1,12 @@
 # Cell-Cycle Markers
-# Last compiled on 2017-09-09 using Ensembl 90
+#
+# Last updated: 2017-09-09
+# Gene annotations: Ensembl Genes 90
+#
 # This code is derived from:
 #   - Tirosh et al, 2015
 #   - http://satijalab.org/seurat/cell_cycle_vignette.html
+
 library(devtools)
 library(googlesheets)
 library(tidyverse)
@@ -14,6 +18,7 @@ gs <- gs_key("1qA5ktYeimNGpZF1UPSQZATbpzEqgyxN6daoMOjv6YYw")
 
 # Get a list of the worksheets (ws)
 ws <- gs_ws_ls(gs)
+print(ws)
 
 cellCycleMarkers <- lapply(seq_along(ws), function(a) {
     # Stop instead of warn on gene identifier match failure
--- data-raw/cellTypeMarkers.R
+++ data-raw/cellTypeMarkers.R
@@ -1,35 +1,40 @@
 # Cell-Type Markers
-# Last compiled on September 7, 2017 using Ensembl 90 (annotables)
+#
+# Last updated: 2017-09-09
+# Gene annotations: Ensembl Genes 90
+
 library(devtools)
+library(googlesheets)
+library(stringr)
 library(tidyverse)
-library(readxl)
 
 load_all()
 
-# Store all the sheets in a list
-file <- file.path("data-raw", "cellTypeMarkers.xlsx")
-sheets <- c("hsapiens", "mmusculus")
+# Download the Google sheet (gs)
+gs <- gs_key("1vGNU2CCxpaoTCLvzOxK1hf5gjULrf2-CpgCp9bOfGJ0")
 
-cellTypeMarkers <- lapply(seq_along(sheets), function(a) {
-    organism <- sheets[[a]]
+# Get a list of the worksheets (ws)
+ws <- gs_ws_ls(gs) %>%
+    # Remove internal worksheets prefixed with "_"
+    .[!str_detect(., "^_")]
+print(ws)
+
+cellTypeMarkers <- lapply(seq_along(ws), function(a) {
     # Stop instead of warn on gene identifier match failure
     tryCatch(
-        read_excel(file, sheet = organism) %>%
+        gs %>%
+            gs_read(ws = ws[[a]]) %>%
             group_by(symbol) %>%
             nest %>%
-            as.data.frame %>%
-            set_rownames(.$symbol) %>%
-            symbol2gene(organism = organism) %>%
-            rownames_to_column("ensgene") %>%
-            as_tibble %>%
+            mutate(ensgene = symbol2gene(symbol, organism = ws[[a]])) %>%
             unnest %>%
-            select(cell, ensgene, symbol) %>%
+            select(cell, symbol, ensgene) %>%
             group_by(cell) %>%
             arrange(symbol, .by_group = TRUE),
         warning = function(w) {
             stop(w)
         })
 }) %>%
-    set_names(sheets)
+    set_names(ws)
 
 use_data(cellTypeMarkers, compress = "xz", overwrite = TRUE)
