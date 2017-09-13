4ef987d20043a1194541c59967e4bf6d9afbb6c0
Michael Steinbaugh <mike@steinbaugh.com>
2017-09-13 08:40:25 -0400

    Attempt to cache Bioconductor and SummarizedExperiment for Travis on bioc-release

--- R/bcbioSingleCell-package.R
+++ R/bcbioSingleCell-package.R
@@ -3,42 +3,7 @@
 #' Import and analyze [bcbio](http://bcbio-nextgen.readthedocs.io) single-cell
 #' RNA-seq data.
 #'
-#' @import methods
-#' @importClassesFrom Seurat seurat
-#' @importClassesFrom SummarizedExperiment SummarizedExperiment
-#' @importFrom basejump camel detectOrganism gene2symbolFromGTF mdHeader mdList
-#'   prepareSummarizedExperiment prepareTemplate readFileByExtension readGTF
-#'   revcomp tidy_filter tidy_select tx2geneFromGTF
-#' @importFrom cowplot draw_plot ggdraw plot_grid
-#' @importFrom dendsort dendsort
-#' @importFrom dplyr arrange bind_rows distinct everything group_by left_join
-#'   mutate n pull rename summarize top_n
-#' @importFrom ggplot2 aes_ coord_flip element_blank element_text expand_limits
-#'   facet_wrap geom_bar geom_boxplot geom_histogram geom_hline geom_label
-#'   geom_line geom_point geom_smooth geom_text geom_violin geom_vline ggplot
-#'   ggtitle labs qplot scale_x_log10 scale_x_sqrt scale_y_continuous
-#'   scale_y_log10 scale_y_sqrt theme unit xlab xlim ylab
-#' @importFrom jsonlite read_json
-#' @importFrom knitr kable
-#' @importFrom magrittr %>% set_colnames set_rownames
-#' @importFrom Matrix cBind
-#' @importFrom Matrix.utils aggregate.Matrix
-#' @importFrom pbapply pblapply pbsapply
-#' @importFrom pheatmap pheatmap
-#' @importFrom readr read_csv read_delim read_lines
-#' @importFrom rlang .data is_string sym syms
-#' @importFrom S4Vectors metadata SimpleList
-#' @importFrom scales math_format percent trans_breaks trans_format
-#' @importFrom Seurat AddMetaData CreateSeuratObject DotPlot FeaturePlot
-#'   FindVariableGenes JoyPlot NormalizeData ScaleData VlnPlot
-#' @importFrom stats median
-#' @importFrom stringr str_detect str_match str_replace str_replace_all
-#'   str_split str_subset
-#' @importFrom SummarizedExperiment assay colData rowData
-#' @importFrom tibble column_to_rownames remove_rownames rownames_to_column
-#'   tibble
-#' @importFrom utils globalVariables methods
-#' @importFrom viridis inferno scale_color_viridis scale_fill_viridis viridis
+#' @import basejump methods SummarizedExperiment
 "_PACKAGE"
 
 globalVariables(".")
@@ -56,7 +21,7 @@ qcLabelAlpha <- 0.75
 qcLabelColor <- "white"
 qcLabelFill <- "black"
 qcLabelFontface <- "bold"
-qcLabelPadding <- unit(0.2, "lines")
+qcLabelPadding <- ggplot2::unit(0.2, "lines")
 qcLabelSize <- NA
 
 # Quality control line appearance
