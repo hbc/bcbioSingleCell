145758682511948d5ab415f00a2b262c9d326e97
roryk <rory.kirchner@gmail.com>
2017-09-15 12:12:31 -0400

    Add ability to color by expression. Add examples, change color to pink.

--- R/seurat-tsne.R
+++ R/seurat-tsne.R
@@ -32,22 +32,37 @@ FetchTsneExpressionData = function(seurat, genes) {
     tibble::rownames_to_column("cell")
   FetchTsneData(seurat) %>%
     left_join(dat) %>%
-    tidyr::gather(gene, expresssion, !!genes)
+    tidyr::gather(gene, expression, !!genes)
 }
 
 ##' Plot fetched t-SNE expression data
 ##'
 ##' @param df a dataframe from FetchTsneExpressionData
+##' @param colorpoints color points by geometric mean or expression of
+##'  individual gene
 ##' @return ggplot2 t-SNE plot
-##' @author Rory Kirchner
-PlotTsneExpressionData = function(df) {
-  ggplot(df, aes(tSNE_1, tSNE_2, color=geomean)) +
-    geom_text(aes(tSNE_1, tSNE_2, label=seurat.ident, color=geomean), size=2) +
+##' @examples
+##' \dontrun{
+##'   dat = FetchTsneExpressionData(seurat, topGenes)
+##'   # to make t-SNE plot colored by geometric mean of topGenes
+##'   PlotTsneExpressionData(dat, colorpoints="geometricmean")
+##'   # to make faceted t-SNE plot of each gene (looks good at up to 6 genes)
+##'   PlotTsneExpression(dat, colorpoints="expression") +
+##'     facet_wrap(~gene)
+##' } @author Rory Kirchner
+PlotTsneExpressionData = function(df, colorpoints="geomean") {
+  ggplot(df, aes_string("tSNE_1", "tSNE_2", color=colorpoints)) +
+    geom_text(aes_string("tSNE_1", "tSNE_2", label="seurat.ident",
+                         color=colorpoints),
+              size=2) +
     scale_color_viridis() +
     guides(color=FALSE) +
-    geom_text(aes(x=centerx, y=centery, label=seurat.ident), color="red") +
+    geom_text(aes(x=centerx, y=centery, label=seurat.ident), color="pink", size=3,
+              fontface="bold") +
     xlab("") +
     ylab("") +
     DarkTheme() +
-    theme(text=element_text(size=8))
+    theme(strip.background=element_rect(fill="black"),
+          strip.text = element_text(face="bold"),
+          text=element_text(size=8))
 }
