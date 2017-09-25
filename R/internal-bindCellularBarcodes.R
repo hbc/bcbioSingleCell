41b8304e6237d0d69a9c75e7b4ccb63cf90827d3
Michael Steinbaugh <mike@steinbaugh.com>
2017-09-25 08:41:50 -0400

    Rename `.bindCB()` to `.bindCellularBarcodes()`

--- R/internal-bindCellularBarcodes.R
+++ R/internal-bindCellularBarcodes.R
@@ -1,17 +1,18 @@
 #' Bind Cellular Barcodes
 #'
-#' @keywords internal
+#' @author Michael Steinbaugh
 #'
 #' @param list List of cellular barcodes.
 #'
 #' @return [tibble].
-.bindCB <- function(list) {
+#' @noRd
+.bindCellularBarcodes <- function(list) {
     lapply(seq_along(list), function(a) {
         sampleID <- names(list)[[a]]
         list[[a]] %>%
             mutate(sampleID = !!sampleID)
     }) %>%
-        bind_rows %>%
+        bind_rows() %>%
         as("tibble") %>%
         mutate(rowname = paste(.data[["sampleID"]],
                                .data[["cellularBarcode"]],
