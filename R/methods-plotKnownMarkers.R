9a05ee483d620ba8493e6e6e1395db9ce709c1e9
Michael Steinbaugh <mike@steinbaugh.com>
2017-09-25 10:57:19 -0400

    Rename `symbols` to `genes`

--- R/methods-plotKnownMarkers.R
+++ R/methods-plotKnownMarkers.R
@@ -22,24 +22,24 @@ NULL
     }
     cellTypes <- knownMarkers %>%
         pull("cell") %>%
-        unique
+        unique()
     pblapply(seq_along(cellTypes), function(a) {
         cellType <- cellTypes[[a]]
-        symbols <- knownMarkers %>%
+        genes <- knownMarkers %>%
             dplyr::filter(.data[["cell"]] == !!cellType) %>%
             pull("symbol") %>%
-            unique %>%
-            sort
-        if (!is.null(symbols)) {
+            unique() %>%
+            sort()
+        if (!is.null(genes)) {
             mdHeader(cellType, level = headerLevel, tabset = TRUE, asis = TRUE)
             plotMarkers(object,
-                        symbols = symbols,
+                        genes = genes,
                         headerLevel = headerLevel + 1L)
         } else {
             NULL
         }
     }) %>%
-        invisible
+        invisible()
 }
 
 
