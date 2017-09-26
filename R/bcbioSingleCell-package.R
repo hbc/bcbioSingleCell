fd8654af0ebd4b4c7719a9d6791dcf92418bec00
Michael Steinbaugh <mike@steinbaugh.com>
2017-09-26 12:14:26 -0400

    Move ggplot2 style settings to `internal-ggplot2.R` file

--- R/bcbioSingleCell-package.R
+++ R/bcbioSingleCell-package.R
@@ -50,33 +50,5 @@
 NULL
 
 globalVariables(".")
-
-bins <- 100L
-
-# Quality control plot colors
-qcBorderColor <- "black"
-qcCutoffColor <- "black"
-qcFailColor <- "red"
-qcPassColor <- "green"
-qcWarnColor <- "orange"
-
-# Quality control label appearance
-qcLabelAlpha <- 0.75
-qcLabelColor <- "white"
-qcLabelFill <- "black"
-qcLabelFontface <- "bold"
-qcLabelPadding <- unit(0.2, "lines")
-qcLabelSize <- NA
-
-# Quality control line appearance
-qcLineAlpha <- 0.75
-qcLineSize <- 1L
-qcLineType <- "dashed"
-
-# Plot label separator
-labelSep <- ": "
-
 metaPriorityCols <- c("sampleID", "sampleName", "fileName")
 projectDirPattern <- "^(\\d{4}-\\d{2}-\\d{2})_([^/]+)$"
-
-url <- "http://bioinformatics.sph.harvard.edu/bcbioSingleCell"
