b7715f6fc3d6a1219dc271d8e5f9172060f5a5e1
Michael Steinbaugh <mike@steinbaugh.com>
2017-09-26 00:26:54 -0400

    Improve checks

--- R/internal-validMarkers.R
+++ R/internal-validMarkers.R
@@ -1,7 +1,12 @@
 .validMarkers <- function(object) {
-    # Check for `sanitizedMarkers()` return
-    if (attributes(object)[["vars"]] != "cluster" |
-        !"ensgene" %in% colnames(object)) {
-        stop("'sanitizeMarkers()' return is required")
+    if (!is(object, "grouped_df")) {
+        stop("Object must be 'grouped_df' class")
     }
+    if (attributes(object)[["vars"]] != "cluster") {
+        stop("Object must be grouped by 'cluster'")
+    }
+    if (!"ensgene" %in% colnames(object)) {
+        stop("Object missing 'ensgene' column")
+    }
+    TRUE
 }
