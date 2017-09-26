38205ca55c68031d293ec44485dd7b159555da5e
Michael Steinbaugh <mike@steinbaugh.com>
2017-09-25 23:11:34 -0400

    Improve constructor function for seurat

--- R/methods-sanitizeMarkers.R
+++ R/methods-sanitizeMarkers.R
@@ -13,33 +13,33 @@ NULL
 # Constructors ====
 .sanitizeMarkersSeurat <- function(
     object,
-    gene2symbol) {
-    # Check for previously sanitized object and early return, if detected. This
+    markers) {
+    # Check for previously sanitized markers and early return, if detected. This
     # function groups by `cluster` ident and ensures Ensembl gene identifiers
     # are added in the `ensgene` column, so this step checks for these.
-    if (is(object, "grouped_df") &
-        attributes(object)[["vars"]] == "cluster" &
-        "ensgene" %in% colnames(object)) {
-        return(object)
+    if (is(markers, "grouped_df") &
+        attributes(markers)[["vars"]] == "cluster" &
+        "ensgene" %in% colnames(markers)) {
+        return(markers)
     }
 
     # Check for original `Seurat::FindAllMarkers()` return
     seuratCols <- c("p_val", "avg_diff", "pct.1", "pct.2", "cluster", "gene")
-    if (all(seuratCols %in% colnames(object))) {
+    if (all(seuratCols %in% colnames(markers))) {
         message("Sanitizing Seurat markers data.frame")
         # Rename specific columns prior to camelCase
-        if ("p_val" %in% colnames(object)) {
-            object <- dplyr::rename(object, pvalue = .data[["p_val"]])
+        if ("p_val" %in% colnames(markers)) {
+            markers <- dplyr::rename(markers, pvalue = .data[["p_val"]])
         }
-        if ("gene" %in% colnames(object)) {
-            object <- dplyr::rename(object, symbol = .data[["gene"]])
+        if ("gene" %in% colnames(markers)) {
+            markers <- dplyr::rename(markers, symbol = .data[["gene"]])
         }
     }
 
     # Sanitize column names into lowerCamelCase
-    object <- camel(object)
+    markers <- camel(markers)
     # Check for ensgene and add from `gene2symbol` if necessary
-    if (!"ensgene" %in% colnames(object)) {
+    if (!"ensgene" %in% colnames(markers)) {
         message("Adding missing Ensembl gene identifiers")
         # Use this data frame to convert the gene symbols ("gene" column) back to
         # Ensembl gene identifiers ("ensgene" column)
@@ -47,20 +47,20 @@ NULL
             ensgene = names(rownames(object@raw.data)),
             symbol = rownames(object@raw.data)
         )
-        object <- left_join(object, gene2symbol, by = "symbol")
+        markers <- left_join(markers, gene2symbol, by = "symbol")
         # Ensure all the symbols matched
-        if (any(is.na(object[["ensgene"]]))) {
+        if (any(is.na(markers[["ensgene"]]))) {
             stop("gene2symbol failed to match all genes")
         }
     }
 
     # Ensure that required columns are present
     requiredCols <- c("cluster", "ensgene", "pvalue", "symbol")
-    if (!all(requiredCols %in% colnames(object))) {
+    if (!all(requiredCols %in% colnames(markers))) {
         stop(paste("Marker data.frame must contain:", toString(requiredCols)))
     }
 
-    object %>%
+    markers %>%
         remove_rownames() %>%
         as("tibble") %>%
         camel() %>%
