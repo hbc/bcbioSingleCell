98089dc038b6c3c6f79e9bb3fb2fd1036ebdd372
Michael Steinbaugh <mike@steinbaugh.com>
2017-09-26 09:32:35 -0400

    Improve legacy class support

--- R/methods-bcbio.R
+++ R/methods-bcbio.R
@@ -14,7 +14,7 @@ NULL
 
 #' @rdname bcbio
 #' @export
-setMethod("bcbio", "bcbioSingleCellANY", function(object, type) {
+setMethod("bcbio", "bcbioSingleCell", function(object, type) {
     if (type %in% names(slot(object, "bcbio"))) {
         slot(object, "bcbio")[[type]]
     } else {
@@ -28,9 +28,35 @@ setMethod("bcbio", "bcbioSingleCellANY", function(object, type) {
 #' @export
 setMethod(
     "bcbio<-",
-    signature(object = "bcbioSingleCellANY", value = "ANY"),
+    signature(object = "bcbioSingleCell", value = "ANY"),
     function(object, type, value) {
         slot(object, "bcbio")[[type]] <- value
+        validObject(object)
+        object
+    })
+
+
+
+# Deprecated `bcbioSCDataSet` object support ====
+#' @rdname bcbio
+#' @export
+setMethod("bcbio", "bcbioSCDataSet", function(object, type) {
+    if (type %in% names(slot(object, "callers"))) {
+        slot(object, "callers")[[type]]
+    } else {
+        stop(paste(type, "not found"))
+    }
+})
+
+
+
+#' @rdname bcbio
+#' @export
+setMethod(
+    "bcbio<-",
+    signature(object = "bcbioSCDataSet", value = "ANY"),
+    function(object, type, value) {
+        slot(object, "callers")[[type]] <- value
         validObject(object)
         object
     })
