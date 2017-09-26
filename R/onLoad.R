3bc26db276346e0dc8da59739da82d2564a78cc9
Michael Steinbaugh <mike@steinbaugh.com>
2017-09-26 00:17:09 -0400

    Update onLoad to use SingleCellExperiment

--- R/onLoad.R
+++ R/onLoad.R
@@ -1,7 +1,7 @@
 # Follow the same order as `Depends:` in `DESCRIPTION` file
 .onLoad <- function(libname, pkgname) {
     pkgs <-
-        c("SummarizedExperiment",
+        c("SingleCellExperiment",
           "Seurat",
           "basejump")
     lapply(seq_along(pkgs), function(a) {
