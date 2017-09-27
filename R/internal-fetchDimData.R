a19a23dc6c020dc66f2425058bd4cb28be8311bd
Michael Steinbaugh <mike@steinbaugh.com>
2017-09-27 14:01:46 -0400

    Initial commit

---
+++ R/internal-fetchDimData.R
@@ -0,0 +1,29 @@
+#' Fetch Data
+#'
+#' @author Rory Kirchner, Michael Steinbaugh
+#'
+#' @param object [seurat] object.
+#' @param dimCode Character vector of X and Y coordinate data to be used for
+#'   plotting. This can be `c("tSNE_1", "tSNE_2")` for tSNE data, or `c("PC1",
+#'   "PC2")` for PCA data.
+#'
+#' @return [data.frame]
+#' @noRd
+.fetchDimDataSeurat <- function(object, dimCode) {
+    meta <- object@meta.data %>%
+        camel(strict = FALSE) %>%
+        rownames_to_column("cell")
+    ident <- object@ident %>%
+        as.data.frame() %>%
+        set_colnames("ident") %>%
+        rownames_to_column("cell")
+    FetchData(object, vars.all = dimCode) %>%
+        as.data.frame() %>%
+        camel(strict = FALSE) %>%
+        rownames_to_column("cell") %>%
+        left_join(meta, by = "cell") %>%
+        left_join(ident, by = "cell") %>%
+        group_by(!!sym("ident")) %>%
+        mutate(centerX = median(.data[[camel(dimCode[[1]], strict = FALSE)]]),
+               centerY = median(.data[[camel(dimCode[[2]], strict = FALSE)]]))
+}
