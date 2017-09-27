cfa999cc8cab0453217ca6524b818265f68a4d26
Michael Steinbaugh <mike@steinbaugh.com>
2017-09-27 15:02:05 -0400

    Update NAMESPACE

--- R/bcbioSingleCell-package.R
+++ R/bcbioSingleCell-package.R
@@ -20,12 +20,13 @@
 #' @importFrom dplyr arrange bind_rows distinct everything filter funs group_by
 #'   left_join mutate n pull rename select slice summarize summarize_all ungroup
 #' @importFrom ggridges geom_density_ridges
-#' @importFrom ggplot2 aes_ aes_string coord_flip element_blank element_rect
-#'   element_text expand_limits facet_wrap geom_bar geom_boxplot geom_histogram
-#'   geom_hline geom_label geom_line geom_point geom_smooth geom_text
-#'   geom_violin geom_vline ggplot ggtitle guide_colourbar guide_legend guides
-#'   labs qplot scale_colour_gradient scale_radius scale_x_log10 scale_x_sqrt
-#'   scale_y_continuous scale_y_log10 scale_y_sqrt theme unit xlab xlim ylab
+#' @importFrom ggplot2 aes_ aes_string coord_flip element_blank element_line
+#'   element_rect element_text expand_limits facet_wrap geom_bar geom_boxplot
+#'   geom_histogram geom_hline geom_label geom_line geom_point geom_smooth
+#'   geom_text geom_violin geom_vline ggplot ggtitle guide_colourbar
+#'   guide_legend guides labs qplot scale_colour_gradient scale_radius
+#'   scale_x_log10 scale_x_sqrt scale_y_continuous scale_y_log10 scale_y_sqrt
+#'   theme unit xlab xlim ylab
 #' @importFrom jsonlite read_json
 #' @importFrom magrittr %>% set_colnames set_rownames
 #' @importFrom Matrix cBind
