1d8bb82fedd364b9c8e25bafeed965c05e9f69e2
Michael Steinbaugh <mike@steinbaugh.com>
2017-10-03 17:19:01 -0400

    Improve label appearance

--- R/methods-plotUMIsPerCell.R
+++ R/methods-plotUMIsPerCell.R
@@ -19,30 +19,33 @@ NULL
     min = NULL,
     filterCells = FALSE) {
     metrics <- metrics(object, filterCells = filterCells)
-    medianUMIs <- aggregate(nUMI ~ sampleID, metrics, median) %>%
-        left_join(sampleMetadata(object), by = "sampleID") %>%
-        mutate(nUMI = round(.data[["nUMI"]]))
     p <- ggplot(metrics,
                 mapping = aes_string(
                     x = "sampleName",
                     y = "nUMI",
                     fill = interestingGroup)) +
-        labs(x = "sample",
-             y = "umis per cell") +
         geom_boxplot(color = lineColor) +
-        geom_label(
-            data = medianUMIs,
-            mapping = aes_string(label = "nUMI"),
-            alpha = qcLabelAlpha,
-            color = qcLabelColor,
-            fill = qcLabelFill,
-            fontface = qcLabelFontface,
-            label.padding = qcLabelPadding,
-            label.size = qcLabelSize,
-            show.legend = FALSE) +
         scale_y_log10() +
         scale_fill_viridis(discrete = TRUE) +
+        labs(x = "sample",
+             y = "umis per cell") +
         theme(axis.text.x = element_text(angle = 90, hjust = 1))
+    if (length(unique(metrics[["sampleName"]])) <= qcLabelMaxNum) {
+        medianUMIs <- aggregate(nUMI ~ sampleID, metrics, median) %>%
+            left_join(sampleMetadata(object), by = "sampleID") %>%
+            mutate(nUMI = round(.data[["nUMI"]]))
+        p <- p +
+            geom_label(
+                data = medianUMIs,
+                mapping = aes_string(label = "nUMI"),
+                alpha = qcLabelAlpha,
+                color = qcLabelColor,
+                fill = qcLabelFill,
+                fontface = qcLabelFontface,
+                label.padding = qcLabelPadding,
+                label.size = qcLabelSize,
+                show.legend = FALSE)
+    }
     if (!is.null(min)) {
         p <- p +
             .qcCutoffLine(yintercept = min)
