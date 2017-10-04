f694d8e85327697a2d3874d1ce3d8a90c238c753
Michael Steinbaugh <mike@steinbaugh.com>
2017-10-04 18:13:59 -0400

    Add `aggregateReplicates` argument, which simply sets `sampleName` column from `sampleNameAggregate`

--- R/methods-metrics.R
+++ R/methods-metrics.R
@@ -6,7 +6,10 @@
 #' @author Michael Steinbaugh, Rory Kirchner
 #'
 #' @inheritParams AllGenerics
-#' @param filterCells Only return cells that pass filtering criteria.
+#' @param filterCells Show only the cells that have passed filtering cutoffs.
+#' @param aggregateReplicates Aggregate technical replicates, if present. If
+#'   `TRUE`, this function uses the values slotted in
+#'   `sampleMetadata(object)[["sampleNameAggregate"]])`.
 #'
 #' @return [data.frame] with cellular barcodes as rows.
 NULL
@@ -21,10 +24,23 @@ NULL
 #'
 #' @return [data.frame].
 #' @noRd
-.metricsBCB <- function(object) {
+.metrics <- function(object, aggregateReplicates = FALSE) {
     colData <- colData(object) %>%
         as.data.frame() %>%
         rownames_to_column("cellID")
+    meta <- sampleMetadata(object)
+
+    # Rename `sampleName` when aggregating replicates
+    if (isTRUE(aggregateReplicates)) {
+        if (!"sampleNameAggregate" %in% colnames(meta)) {
+            stop(paste(
+                "'sampleNameAggregate' column must be set in sample metadata",
+                "to aggregate replicates"
+            ))
+        }
+        meta[["sampleName"]] <- meta[["sampleNameAggregate"]]
+        meta[["sampleNameAggregate"]] <- NULL
+    }
 
     # Get the barcode format based on the umiType
     umiType <- metadata(object)[["umiType"]]
@@ -47,13 +63,10 @@ NULL
         # Remove the unnecessary cellularBarcode column
         mutate(cellularBarcode = NULL)
 
-    # In a future update, attempt to detect duplicate columns present in colData
-    # and sampleMetadata. We may want to remove the duplicated columns present
-    # in the sampleMetadata before joining with the colData.
-
     # Join the sample metadata by sampleID, extracted from the barcode ID
-    left_join(colData, match, by = "cellID") %>%
-        left_join(sampleMetadata(object), by = "sampleID") %>%
+    colData %>%
+        left_join(match, by = "cellID") %>%
+        left_join(meta, by = "sampleID") %>%
         as.data.frame() %>%
         column_to_rownames("cellID")
 }
@@ -72,14 +85,14 @@ setMethod("metrics", "bcbioSingleCell", function(
             object <- object[, cells]
         }
     }
-    .metricsBCB(object)
+    .metrics(object, aggregateReplicates = aggregateReplicates)
 })
 
 
 
 #' @rdname metrics
 #' @export
-setMethod("metrics", "bcbioSingleCellLegacy", .metricsBCB)
+setMethod("metrics", "bcbioSingleCellLegacy", .metrics)
 
 
 
@@ -87,5 +100,6 @@ setMethod("metrics", "bcbioSingleCellLegacy", .metricsBCB)
 #' @export
 setMethod("metrics", "seurat", function(object) {
     object@data.info %>%
-        camel()
+        camel(strict = FALSE) %>%
+        .metrics(aggregateReplicates = FALSE)
 })
