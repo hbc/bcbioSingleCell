cbe0d62691e45a12aff55aa102fad56593351b25
Michael Steinbaugh <mike@steinbaugh.com>
2017-10-05 09:40:26 -0400

    Add support for `aggregateReplicates` argument

--- R/methods-plotUMIsVsGenes.R
+++ R/methods-plotUMIsVsGenes.R
@@ -11,27 +11,50 @@ NULL
 
 
 # Constructors ====
-.plotUMIsVsGenes <- function(object) {
-    metrics <- metrics(object)
+.plotUMIsVsGenes <- function(
+    object,
+    interestingGroup = "sampleName",
+    filterCells = FALSE,
+    aggregateReplicates = FALSE) {
+    metrics <- metrics(
+        object,
+        filterCells = filterCells,
+        aggregateReplicates = aggregateReplicates)
     p <- ggplot(
         metrics,
         mapping = aes_string(
             x = "nUMI",
             y = "nGene",
-            color = "sampleName")
+            color = interestingGroup)
     ) +
         labs(x = "umis per cell",
              y = "genes per cell") +
-        geom_point(size = 0.8, alpha = 0.25) +
-        geom_smooth(method = "gam", se = FALSE) +
+        geom_point(alpha = 0.25, size = 0.8) +
+        geom_smooth(method = "gam", se = FALSE, size = 2) +
         scale_x_log10() +
         scale_y_log10() +
         scale_color_viridis(discrete = TRUE) +
         theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+    # Facets
+    facets <- NULL
     if (isTRUE(metadata(object)[["multiplexedFASTQ"]])) {
-        p <- p +
-            facet_wrap(~fileName)
+        facets <- c(facets, "fileName")
     }
+    if (!isTRUE(aggregateReplicates) &
+        "sampleNameAggregate" %in% colnames(metrics)) {
+        facets <- c(facets, "sampleNameAggregate")
+        if (interestingGroup == "sampleName") {
+            p <- p +
+                theme(legend.position = "none")
+        }
+    }
+    if (!is.null(facets)) {
+        p <- p +
+            facet_wrap(facets = facets,
+                       scales = "free_x")
+    }
+
     p
 }
 
