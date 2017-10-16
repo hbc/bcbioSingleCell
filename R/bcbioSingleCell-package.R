a38b6e4a06655d1a0f267af610d2c537b6eef3df
Michael Steinbaugh <mike@steinbaugh.com>
2017-10-16 11:51:54 -0400

    Break out package imports to each function

--- R/bcbioSingleCell-package.R
+++ R/bcbioSingleCell-package.R
@@ -12,42 +12,7 @@
 #' @importClassesFrom Seurat seurat
 #' @importClassesFrom SummarizedExperiment SummarizedExperiment
 #'
-#' @importFrom BiocGenerics plotPCA
-#' @importFrom cowplot draw_plot ggdraw plot_grid
-#' @importFrom dendsort dendsort
-#' @importFrom devtools session_info
-#' @importFrom dplyr arrange bind_rows distinct everything filter funs group_by
-#'   left_join mutate n pull rename select slice summarize summarize_all ungroup
-#' @importFrom ggridges geom_density_ridges
-#' @importFrom ggplot2 aes_ aes_string coord_flip element_blank element_line
-#'   element_rect element_text expand_limits facet_wrap geom_bar geom_boxplot
-#'   geom_histogram geom_hline geom_label geom_line geom_point geom_smooth
-#'   geom_text geom_violin geom_vline ggplot ggtitle guide_colorbar
-#'   guide_legend guides labs qplot scale_color_gradient scale_radius
-#'   scale_x_log10 scale_x_sqrt scale_y_continuous scale_y_log10 scale_y_sqrt
-#'   theme unit xlab xlim ylab
-#' @importFrom jsonlite read_json
-#' @importFrom magrittr %>% set_colnames set_rownames
-#' @importFrom Matrix cBind
-#' @importFrom Matrix.utils aggregate.Matrix
-#' @importFrom parallel mclapply
-#' @importFrom pbapply pblapply pbsapply
-#' @importFrom pheatmap pheatmap
-#' @importFrom readr read_csv read_delim read_lines
-#' @importFrom rlang .data is_string sym syms
-#' @importFrom S4Vectors metadata SimpleList
-#' @importFrom scales math_format percent trans_breaks trans_format
-#' @importFrom Seurat AddMetaData CreateSeuratObject DarkTheme DotPlot
-#'   FeaturePlot FetchData FindVariableGenes JoyPlot NormalizeData ScaleData
-#'   VlnPlot
-#' @importFrom stats median
-#' @importFrom stringr str_detect str_match str_replace str_replace_all
-#'   str_split str_subset str_trunc
-#' @importFrom tibble column_to_rownames remove_rownames rownames_to_column
-#'   tibble
-#' @importFrom tidyr gather
-#' @importFrom utils globalVariables methods
-#' @importFrom viridis inferno scale_color_viridis scale_fill_viridis viridis
+#' @importFrom magrittr %>%
 NULL
 
 globalVariables(".")
