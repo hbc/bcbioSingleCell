d0f1c5ac65d116198dca6e4216fb566c19c8ceb6
Michael Steinbaugh <mike@steinbaugh.com>
2017-10-24 10:10:13 -0400

    Add back ggplot imports until we can add per file

--- R/internal-ggplot2.R
+++ R/internal-ggplot2.R
@@ -1,10 +1,20 @@
-#' @importFrom ggplot2 unit
+#' @importFrom ggplot2 aes_ aes_string coord_flip element_blank element_line
+#'   element_rect element_text expand_limits facet_wrap geom_bar geom_boxplot
+#'   geom_histogram geom_hline geom_label geom_line geom_point geom_smooth
+#'   geom_text geom_violin geom_vline ggplot ggtitle guide_colorbar
+#'   guide_legend guides labs qplot scale_color_gradient scale_radius
+#'   scale_x_log10 scale_x_sqrt scale_y_continuous scale_y_log10 scale_y_sqrt
+#'   theme xlab xlim ylab
+
+
+
+#' @importFrom grid unit
 #' @importFrom viridis inferno
 
 bins <- 200
 
 lineColor <- "black"
-qcColors <- viridis::inferno(3)
+qcColors <- inferno(3)
 
 # Quality control plot colors
 qcPassColor <- qcColors[[1]]
@@ -20,7 +30,7 @@ qcLabelAlpha <- 0.75
 qcLabelColor <- "white"
 qcLabelFill <- "black"
 qcLabelFontface <- "bold"
-qcLabelPadding <- ggplot2::unit(0.2, "lines")
+qcLabelPadding <- unit(0.2, "lines")
 qcLabelSize <- NA
 
 # Maximum number of samples to label in a bar or boxplot
@@ -54,6 +64,6 @@ labelSep <- ": "
             size = qcLineSize,
             yintercept = yintercept)
     } else {
-        stop("xintercept or yintercept required")
+        stop("xintercept or yintercept required", call. = FALSE)
     }
 }
