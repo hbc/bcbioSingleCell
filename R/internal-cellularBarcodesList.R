33315e601356c24961710ce25fb9c9cecd13de76
Michael Steinbaugh <mike@steinbaugh.com>
2017-10-24 10:09:16 -0400

    Improve error message

--- R/internal-cellularBarcodesList.R
+++ R/internal-cellularBarcodesList.R
@@ -15,7 +15,7 @@
         file.path(paste(basename(.), "barcodes.tsv", sep = "-")) %>%
         setNames(names(sampleDirs))
     if (!all(file.exists(files))) {
-        stop("Cellular barcode file missing")
+        stop("Cellular barcode file missing", call. = FALSE)
     }
     message("Reading cellular barcode distributions")
     pblapply(seq_along(files), function(a) {
