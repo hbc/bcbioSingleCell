544a5c757b5fa337a27c2b3acf0207c71707e64e
Michael Steinbaugh <mike@steinbaugh.com>
2017-10-24 14:28:44 -0400

    Initial commit of clustering per sample script

---
+++ inst/R_scripts/clustering_per_sample.R
@@ -0,0 +1,28 @@
+library(pbapply)
+library(rmarkdown)
+source("setup.R")
+loadData(bcb)
+
+sampleIDs <- sampleMetadata(bcb) %>% pull("sampleID")
+sampleSubsets <- pblapply(seq_along(sampleIDs), function(a) {
+    sampleID <- sampleIDs[[a]]
+    subset <- selectSamples(bcb, sampleID = sampleID)
+    assignAndSaveData(name = sampleID, object = subset)
+    sampleID
+}) %>%
+    unlist()
+saveData(sampleSubsets)
+
+# Render RMarkdown reports per bcbioSingleCell subset file
+pblapply(seq_along(sampleSubsets), function(a) {
+    bcbName <- sampleSubsets[[a]]
+    bcbFile <- paste0("data/", bcbName, ".rda")
+    seuratName <- paste(bcbName, "seurat", sep = "_")
+    render(input = "seurat.Rmd",
+           output_file = paste0(bcbName, ".html"),
+           output_format = "html_document",
+           params = list(
+               bcbFile = bcbFile,
+               seuratName = seuratName
+           ))
+})
