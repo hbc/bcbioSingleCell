eb33cd426fb79bd9a67e6232ccee01dba606595b
Michael Steinbaugh <mike@steinbaugh.com>
2017-10-24 10:21:15 -0400

    Improve tidyverse imports

--- R/methods-topBarcodes.R
+++ R/methods-topBarcodes.R
@@ -22,19 +22,20 @@ NULL
 
 # Constructors ====
 #' @importFrom dplyr slice
-#' @importFrom tibble column_to_rownames
+#' @importFrom tibble as_tibble column_to_rownames rownames_to_column
 .topBarcodes <- function(object, n = 10) {
     metrics <- metrics(object) %>%
-        as("tibble")
+        rownames_to_column() %>%
+        as_tibble()
     # Check for unfiltered barcode counts in `nCount`
     if (!"nCount" %in% colnames(metrics)) {
-        warning("'nCount' missing from 'metrics()'")
+        warning("'nCount' missing from 'metrics()'", call. = FALSE)
         return(NULL)
     }
     metrics %>%
         .[order(.[["nCount"]], decreasing = TRUE), , drop = FALSE] %>%
         # Take the top rows by using slice
-        dplyr::slice(1:n) %>%
+        slice(1:n) %>%
         as.data.frame() %>%
         column_to_rownames()
 }
