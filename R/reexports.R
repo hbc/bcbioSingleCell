1b525cb699c5b29b65a3e70cbbfaece9bb86eef1
Michael Steinbaugh <mike@steinbaugh.com>
2017-10-24 10:08:04 -0400

    Reexport viridis color palettes, for use in documentation

---
+++ R/reexports.R
@@ -0,0 +1,13 @@
+#' @importFrom viridis inferno scale_color_viridis scale_fill_viridis viridis
+
+#' @export
+viridis::inferno
+
+#' @export
+viridis::scale_color_viridis
+
+#' @export
+viridis::scale_fill_viridis
+
+#' @export
+viridis::viridis
