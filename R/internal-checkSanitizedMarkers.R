a06788cd81cc211ea3ef3ab17b18f39840b07473
Michael Steinbaugh <mike@steinbaugh.com>
2017-10-25 15:01:42 -0400

    Add code checks from `.validMarkers()`

--- R/internal-checkSanitizedMarkers.R
+++ R/internal-checkSanitizedMarkers.R
@@ -14,6 +14,18 @@
     object,
     package = "Seurat",
     stop = FALSE) {
+    # General checks
+    if (!is(object, "grouped_df")) {
+        stop("Object must be 'grouped_df' class", call. = FALSE)
+    }
+    if (attributes(object)[["vars"]] != "cluster") {
+        stop("Object must be grouped by 'cluster'", call. = FALSE)
+    }
+    if (!"ensgene" %in% colnames(object)) {
+        stop("Object missing 'ensgene' column", call. = FALSE)
+    }
+
+    # Package-specific checks
     if (package == "Seurat") {
         # Check for `Seurat::FindAllMarkers()` return.
         # These columns are output in an inconsistent format, so we'll sanitize
