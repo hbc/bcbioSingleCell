d48a401ddeb6220fab45115bb936b48c9e444e6b
Michael Steinbaugh <mike@steinbaugh.com>
2017-10-26 12:12:59 -0400

    Add dark mode support

--- R/methods-plotKnownMarkers.R
+++ R/methods-plotKnownMarkers.R
@@ -18,6 +18,8 @@ NULL
 .plotKnownMarkers <- function(
     object,
     knownMarkers,
+    color = scale_color_viridis(option = "inferno"),
+    dark = TRUE,
     headerLevel = 2) {
     if (nrow(knownMarkers) == 0) {
         return(NULL)
@@ -34,7 +36,12 @@ NULL
             sort()
         if (!is.null(genes)) {
             mdHeader(cellType, level = headerLevel, tabset = TRUE, asis = TRUE)
-            plotMarkers(object, genes = genes, headerLevel = headerLevel + 1)
+            plotMarkers(
+                object,
+                genes = genes,
+                color = color,
+                dark = dark,
+                headerLevel = headerLevel + 1)
             # Show is already declared in `plotMarkers()`
         }
     }) %>%
