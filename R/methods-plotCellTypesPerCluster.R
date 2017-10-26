545c802ebb87458697ac4e5eb354848f9f3714ee
Michael Steinbaugh <mike@steinbaugh.com>
2017-10-26 11:56:55 -0400

    Improve Markdown headers for cluster looping

--- R/methods-plotCellTypesPerCluster.R
+++ R/methods-plotCellTypesPerCluster.R
@@ -32,24 +32,39 @@ NULL
     object,
     cellTypesPerCluster,
     headerLevel = 2) {
-    pblapply(seq_len(nrow(cellTypesPerCluster)), function(a) {
-        cellType <- cellTypesPerCluster[a, ]
-        genes <- pull(cellType, "symbol") %>%
-            str_split(", ") %>%
-            .[[1]]
-        title <- paste(
-            paste0("Cluster ", pull(cellType, "cluster"), ":"),
-            pull(cellType, "cell")
-        )
-        mdHeader(title, level = headerLevel, tabset = TRUE, asis = TRUE)
-        plotMarkerTSNE(
-            object = object,
-            genes = genes,
-            colorPoints = "geomean",
-            pointsAsNumbers = FALSE,
-            label = TRUE,
-            title = title) %>%
-            show()
+    # Output Markdown headers per cluster
+    clusters <- unique(cellTypesPerCluster[["cluster"]])
+    pblapply(seq_along(clusters, function(a) {
+        cluster <- clusters[[a]]
+        mdHeader(
+            paste("Cluster", cluster),
+            level = headerLevel,
+            tabset = TRUE,
+            asis = TRUE)
+        subset <- filter(cellTypesPerCluster, .data[["cluster"]] == cluster)
+        pblapply(seq_len(nrow(subset)), function(b) {
+            cellType <- subset[b, ]
+            genes <- pull(cellType, "symbol") %>%
+                str_split(", ") %>%
+                .[[1]]
+            title <- pull(cellType, "cell")
+            mdHeader(
+                title,
+                level = headerLevel + 1,
+                tabset = TRUE,
+                asis = TRUE)
+            # Modify the title by adding the cluster number (for the plot)
+            title <- paste(paste0("Cluster ", cluster, ":"), title)
+            plotMarkerTSNE(
+                object = object,
+                genes = genes,
+                colorPoints = "geomean",
+                pointsAsNumbers = FALSE,
+                label = TRUE,
+                title = title) %>%
+                show()
+        }) %>%
+            invisible()
     }) %>%
         invisible()
 }
