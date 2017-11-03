66ab9c830eeef53eebc4319c2da0ca830a934142
Michael Steinbaugh <mike@steinbaugh.com>
2017-11-03 15:34:35 -0400

    Add support for `data.frame` and `seurat` classes

--- R/methods-plotCellCounts.R
+++ R/methods-plotCellCounts.R
@@ -6,6 +6,24 @@
 #' @author Michael Steinbaugh, Rory Kirchner
 #'
 #' @inherit plotGenesPerCell
+#'
+#' @examples
+#' # bcbioSingleCell
+#' \dontrun{
+#' plotCellCounts(bcb)
+#' }
+#'
+#' # seurat
+#' \dontrun{
+#' plotCellCounts(seurat)
+#' }
+#'
+#' # metrics data.frame
+#' \dontrun{
+#' metrics <- metrics(bcb)
+#' metadata <- sampleMetadata(bcb)
+#' plotCellCounts(metrics, metadata)
+#' }
 NULL
 
 
@@ -18,54 +36,53 @@ NULL
 #' @importFrom viridis scale_fill_viridis
 .plotCellCounts <- function(
     object,
+    metadata,
     interestingGroups,
-    filterCells = TRUE,
-    aggregateReplicates = TRUE) {
-    if (missing(interestingGroups)) {
-        interestingGroups <-
-            metadata(object)[["interestingGroups"]][[1]]
-    }
-    metrics <- metrics(
-        object,
-        filterCells = filterCells,
-        aggregateReplicates = aggregateReplicates)
-    meta <- sampleMetadata(
-        object,
-        aggregateReplicates = aggregateReplicates)
-    cellCounts <- metrics %>%
+    multiplexed = FALSE,
+    fill = scale_fill_viridis(discrete = TRUE)) {
+    data <- object %>%
         group_by(!!sym("sampleName")) %>%
-        summarize(cells = n()) %>%
-        left_join(meta, by = "sampleName")
+        summarize(nCells = n()) %>%
+        left_join(metadata, by = "sampleName")
     p <- ggplot(
-        cellCounts,
+        data,
         mapping = aes_string(
             x = "sampleName",
-            y = "cells",
-            fill = interestingGroups)
+            y = "nCells",
+            fill = "interestingGroups")
     ) +
-        labs(x = "sample",
-             y = "cell count") +
         geom_bar(stat = "identity") +
-        scale_fill_viridis(discrete = TRUE) +
         theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
+    # Label interesting groups
+    if (!missing(interestingGroups)) {
+        p <- p + labs(fill = paste(interestingGroups, collapse = ":\n"))
+    } else {
+        p <- p + labs(color = NULL, fill = NULL)
+    }
+
+    # Color palette
+    if (!is.null(fill)) {
+        p <- p + fill
+    }
+
     # Labels
-    if (nrow(cellCounts) <= qcLabelMaxNum) {
+    if (nrow(data) <= qcLabelMaxNum) {
         p <- p +
             geom_text(
-                mapping = aes_string(label = "cells"),
+                mapping = aes_string(label = "nCells"),
                 fontface = "bold",
                 vjust = -0.5)
     }
 
     # Facets
     facets <- NULL
-    if (isTRUE(metadata(object)[["multiplexedFASTQ"]]) &
-        length(unique(metrics[["description"]])) > 1) {
+    if (isTRUE(multiplexed) &
+        length(unique(object[["description"]])) > 1) {
         facets <- c(facets, "description")
     }
     if (!isTRUE(aggregateReplicates) &
-        "sampleNameAggregate" %in% colnames(metrics)) {
+        "sampleNameAggregate" %in% colnames(object)) {
         facets <- c(facets, "sampleNameAggregate")
     }
     if (!is.null(facets)) {
@@ -83,4 +100,65 @@ NULL
 setMethod(
     "plotCellCounts",
     signature("bcbioSingleCell"),
+    function(
+        object,
+        interestingGroups,
+        filterCells = TRUE,
+        aggregateReplicates = TRUE,
+        fill = scale_fill_viridis(discrete = TRUE)) {
+        if (missing(interestingGroups)) {
+            interestingGroups <-
+                metadata(object)[["interestingGroups"]]
+        }
+        metrics <- metrics(
+            object,
+            interestingGroups = interestingGroups,
+            filterCells = filterCells,
+            aggregateReplicates = aggregateReplicates)
+        metadata <- sampleMetadata(
+            object,
+            aggregateReplicates = aggregateReplicates)
+        multiplexed <- metadata(object)[["multiplexedFASTQ"]]
+        .plotCellCounts(
+            object = metrics,
+            metadata = metadata,
+            interestingGroups = interestingGroups,
+            multiplexed = multiplexed,
+            fill = fill)
+    })
+
+
+
+#' @rdname plotCellCounts
+#' @export
+setMethod(
+    "plotCellCounts",
+    signature("data.frame"),
     .plotCellCounts)
+
+
+
+#' @rdname plotCellCounts
+#' @export
+setMethod(
+    "plotCellCounts",
+    signature("seurat"),
+    function(
+        object,
+        interestingGroups,
+        fill = scale_fill_viridis(discrete = TRUE)) {
+        if (missing(interestingGroups)) {
+            interestingGroups <-
+                slot(object, "misc") %>%
+                .[["bcbio"]] %>%
+                .[["interestingGroups"]]
+        }
+        metrics <- metrics(object, interestingGroups = interestingGroups)
+        metadata <- sampleMetadata(object)
+        .plotCellCounts(
+            object = metrics,
+            metadata = metadata,
+            interestingGroups = interestingGroups,
+            multiplexed = FALSE,
+            fill = fill)
+    })
