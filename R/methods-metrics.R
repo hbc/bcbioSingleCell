5d7de07e0c9699fdcb5af65755b478dadb1c1aa4
Michael Steinbaugh <mike@steinbaugh.com>
2017-11-03 11:47:13 -0400

    Update support for metrics from a seurat object

--- R/methods-metrics.R
+++ R/methods-metrics.R
@@ -9,6 +9,7 @@
 #'
 #' @inheritParams AllGenerics
 #'
+#' @param interestingGroups Interesting group, to use for colors.
 #' @param filterCells Show only the cells that have passed filtering cutoffs.
 #' @param aggregateReplicates Aggregate technical replicates, if specified. This
 #'   function uses values assigned in the `sampleNameAggregate` column of the
@@ -21,90 +22,125 @@ NULL
 
 
 
-# Constructors ====
-#' Metrics Constructor
-#'
+# Methods ====
+#' @rdname metrics
+#' @importFrom basejump uniteInterestingGroups
 #' @importFrom dplyr left_join mutate
 #' @importFrom magrittr set_colnames
 #' @importFrom stringr str_match
 #' @importFrom tibble column_to_rownames rownames_to_column
-#'
-#' @inheritParams AllGenerics
-#'
-#' @param filterCells Return only metrics for the filtered cells.
-#'
-#' @return [data.frame].
-#' @noRd
-.metrics <- function(object, aggregateReplicates = TRUE) {
-    colData <- colData(object) %>%
-        as.data.frame() %>%
-        rownames_to_column("cellID")
-
-    # Prepare the metadata
-    meta <- metadata(object)[["sampleMetadata"]] %>%
-        as.data.frame()
-
-    # Rename `sampleName` when aggregating replicates
-    if (isTRUE(aggregateReplicates) &
-        "sampleNameAggregate" %in% colnames(meta)) {
-        meta[["sampleName"]] <- meta[["sampleNameAggregate"]]
-        meta[["sampleNameAggregate"]] <- NULL
-    }
-
-    # Get the barcode format based on the umiType
-    umiType <- metadata(object)[["umiType"]]
-    if (umiType == "surecell") {
-        barcode <- "[ACGT]{6}_[ACGT]{6}_[ACGT]{6}"
-    } else {
-        barcode <- "[ACGT]{8}_[ACGT]{8}"
-    }
-
-    # Check for presence of valid barcode in cell identifiers
-    if (!all(grepl(x = colData[["cellID"]], pattern = paste0(barcode, "$")))) {
-        stop("Failed to detect surecell barcodes", call. = FALSE)
-    }
-
-    # Match the sampleID from the cellular barcode (rowname)
-    match <- colData[["cellID"]] %>%
-        str_match(paste0("^(.+)_(", barcode, ")$")) %>%
-        as.data.frame() %>%
-        set_colnames(c("cellID", "sampleID", "cellularBarcode")) %>%
-        # Remove the unnecessary cellularBarcode column
-        mutate(cellularBarcode = NULL)
-
-    # Join the sample metadata by sampleID, extracted from the barcode ID
-    colData %>%
-        left_join(match, by = "cellID") %>%
-        left_join(meta, by = "sampleID") %>%
-        as.data.frame() %>%
-        column_to_rownames("cellID")
-}
-
-
-
-# Methods ====
-#' @rdname metrics
 #' @export
-setMethod("metrics", "bcbioSingleCell", function(
-    object,
-    filterCells = TRUE,
-    aggregateReplicates = TRUE) {
-    if (isTRUE(filterCells)) {
-        cells <- metadata(object)[["filterCells"]]
-        if (!is.null(cells)) {
-            object <- object[, cells]
+setMethod(
+    "metrics",
+    signature("bcbioSingleCell"),
+    function(
+        object,
+        interestingGroups,
+        filterCells = TRUE,
+        aggregateReplicates = TRUE) {
+        # Filter cells that passed QC checks, if desired
+        if (isTRUE(filterCells)) {
+            cells <- metadata(object)[["filterCells"]]
+            if (!is.null(cells)) {
+                object <- object[, cells]
+            }
         }
-    }
-    .metrics(object, aggregateReplicates = aggregateReplicates)
-})
+
+        colData <- colData(object) %>%
+            as.data.frame() %>%
+            rownames_to_column("cellID")
+
+        # Prepare the metadata
+        if (missing(interestingGroups)) {
+            interestingGroups <- metadata(object)[["interestingGroups"]]
+        }
+        metadata <- metadata(object)[["sampleMetadata"]] %>%
+            as.data.frame() %>%
+            uniteInterestingGroups(interestingGroups) %>%
+            # Ensure all the columns are factors here. This will also sanitize
+            # any previously saved datasets, where the sample identifiers are
+            # character vectors.
+            mutate_all(as.factor)
+
+        # Rename `sampleName` when aggregating replicates
+        if (isTRUE(aggregateReplicates) &
+            "sampleNameAggregate" %in% colnames(metadata)) {
+            metadata[["sampleName"]] <- metadata[["sampleNameAggregate"]]
+            metadata[["sampleNameAggregate"]] <- NULL
+        }
+
+        # Join the sample metadata by maching against the cellular barcode
+        # Get the barcode format based on `umiType`
+        umiType <- metadata(object)[["umiType"]]
+        if (umiType == "surecell") {
+            barcode <- "[ACGT]{6}_[ACGT]{6}_[ACGT]{6}"
+        } else {
+            barcode <- "[ACGT]{8}_[ACGT]{8}"
+        }
+        # Check for presence of valid barcode in cell identifiers
+        if (!all(grepl(x = colData[["cellID"]],
+                       pattern = paste0(barcode, "$")))) {
+            stop("Failed to detect surecell barcodes", call. = FALSE)
+        }
+        # Match `sampleID` from the cellular barcode (rowname)
+        match <- colData[["cellID"]] %>%
+            str_match(paste0("^(.+)_(", barcode, ")$")) %>%
+            as.data.frame() %>%
+            set_colnames(c("cellID", "sampleID", "cellularBarcode")) %>%
+            # Remove the unnecessary cellularBarcode column
+            mutate(
+                cellularBarcode = NULL,
+                sampleID = as.factor(.data[["sampleID"]])
+            )
+        # Join the sample metadata by `sampleID`, extracted from `cellID`
+        metrics <- colData %>%
+            left_join(match, by = "cellID") %>%
+            left_join(metadata, by = "sampleID") %>%
+            as.data.frame() %>%
+            column_to_rownames("cellID")
+        metrics
+    })
 
 
 
 #' @rdname metrics
 #' @importFrom basejump camel
+#' @importFrom dplyr mutate_if
+#' @importFrom tibble column_to_rownames rownames_to_column
 #' @export
-setMethod("metrics", "seurat", function(object) {
-    object@data.info %>%
-        camel(strict = FALSE) %>%
-        .metrics(aggregateReplicates = FALSE)
+setMethod(
+    "metrics",
+    signature("seurat"),
+    function(
+        object,
+        interestingGroups) {
+    if (missing(interestingGroups)) {
+        # Check to see if bcbio interestingGroups are stashed.
+        # Stop if they're not defined.
+        interestingGroups <- slot(object, "misc") %>%
+            .[["bcbio"]] %>%
+            .[["interestingGroups"]]
+        if (is.null(interestingGroups)) {
+            stop("'interestingGroups' is required", call. = FALSE)
+        }
+    }
+    # This slot was previously named `data.info` in earlier releases
+    metrics <- slot(object, "meta.data") %>%
+        as.data.frame() %>%
+        rownames_to_column() %>%
+        camel(strict = FALSE)
+    # Check for required columns, and make sure they're factors
+    requiredCols <- c("description", "sampleID", "sampleName")
+    if (!all(requiredCols %in% colnames(metrics))) {
+        stop(paste(
+            "object@meta.data must contain these columns:",
+            toString(requiredCols)
+        ), call. = FALSE)
+    }
+    # Ensure all strings are factors
+    metrics <- mutate_if(metrics, is.character, as.factor)
+    # Create the `interestingGroups` column required for QC plots
+    metrics <- uniteInterestingGroups(metrics, interestingGroups)
+    metrics <- column_to_rownames(metrics)
+    metrics
 })
