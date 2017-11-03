53e7eb918cacd5a630ad214098cbd084404c4b06
Michael Steinbaugh <mike@steinbaugh.com>
2017-11-03 18:13:15 -0400

    Improve generation of sampleMetadata from seurat object

--- R/methods-sampleMetadata.R
+++ R/methods-sampleMetadata.R
@@ -13,6 +13,48 @@ NULL
 
 
 
+# Constructors ====
+#' Prepare Sample Metadata from Seurat
+#'
+#' @author Michael Steinbaugh
+#' @keywords internal
+#' @noRd
+#'
+#' @importFrom basejump camel
+#' @importFrom dplyr distinct mutate_if select_if
+#' @importFrom tibble remove_rownames
+.prepareSampleMetadataFromSeurat <- function(object) {
+    df <- slot(object, "meta.data") %>%
+        as.data.frame() %>%
+        remove_rownames()
+
+    # Check for required columns
+    if (!all(metadataPriorityCols %in% colnames(df))) {
+        stop(paste(
+            "Required columns:", toString(metadataPriorityCols)
+        ), call. = FALSE)
+    }
+
+    # Remove cell cycle regression and resolution columns
+    blacklist <- paste(c("Phase", "res\\."), collapse = "|")
+
+    df <- df %>%
+        .[, !grepl(x = colnames(.), pattern = blacklist)] %>%
+        mutate_if(is.character, as.factor) %>%
+        select_if(is.factor) %>%
+        distinct() %>%
+        camel(strict = FALSE)
+
+    # Check for distinct failure
+    if (any(duplicated(df[["sampleID"]]))) {
+        stop("Failed to make sample metadata rows distinct", call. = TRUE)
+    }
+
+    df
+}
+
+
+
 # Methods ====
 #' @rdname sampleMetadata
 #' @importFrom dplyr distinct everything mutate mutate_if select
@@ -24,35 +66,38 @@ setMethod(
     signature("bcbioSingleCell"),
     function(
         object,
+        interestingGroups,
         aggregateReplicates = TRUE) {
-        meta <- metadata(object)[["sampleMetadata"]] %>%
+        if (missing(interestingGroups)) {
+            interestingGroups <-
+                metadata(object)[["interestingGroups"]]
+        }
+
+        metadata <- metadata(object)[["sampleMetadata"]] %>%
             as.data.frame()
 
         # Aggregate replicates, if necessary
         if (isTRUE(aggregateReplicates) &
-            "sampleNameAggregate" %in% colnames(meta)) {
-            meta <- meta %>%
+            "sampleNameAggregate" %in% colnames(metadata)) {
+            metadata <- metadata %>%
                 mutate(sampleName = .data[["sampleNameAggregate"]],
                        sampleID = make.names(
                            .data[["sampleName"]], unique = FALSE),
                        sampleNameAggregate = NULL) %>%
                 select(unique(c(
-                    "sampleID",
-                    "sampleName",
-                    interestingGroups(object)
+                    "sampleID", "sampleName", interestingGroups
                 ))) %>%
                 distinct()
         } else {
             # Put the priority columns first
-            meta <- meta %>%
+            metadata <- metadata %>%
                 select(c("sampleID",
                          "sampleName",
                          "description"),
                        everything())
         }
 
-        interestingGroups <- metadata(object)[["interestingGroups"]]
-        meta %>%
+        metadata %>%
             uniteInterestingGroups(interestingGroups) %>%
             # Ensure strings as factors
             mutate_if(is.character, as.factor) %>%
@@ -63,66 +108,38 @@ setMethod(
 
 
 #' @rdname sampleMetadata
-#' @importFrom basejump camel
-#' @importFrom dplyr distinct funs mutate_if summarize_all
+#' @importFrom dplyr mutate_if
 #' @importFrom magrittr set_rownames
-#' @importFrom tibble remove_rownames
 #' @export
 setMethod(
     "sampleMetadata",
     signature("seurat"),
-    function(object) {
-        # Check to see if bcbio metadata is stashed in '@misc' slot. This is
-        # saved when using our `setAs()` coercion method.
-        bcbMeta <- slot(object, "misc") %>%
-            .[["bcbio"]] %>%
-            .[["sampleMetadata"]]
-        if (!is.null(bcbMeta)) {
-            df <- bcbMeta
-        } else {
-            message(paste(
-                "Constructing sample metadata",
-                "from 'seurat@meta.data' slot"
-            ))
-            df <- slot(object, "meta.data") %>%
-                remove_rownames()
-            # Drop any columns that appear to be a count (e.g. `nUMI`)
-            df <- df[, !grepl(x = colnames(df), pattern = "^n[A-Z]")]
-            # Drop any tSNE resolution columns (e.g. `res.0.8`)
-            df <- df[, !grepl(x = colnames(df), pattern = "^res\\.")]
-            # Drop any blacklisted columns
-            blacklist <- c(
-                # Seurat
-                "orig.ident",
-                "percent.mito",
-                "G2M.Score",
-                "S.Score",
-                "Phase",
-                # bcbio
-                "cellularBarcode",
-                "log10GenesPerUMI",
-                "mitoRatio")
-            df <- df %>%
-                .[, !colnames(.) %in% blacklist]
-
-            # Only keep columns containing duplicates. Any remaining metrics
-            # columns should be filtered by this step.
-            findColsWithDupes <- function(.data) {
-                .data %>%
-                    as.character() %>%
-                    duplicated() %>%
-                    any()
+    function(
+        object,
+        interestingGroups) {
+        # Check for stashed metadata in `misc` slot
+        bcbio <- bcbio(object)
+        if (!is.null(bcbio)) {
+            if (missing(interestingGroups)) {
+                interestingGroups <- bcbio[["interestingGroups"]]
             }
-            hasDupes <- summarize_all(df, funs(findColsWithDupes))
-            df <- df[, as.logical(hasDupes)]
-
-            # Finally, attempt to collapse into distinct rows
-            df <- distinct(df) %>%
-                camel(strict = FALSE)
+            df <- bcbio[["sampleMetadata"]]
+            if (!identical(
+                unique(df[["sampleID"]]),
+                unique(slot(object, "meta.data")[["sampleID"]])
+            )) {
+                stop("Dimension mismatch with stashed metadata",
+                     call. = FALSE)
+            }
+        } else {
+            if (missing(interestingGroups)) {
+                interestingGroups <- "sampleName"
+            }
+            df <- .prepareSampleMetadataFromSeurat(object)
         }
-
-        # Ensure strings as factors
-        df <- mutate_if(df, is.character, as.factor)
-        rownames(df) <- df[["sampleID"]]
-        df
+        df %>%
+            # Ensure strings as factors
+            mutate_if(is.character, as.factor) %>%
+            uniteInterestingGroups(interestingGroups) %>%
+            set_rownames(.[["sampleID"]])
     })
