da45f36e7cad5de7e66dbf4db4fc2b8fcd0ec1e4
Michael Steinbaugh <mike@steinbaugh.com>
2017-11-08 14:35:44 -0500

    Update per sample clustering script

--- inst/R_scripts/clustering_per_sample.R
+++ inst/R_scripts/clustering_per_sample.R
@@ -1,22 +1,31 @@
+# Latest version of this script is available here:
+# script <- system.file(
+#     file.path("R_scripts", "clustering_per_sample.R"),
+#     package = "bcbioSingleCell")
+# file.edit(script)
+
 library(bcbioSingleCell)
 library(pbapply)
 library(rmarkdown)
 
 prepareSingleCellTemplate()
 source("setup.R")
+dataDir <- "data"
 
-loadData(bcb, dir = "data")
+loadDataAsName(bcb = "bcb_pool_filtered", dir = dataDir)
+metadata(bcb)$filterParams
 
-sampleIDs <- sampleMetadata(bcb) %>%
-    pull(sampleID)
-sampleSubsets <- pblapply(seq_along(sampleIDs), function(a) {
-    sampleID <- sampleIDs[[a]]
-    subset <- selectSamples(bcb, sampleID = sampleID)
-    assignAndSaveData(name = sampleID, object = subset, dir = "data")
-    sampleID
+sampleNames <- sampleMetadata(bcb) %>%
+    pull(sampleName)
+sampleSubsets <- pblapply(seq_along(sampleNames), function(a) {
+    sampleName <- as.character(sampleNames[[a]])
+    sampleFileName <- snake(sampleName)
+    subset <- selectSamples(bcb, sampleName = sampleName)
+    assignAndSaveData(name = sampleFileName, object = subset, dir = dataDir)
+    sampleFileName
 }) %>%
     unlist()
-saveData(sampleSubsets, dir = "data")
+saveData(sampleSubsets, dir = dataDir)
 
 # Render RMarkdown reports per bcbioSingleCell subset file
 pblapply(seq_along(sampleSubsets), function(a) {
