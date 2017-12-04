4ee85b88fcf914d0c61cf6768e63f3462314a5ea
Michael Steinbaugh <mike@steinbaugh.com>
2017-12-04 12:14:41 -0500

    Draft commit to remove `Seurat::DarkTheme()` dependency

--- R/darkTheme.R
+++ R/darkTheme.R
@@ -5,29 +5,52 @@
 #' @author Michael Steinbaugh
 #' @keywords internal
 #'
-#' @importFrom ggplot2 element_blank element_line theme
-#' @importFrom Seurat DarkTheme
+#' @importFrom ggplot2 element_blank element_line element_rect margin theme
+#'
+#' @param ... Passthrough arguments to [ggplot2::theme()].
 #'
 #' @seealso Modified version of [Seurat::DarkTheme()].
 #'
-#' @return No return.
+#' @seealso
+#' - [ggplot2::theme()].
+#' - [Seurat::DarkTheme()].
+#'
+#' @return [ggplot] theme.
 #' @export
-darkTheme <- function() {
-    DarkTheme() +
-        theme(
-            axis.line = element_line(
-                color = "white",
-                linetype = "solid",
-                size = 1),
-            axis.ticks = element_line(
-                color = "white",
-                linetype = "solid",
-                size = 1),
-            legend.justification = "center",
-            legend.position = "bottom",
-            panel.background = element_blank(),
-            panel.border = element_blank(),
-            panel.grid.major = element_blank(),
-            panel.grid.minor = element_blank()
-        )
+darkTheme <- function(...) {
+    blackBackground <- element_rect(fill = "black")
+    blackBackgroundNoBorder <- element_rect(fill = "black", size = 0)
+    fontMargin <- 4
+    whiteText <- element_text(
+        colour = "white",
+        margin = margin(
+            t = fontMargin,
+            r = fontMargin,
+            b = fontMargin,
+            l = fontMargin))
+    whiteLine <- element_line(
+        color = "white",
+        linetype = "solid",
+        size = 1)
+    theme(
+        axis.line = whiteLine,
+        axis.text = whiteText,
+        axis.ticks = whiteLine,
+        axis.title = whiteText,
+        legend.background = blackBackground,
+        legend.box.background = blackBackgroundNoBorder,
+        legend.justification = "center",
+        legend.key = blackBackgroundNoBorder,
+        legend.position = "bottom",
+        legend.text = whiteText,
+        legend.title = whiteText,
+        panel.background = element_blank(),
+        panel.border = element_blank(),
+        panel.grid = element_blank(),
+        plot.background = blackBackground,
+        plot.subtitle = whiteText,
+        plot.title = whiteText,
+        complete = TRUE,
+        validate = TRUE,
+        ...)
 }
