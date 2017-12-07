5f2f52ab71fa6a99a4938614eff8019233b2a875
Michael Steinbaugh <mike@steinbaugh.com>
2017-12-07 11:44:04 -0500

    Update sampleMetadata unit test

--- tests/testthat/test_sampleMetadata.R
+++ tests/testthat/test_sampleMetadata.R
@@ -5,18 +5,20 @@ test_that("sampleMetadata", {
     expect_is(data, "data.frame")
     expect_identical(
         lapply(data, class),
-        list(sampleID = "factor",
-             sampleName  = "factor",
-             description = "factor",
-             fileName = "factor",
-             index = "factor",
-             sequence = "factor",
-             sampleNameAggregate = "factor",
-             sequencingReplicate = "factor",
-             biologicalReplicate = "factor",
-             genotype = "factor",
-             concNgUl = "factor",
-             revcomp = "factor",
-             interestingGroups = "factor")
+        list(
+            "sampleID" = "factor",
+            "sampleName"  = "factor",
+            "description" = "factor",
+            "fileName" = "factor",
+            "index" = "factor",
+            "sequence" = "factor",
+            "sampleNameAggregate" = "factor",
+            "sequencingReplicate" = "factor",
+            "biologicalReplicate" = "factor",
+            "genotype" = "factor",
+            "concNgUl" = "factor",
+            "revcomp" = "factor",
+            "interestingGroups" = "factor"
+        )
     )
 })
