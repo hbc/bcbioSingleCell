bc454c6524c8e346ddaa45a01e8ee1c58e15823a
Michael Steinbaugh <mike@steinbaugh.com>
2017-12-07 12:11:35 -0500

    Initial `selectSamples()` unit test

---
+++ tests/testthat/test_selectSamples.R
@@ -0,0 +1,52 @@
+context("selectSamples")
+
+filtered <- filterCells(bcb, quiet = TRUE)
+
+test_that("selectSamples", {
+    # Add a quiet argument here in the future
+    subset <- suppressMessages(
+        selectSamples(filtered, interestingGroups = "homozygote")
+    )
+    expect_is(subset, "bcbioSingleCell")
+    expect_identical(
+        metadata(subset)[["selectSamples"]],
+        TRUE
+    )
+    expect_identical(
+        dim(subset),
+        c(22173L, 1616L)
+    )
+    expect_identical(
+        sampleMetadata(subset)[["sampleID"]],
+        factor(
+            c("run1_AGGCTTAG",
+              "run1_TATGCAGT",
+              "run2_AGGCTTAG",
+              "run2_TATGCAGT")
+        )
+    )
+})
+
+test_that("Match failure", {
+    expect_equal(
+        suppressWarnings(
+            selectSamples(filtered, sampleID = "XXX")
+        ),
+        NULL
+    )
+    expect_warning(
+        selectSamples(filtered, sampleID = "XXX"),
+        "Match failure: sampleID = XXX"
+    )
+    expect_warning(
+        selectSamples(filtered, sampleID = "XXX"),
+        "No samples matched"
+    )
+})
+
+test_that("Stop on unfiltered object", {
+    expect_error(
+        selectSamples(bcb, interestingGroups = "homozygote"),
+        "'filterCells\\(\\)' hasn't been applied to this dataset"
+    )
+})
