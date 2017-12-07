831e37c500e645f8386a2971304596428f6d161e
Michael Steinbaugh <mike@steinbaugh.com>
2017-12-07 14:52:32 -0500

    Switch method of handling external data

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,14 +3,4 @@ set.seed(42L)
 library(testthat)
 library(bcbioSingleCell)
 
-loadRemoteData(
-    file.path(
-        "http://bcbiosinglecell.seq.cloud",
-        "testthat",
-        "bcb.rda"),
-    quiet = TRUE)
-
-pooled <- suppressMessages(aggregateReplicates(bcb))
-filtered <- filterCells(pooled, quiet = TRUE)
-
 test_check("bcbioSingleCell")
