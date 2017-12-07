ea1d9f0a7ec0d7266ec3f2595a88eeef9e8df91a
Michael Steinbaugh <mike@steinbaugh.com>
2017-12-07 18:09:52 -0500

    Resave working examples

--- data-raw/examples.R
+++ data-raw/examples.R
@@ -5,12 +5,28 @@ extdataDir <- system.file("extdata", package = "bcbioSingleCell")
 uploadDir <- file.path(extdataDir, "harvard_indrop_v3")
 sampleMetadataFile <- file.path(extdataDir, "harvard_indrop_v3.xlsx")
 annotable <- annotable("Homo sapiens", release = 90)
+
 bcb <- loadSingleCell(
     uploadDir = uploadDir,
     sampleMetadataFile = sampleMetadataFile,
     annotable = annotable)
 
+pooled <- aggregateReplicates(bcb)
+filtered <- filterCells(pooled)
+
+# Minimal simple Seurat working example
+seurat <- as(filtered, "seurat") %>%
+    NormalizeData() %>%
+    FindVariableGenes(do.plot = FALSE) %>%
+    ScaleData() %>%
+    RunPCA(do.print = FALSE) %>%
+    FindClusters(dims.use = 1:20) %>%
+    RunTSNE(dims.use = 1:20, do.fast = TRUE)
+
 examples <- list(
-    bcb = bcb
+    bcb = bcb,
+    pooled = pooled,
+    filtered = filtered,
+    seurat = seurat
 )
 use_data(examples, compress = "xz", overwrite = TRUE)
