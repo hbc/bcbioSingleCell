6dc4ff17fbfb38f90097b4ca7f492f5e4a30ce72
Michael Steinbaugh <mike@steinbaugh.com>
2017-12-08 14:42:20 -0500

    Update method support to include `dgCMatrix` and `matrix`

---
+++ R/methods-plotZerosVsDepth.R
@@ -0,0 +1,93 @@
+#' Plot Percentage of Zeros vs. Library Depth
+#'
+#' @rdname plotZerosVsDepth
+#' @name plotZerosVsDepth
+#' @family Quality Control Metrics
+#' @author Rory Kirchner, Michael Steinbaugh
+#'
+#' @inheritParams AllGenerics
+#'
+#' @return [ggplot].
+#'
+#' @examples
+#' load(system.file(
+#'     file.path("inst", "extdata", "bcb.rda"),
+#'     package = "bcbioSingleCell"))
+#'
+#' # bcbioSingleCell
+#' plotZerosVsDepth(bcb)
+#'
+#' # dgCMatrix
+#' counts <- counts(bcb)
+#' metrics <- metrics(bcb)
+#' plotZerosVsDepth(counts, metrics = metrics)
+NULL
+
+
+
+# Constructors ====
+#' @importFrom ggplot2 aes_string facet_wrap geom_point ggplot labs
+#'   scale_x_log10
+#' @importFrom Matrix colSums
+.plotZerosVsDepth <- function(object, metrics) {
+    # Using a logical matrix is fast and memory efficient
+    present <- as(object, "lgCMatrix")
+    df <- data.frame(
+        dropout = (nrow(present) - Matrix::colSums(present)) / nrow(present),
+        depth = Matrix::colSums(object),
+        description = metrics[["description"]])
+    p <- ggplot(
+        df,
+        mapping = aes_string(
+            x = "depth",
+            y = "dropout")
+    ) +
+        geom_point(size = 0.8, alpha = 0.3) +
+        scale_x_log10() +
+        labs(x = "library size (depth)",
+             y = "dropout rate")
+
+    # Facets
+    facets <- NULL
+    if (isTRUE(.checkAggregate(object))) {
+        facets <- c(facets, "sampleNameAggregate")
+    }
+    if (!is.null(facets)) {
+        p <- p + facet_wrap(facets = facets, scales = "free")
+    }
+
+    p
+}
+
+
+
+# Methods ====
+#' @rdname plotZerosVsDepth
+#' @export
+setMethod(
+    "plotZerosVsDepth",
+    signature("bcbioSingleCell"),
+    function(object) {
+        .plotZerosVsDepth(
+            object = assay(object),
+            metrics = metrics(object)
+        )
+    })
+
+
+
+#' @rdname plotZerosVsDepth
+#' @export
+setMethod(
+    "plotZerosVsDepth",
+    signature("dgCMatrix"),
+    .plotZerosVsDepth)
+
+
+
+#' @rdname plotZerosVsDepth
+#' @export
+setMethod(
+    "plotZerosVsDepth",
+    signature("matrix"),
+    .plotZerosVsDepth)
