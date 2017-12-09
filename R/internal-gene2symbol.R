76f7d3d24c60db4a79492b485cfd3272871fce48
Michael Steinbaugh <mike@steinbaugh.com>
2017-12-09 17:43:01 -0500

    Rename to `.convertGenesToSymbols()`

--- R/internal-gene2symbol.R
+++
@@ -1,8 +0,0 @@
-.gene2symbol <- function(object, genes) {
-    gene2symbol <- bcbio(object, "gene2symbol")
-    if (is.null(gene2symbol)) {
-        stop("Object doesn't contain Ensembl gene to symbol mappings")
-    }
-    gene2symbol %>%
-        .[which(.[["ensgene"]] %in% genes), "symbol", drop = TRUE]
-}
