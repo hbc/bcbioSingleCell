80dcbfc75c616cb8a4a54dbce1bc26aea87270d9
Michael Steinbaugh <mike@steinbaugh.com>
2017-12-11 13:42:05 -0500

    Make a more minimal example by sorting the counts by abundance per gene and per cell

--- data-raw/examples.R
+++ data-raw/examples.R
@@ -10,8 +10,52 @@ bcb <- loadSingleCell(
     sampleMetadataFile = sampleMetadataFile,
     annotable = annotable)
 
+# Make the example bcbioSingleCell object more minimal to save disk space
+# First, let's pick only the top 250 genes that have the most robust expression.
+counts <- counts(bcb)
+if (!is(counts, "dgCMatrix")) {
+    stop("counts should be sparse dgCMatrix")
+}
+
+nGenes <- 1000L
+countsPerGene <- Matrix::rowSums(counts) %>%
+    sort(decreasing = TRUE) %>%
+    head(n = nGenes)
+topGenes <- names(countsPerGene) %>%
+    sort()
+length(topGenes)
+
+nCells <- 500L
+countsPerCell <- Matrix::colSums(counts) %>%
+    sort(decreasing = TRUE) %>%
+    head(n = nCells)
+topCells <- names(countsPerCell) %>%
+    sort()
+length(topCells)
+
+if (!identical(
+    c(nGenes, nCells),
+    c(length(topGenes), length(topCells))
+)) {
+    stop("Example should have same number of genes and cells")
+}
+
+bcb <- bcb[topGenes, topCells]
+if (!identical(c(nGenes, nCells), dim(bcb))) {
+    stop("Subset operation failed")
+}
+bcb
+object.size(bcb) %>%
+    format(units = "auto")
+
 pooled <- aggregateReplicates(bcb)
-filtered <- filterCells(pooled)
+pooled
+
+filtered <- filterCells(
+    pooled,
+    maxMitoRatio = 0.25,
+    minNovelty = 0.7)
+filtered
 
 # Minimal simple Seurat working example
 seurat <- as(filtered, "seurat") %>%
@@ -21,6 +65,7 @@ seurat <- as(filtered, "seurat") %>%
     RunPCA(do.print = FALSE) %>%
     FindClusters(dims.use = 1:20) %>%
     RunTSNE(dims.use = 1:20, do.fast = TRUE)
+seurat
 
 saveData(
     bcb,
