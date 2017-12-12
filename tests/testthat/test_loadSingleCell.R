1a9be5f7a0e4ff734b1543a1ad6827f8784e1996
Michael Steinbaugh <mike@steinbaugh.com>
2017-12-12 14:48:34 -0500

    Add unit test

---
+++ tests/testthat/test_loadSingleCell.R
@@ -0,0 +1,15 @@
+context("loadSingleCell")
+
+test_that("Homo sapiens", {
+    # Homo sapiens
+    # Minimal working example dataset
+    extdataDir <- system.file("extdata", package = "bcbioSingleCell")
+    uploadDir <- file.path(extdataDir, "harvard_indrop_v3")
+    sampleMetadataFile <- file.path(extdataDir, "harvard_indrop_v3.xlsx")
+    bcb <- suppressWarnings(suppressMessages(
+        loadSingleCell(
+        uploadDir = uploadDir,
+        sampleMetadataFile = sampleMetadataFile)
+    ))
+    expect_is(bcb, "bcbioSingleCell")
+})
