1aa1bbc8b57eb3cfe2f42ca7bdc5ab2a66104363
Michael Steinbaugh <mike@steinbaugh.com>
2017-12-12 12:27:34 -0500

    Add unit tests

---
+++ tests/testthat/test_plotTopMarkers.R
@@ -0,0 +1,19 @@
+context("plotTopMarkers")
+
+load(system.file(
+    file.path("extdata", "seurat.rda"),
+    package = "bcbioSingleCell"))
+load(system.file(
+    file.path("extdata", "topMarkers.rda"),
+    package = "bcbioSingleCell"))
+topMarkers <- topMarkers[1:2, ]
+
+
+
+test_that("seurat", {
+    expect_is(topMarkers, "grouped_df")
+    plotlist <- plotTopMarkers(seurat, topMarkers)
+    # FIXME This is returning empty lists
+    expect_is(plotlist, "list")
+    expect_is(plotlist[[1]][[1]], "ggplot")
+})
