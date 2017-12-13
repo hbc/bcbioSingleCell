5f0466d950f41bedaedb69bcf2b7cc15701d6ee9
Michael Steinbaugh <mike@steinbaugh.com>
2017-12-13 10:49:58 -0500

    Add `geom` parameter tests

--- tests/testthat/test_plotReadsPerCell.R
+++ tests/testthat/test_plotReadsPerCell.R
@@ -4,11 +4,54 @@ load(system.file(
     file.path("extdata", "bcb.rda"),
     package = "bcbioSingleCell"))
 load(system.file(
+    file.path("extdata", "filtered.rda"),
+    package = "bcbioSingleCell"))
+load(system.file(
     file.path("extdata", "seurat.rda"),
     package = "bcbioSingleCell"))
 
 test_that("bcbioSingleCell", {
-    p <- plotReadsPerCell(bcb)
+    # Example dataset doesn't have a cellular barcode cutoff because we removed
+    # the bcbio commands log file (which conflicts with Travis CI)
+    histogram <- suppressWarnings(
+        plotReadsPerCell(bcb, geom = "histogram")
+    )
+    expect_is(histogram, "ggplot")
+    expect_is(
+        histogram %>%
+            .[["layers"]] %>%
+            .[[1L]] %>%
+            .[["geom"]],
+        "GeomLine"
+    )
+
+    ridgeline <- suppressWarnings(
+        plotReadsPerCell(bcb, geom = "ridgeline")
+    )
+    expect_is(ridgeline, "ggplot")
+    expect_is(
+        ridgeline %>%
+            .[["layers"]] %>%
+            .[[1L]] %>%
+            .[["geom"]],
+        "GeomDensityRidges"
+    )
+
+    violin <- suppressWarnings(
+        plotReadsPerCell(bcb, geom = "violin")
+    )
+    expect_is(violin, "ggplot")
+    expect_is(
+        violin %>%
+            .[["layers"]] %>%
+            .[[1L]] %>%
+            .[["geom"]],
+        "GeomViolin"
+    )
+})
+
+test_that("filtered", {
+    p <- suppressWarnings(plotReadsPerCell(filtered))
     expect_is(p, "ggplot")
 })
 
