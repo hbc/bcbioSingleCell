f8d35bca8e5e2b120e171dddf394a166e13ad628
Michael Steinbaugh <mike@steinbaugh.com>
2018-01-12 09:42:01 -0500

    Remove unused parameter

--- R/methods-aggregateReplicates.R
+++ R/methods-aggregateReplicates.R
@@ -9,10 +9,6 @@
 #'
 #' @inheritParams AllGenerics
 #'
-#' @param groupings Factor that defines the aggregation groupings. The new
-#'   aggregation names are defined as the factor levels, and the original
-#'   replicates are defined as the names of the factor.
-#'
 #' @examples
 #' load(system.file(
 #'     file.path("extdata", "bcb.rda"),
