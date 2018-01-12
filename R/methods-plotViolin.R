e4deb76441ae14c86b276e95d848f24e03e39b91
Michael Steinbaugh <mike@steinbaugh.com>
2018-01-12 09:43:54 -0500

    Fix comment typo in working example

--- R/methods-plotViolin.R
+++ R/methods-plotViolin.R
@@ -15,9 +15,9 @@
 #'   `return = "markdown"`.
 #'
 #' @examples
-# load(system.file(
-#     file.path("extdata", "seurat.rda"),
-#     package = "bcbioSingleCell"))
+#' load(system.file(
+#'     file.path("extdata", "seurat.rda"),
+#'     package = "bcbioSingleCell"))
 #'
 #' genes <- slot(seurat, "raw.data") %>% rownames() %>% .[1:2]
 #' print(genes)
