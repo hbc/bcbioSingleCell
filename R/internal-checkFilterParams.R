6c33b0bc9dcb19c7e39a65681d4c6861bfc8f49c
Michael Steinbaugh <mike@steinbaugh.com>
2018-01-23 13:28:57 -0500

    Update check to coerce legacy numeric to list, if necessary (changed in v0.0.29)

--- R/internal-checkFilterParams.R
+++ R/internal-checkFilterParams.R
@@ -1,9 +1,22 @@
 .checkFilterParams <- function(object) {
-    filterParams <- metadata(object)[["filterParams"]]
-    if (is.null(filterParams)) {
+    params <- metadata(object)[["filterParams"]]
+    if (is.null(params)) {
         abort("`filterCells()` hasn't been applied to this dataset")
     }
-    if (!is.numeric(filterParams)) {
-        abort("Filter parameters must all be numeric")
+
+    # `filterParams` metadata was stored as a named numeric vector up until
+    # v0.0.28. We changed to storing as a list in v0.0.29, to allow for per
+    # sample cutoffs.
+    if (is.numeric(params)) {
+        params <- as.list(params)
+    }
+
+    # Ensure all params are numeric
+    if (!all(vapply(
+        X = params,
+        FUN = is.numeric,
+        FUN.VALUE = logical(1L)
+    ))) {
+        abort("Filter parameters must be numeric")
     }
 }
