85d374b0eccb17470cf94debc31e775c58231ea4
Michael Steinbaugh <mike@steinbaugh.com>
2018-01-26 15:07:09 -0500

    Add unit test

--- tests/testthat/test_gene2symbol.R
+++ tests/testthat/test_gene2symbol.R
@@ -3,7 +3,6 @@ context("gene2symbol")
 load(system.file(
     file.path("extdata", "bcb.rda"),
     package = "bcbioSingleCell"))
-
 load(system.file(
     file.path("extdata", "seurat.rda"),
     package = "bcbioSingleCell"))
