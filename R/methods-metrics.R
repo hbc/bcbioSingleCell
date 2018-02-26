83b16ac7aca97f5e8e1e9a82fd4923d8d3db21b1
Michael Steinbaugh <mike@steinbaugh.com>
2018-02-26 16:45:32 -0500

    Can make internal `metrics()` code common between bcbioSingleCell and seurat, now that we have `colData()` method support

--- R/methods-metrics.R
+++ R/methods-metrics.R
@@ -28,68 +28,47 @@ NULL
 
 
 
-# Methods ======================================================================
-#' @rdname metrics
+# Constructors =================================================================
 #' @importFrom bcbioBase uniteInterestingGroups
 #' @importFrom dplyr left_join
 #' @importFrom tibble column_to_rownames rownames_to_column
+.metrics <- function(object, interestingGroups) {
+    if (missing(interestingGroups)) {
+        interestingGroups <- bcbioBase::interestingGroups(object)
+    }
+    colData <- colData(object)
+    sampleID <- cell2sample(object)
+    assert_are_identical(rownames(colData), names(sampleID))
+    sampleMetadata <- sampleMetadata(
+        object,
+        interestingGroups = interestingGroups)
+    cbind(
+        as.data.frame(sampleID),
+        as.data.frame(colData)
+    ) %>%
+        # Need to use `cellID` column for rownames, because that is the
+        # column set in `sampleMetadata`
+        rownames_to_column("cellID") %>%
+        left_join(sampleMetadata, by = "sampleID") %>%
+        .sanitizeMetrics() %>%
+        column_to_rownames("cellID")
+}
+
+
+
+# Methods ======================================================================
+#' @rdname metrics
 #' @export
 setMethod(
     "metrics",
     signature("bcbioSingleCell"),
-    function(
-        object,
-        interestingGroups) {
-        if (missing(interestingGroups)) {
-            interestingGroups <- bcbioBase::interestingGroups(object)
-        }
-        colData <- colData(object)
-        sampleID <- cell2sample(object)
-        assert_are_identical(rownames(colData), names(sampleID))
-        sampleMetadata <- sampleMetadata(
-            object,
-            interestingGroups = interestingGroups)
-        metrics <- cbind(
-            as.data.frame(sampleID),
-            as.data.frame(colData)
-        ) %>%
-            # Need to use `cellID` column for rownames, because that is the
-            # column set in `sampleMetadata`
-            rownames_to_column("cellID") %>%
-            left_join(sampleMetadata, by = "sampleID") %>%
-            .sanitizeMetrics() %>%
-            column_to_rownames("cellID")
-        assert_are_identical(colnames(object), rownames(metrics))
-        metrics
-    })
+    .metrics)
 
 
 
 #' @rdname metrics
-#' @importFrom basejump camel
-#' @importFrom dplyr mutate_if
-#' @importFrom tibble column_to_rownames rownames_to_column
 #' @export
 setMethod(
     "metrics",
     signature("seurat"),
-    function(
-        object,
-        interestingGroups) {
-    if (missing(interestingGroups)) {
-        interestingGroups <- bcbioBase::interestingGroups(object)
-    }
-    metrics <- slot(object, "meta.data") %>%
-        as.data.frame() %>%
-        camel(strict = FALSE) %>%
-        rownames_to_column("cellID")
-    # Join columns can vary here, so suppress message
-    metrics <- suppressMessages(left_join(
-        metrics,
-        sampleMetadata(object, interestingGroups = interestingGroups)
-    )) %>%
-        .sanitizeMetrics() %>%
-        column_to_rownames("cellID") %>%
-        uniteInterestingGroups(interestingGroups)
-    metrics
-})
+    .metrics)
