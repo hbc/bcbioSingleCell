f7012498bed99afa197fdaae2f22ab782ff93c46
Michael Steinbaugh <mike@steinbaugh.com>
2018-02-27 12:39:36 -0500

    Improve loading of cached files

--- tests/testthat/helper_01_cache.R
+++ tests/testthat/helper_01_cache.R
@@ -1,24 +1,27 @@
 cacheURL <- "http://bcbiosinglecell.seq.cloud"
+files <- c(
+    "bcb.rda",
+    "filtered.rda",
+    "known_markers_detected.rda",
+    "pooled.rda",
+    "seurat.rda",
+    "seurat_all_markers.rda",
+    "seurat_all_markers_original.rda",
+    "top.rda")
 mapply(
-    FUN = function(cacheURL, file) {
+    FUN = function(cacheURL, file, envir) {
         # Download file to testthat directory
         if (!file_exists(file)) {
-            download.file(url = paste(cacheURL, file, sep = "/"), destfile = file)
+            download.file(
+                url = paste(cacheURL, file, sep = "/"),
+                destfile = file)
         }
         # Load R Data file
-        if (grepl("\\.rda", file)) {
-            load(file)
+        if (grepl("\\.rda$", file)) {
+            message(paste("Loading", file))
+            load(file, envir = envir)
         }
     },
-    file = c(
-        "bcb.rda",
-        "filtered.rda",
-        "known_markers_detected.rda",
-        "pooled.rda",
-        "seurat.rda",
-        "seurat_all_markers.rda",
-        "seurat_all_markers_original.rda",
-        "top.rda"
-    ),
-    MoreArgs = list(cacheURL = cacheURL)
+    file = files,
+    MoreArgs = list(cacheURL = cacheURL, envir = environment())
 )
