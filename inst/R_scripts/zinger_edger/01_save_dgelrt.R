00248ff9df68ba560b44cbbf1099a951890f1e89
Michael Steinbaugh <mike@steinbaugh.com>
2018-03-07 16:47:21 -0500

    Update DGELRT script

--- inst/R_scripts/zinger_edger/01_save_dgelrt.R
+++ inst/R_scripts/zinger_edger/01_save_dgelrt.R
@@ -1,104 +1,80 @@
-# Differential Expression Using zingeR-edgeR
+# Differential Expression Per Cluster Using zingeR-edgeR
 #
 # Michael Steinbaugh
-# 2018-02-26
+# 2018-03-07
+#
+# Compatible with bcbioSingleCell v0.0.32
+# zingeR vignette v2: https://goo.gl/4rTK1w
 
-library(BiocParallel)
+library(parallel)
 library(edgeR)
 library(zingeR)
 library(Seurat)
 library(bcbioSingleCell)
-
-# ==============================================================================
-# zingeR-edgeR
-#
-# We first perform a differential expression analysis using zingeR posterior
-# probabilities and focussing on the count component of the ZINB model. We use
-# edgeR to model the count component.
-#
-# The weights are estimated with the core function of zingeR, `zeroWeightsLS()`.
-# It is important to be consistent with the normalization procedure, i.e. if you
-# use TMM normalization for the analysis, you should also use it for estimating
-# the zingeR posterior probabilities. The weights can then be provided in the
-# DGEList object which will be used for dispersion estimation in the native
-# edgeR `estimateDisp()` function.
-#
-# After estimation of the dispersions and zingeR posterior probabilities, the
-# `glmWeightedF()` function is used for statistical inference. This is an
-# adapted function from the `glmLRT()` function of edgeR. It uses an F-test for
-# which the denominator degrees of freedom are by default adjusted according to
-# the downweighting of excess zeros (`ZI = TRUE`). Also, independent filtering
-# can be performed on the obtained p-values (`independentFiltering = TRUE`). We
-# use the independent filtering strategy that was originally implemented in
-# DESeq2. By default, the average fitted values are used as a filter criterion.
-#
-# https://goo.gl/4rTK1w
-# ==============================================================================
+library(tidyverse)
 
 data_dir <- path("dir", Sys.Date())
 loadData(seurat, dir = data_dir)
 
 # Get the cell cluster identities from the final Seurat object
-ident <- slot(seurat, "ident") %>% levels()
+interestingGroups(seurat) <- "<INTGROUP>"
+sampleData(seurat)
+metrics(seurat) %>% glimpse()
 
-dgelrt <- bpmapply(
+ident <- levels(seurat@ident)
+treatment <- "<TREATMENT>"
+control <- "<CONTROL>"
+
+dgelrt <- mcmapply(
+    ident = ident,
+    MoreArgs = list(
+        object = seurat,
+        treatment = treatment,
+        control = control,
+        prefix = "<PREFIX>",
+        dir = data_dir
+    ),
     FUN = function(
-        seurat,
+        object,
         ident,
+        treatment,
+        control,
+        maxit = 1000L,
+        prefix = "zinger_edger_lrt_cluster",
         envir = parent.frame(),
         dir = ".") {
-        cells <- WhichCells(seurat, ident = ident)
-        counts <- counts(seurat, normalized = FALSE)
-        counts <- counts[, cells]
+        map <- metrics(object) %>%
+            as_tibble() %>%
+            rownames_to_column("cellID") %>%
+            .[, c("ident", "interestingGroups", "cellID")] %>%
+            .[.[["ident"]] == ident, ,] %>%
+            group_by(!!!syms(c("ident", "interestingGroups"))) %>%
+            arrange(!!sym("cellID"), .by_group = TRUE)
+        numerator <- map %>%
+            .[.[["interestingGroups"]] == treatment, ] %>%
+            pull("cellID")
+        denominator <- map %>%
+            .[.[["interestingGroups"]] == control, ] %>%
+            pull("cellID")
+        assert_are_set_equal(map[["cellID"]], c(numerator, denominator))
 
-        # Get the primary interesting group, used to set up the contrast
-        intgroup <- interestingGroups(seurat)[[1L]]
-        group <- metrics(seurat) %>%
-            .[cells, intgroup] %>%
-            as.factor() %>%
-            # Be sure to set the control as the reference level!
-            relevel(ref = "control")
+        # zingeR-edgeR differential expression
+        lrt <- diffExp(
+            object,
+            numerator = numerator,
+            denominator = denominator,
+            maxit = maxit)
 
-        # Check the counts
-        table(group)
-
-        # zingeR + edgeR analysis
-        dge <- DGEList(counts, group = group)
-        dge <- calcNormFactors(dge, method = "TMM")
-
-        # Ensure the design matrix is set up correctly.
-        # For comparing `mut` vs. `wt`, define `wt` as the reference level
-        design <- model.matrix(~group)
-        levels(group)  # wt, mut
-        table(design[, 2L])
-
-        # This is the zingeR step that is computationally expensive
-        weights <- zeroWeightsLS(
-            counts = dge[["counts"]],
-            design = design,
-            # The `maxit` default of 200 is too low
-            maxit = 1000L,
-            normalization = "TMM")
-
-        dge[["weights"]] <- weights
-        dge <- estimateDisp(dge, design)
-
-        plotBCV(dge)
-
-        fit <- glmFit(dge, design)
-        lrt <- glmWeightedF(fit, coef = 2L, independentFiltering = TRUE)
-
-        # Save the individual DGELRT to disk, as a backup
+        # Save the individual DGELRT to disk, as a backup in case parallel
+        # call is suspended on cluster
         assignAndSaveData(
-            name = paste("zinger_edger_lrt_cluster", ident, sep = "_"),
+            name = paste(prefix, ident, sep = "_"),
             object = lrt,
             envir = envir,
             dir = dir)
 
         lrt
     },
-    ident = ident,
-    MoreArgs = list(seurat = seurat, dir = data_dir),
     SIMPLIFY = FALSE,
     USE.NAMES = TRUE)
 saveData(dgelrt, dir = dat_dir)
