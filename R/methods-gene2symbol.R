69bdf16a08e212a0c4998a65e6ad0ef7f605cc2b
Michael Steinbaugh <mike@steinbaugh.com>
2018-03-19 17:02:06 -0400

    Simplify, requiring rowRanges

--- R/methods-gene2symbol.R
+++ R/methods-gene2symbol.R
@@ -8,28 +8,12 @@
 #' @inheritParams general
 #'
 #' @examples
-#' load(system.file("extdata/bcb.rda", package = "bcbioSingleCell"))
-#' load(system.file("extdata/seurat.rda", package = "bcbioSingleCell"))
-#'
 #' # bcbioRNASeq ====
-#' gene2symbol(bcb) %>% glimpse()
+#' gene2symbol(bcb_small) %>% glimpse()
 #'
 #' # seurat ====
-#' gene2symbol(seurat) %>% glimpse()
+#' gene2symbol(seurat_small) %>% glimpse()
 NULL
-
-
-
-# Constructors =================================================================
-.gene2symbolFromRowData <- function(object) {
-    rowData <- rowData(object)
-    if (is.null(rowData)) {
-        abort("Object does not contain internal rowData")
-    }
-    cols <- c("geneID", "geneName")
-    assert_is_subset(cols, colnames(rowData))
-    rowData[, cols]
-}
 
 
 
@@ -40,32 +24,12 @@ setMethod(
     "gene2symbol",
     signature("seurat"),
     function(object) {
-        gene2symbol <- bcbio(object, "gene2symbol")
-        if (is.data.frame(gene2symbol)) {
-            return(gene2symbol)
+        rowData <- rowData(object)
+        if (is.null(rowData)) {
+            abort("Object does not contain internal rowData")
         }
-
-        gene2symbol <- .gene2symbolFromRowData(object)
-        if (is.data.frame(gene2symbol)) {
-            return(gene2symbol)
-        }
-
-        rownames <- slot(object, "data") %>% rownames()
-        if (!is.null(names(rownames))) {
-            geneID <- names(rownames)
-            geneName <- rownames
-            gene2symbol <- data.frame(
-                "geneID" = geneID,
-                "geneName" = geneName,
-                row.names = geneID,
-                stringsAsFactors = FALSE
-            )
-            return(gene2symbol)
-        }
-
-        abort(paste(
-            "seurat object doesn't appear to contain",
-            "Ensembl gene-to-symbol mappings"
-        ))
+        cols <- c("geneID", "geneName")
+        assert_is_subset(cols, colnames(rowData))
+        rowData[, cols]
     }
 )
