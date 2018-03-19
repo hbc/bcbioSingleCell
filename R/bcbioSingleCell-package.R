55d726700083c4512208de3cb379e60b7ce464b1
Michael Steinbaugh <mike@steinbaugh.com>
2018-03-18 22:25:19 -0400

    Update NAMESPACE

--- R/bcbioSingleCell-package.R
+++ R/bcbioSingleCell-package.R
@@ -21,6 +21,7 @@
 #' @importFrom assertive assert_are_intersecting_sets
 #' @importFrom assertive assert_are_identical
 #' @importFrom assertive assert_has_no_duplicates
+#' @importFrom assertive assert_has_names
 #' @importFrom assertive assert_is_a_bool
 #' @importFrom assertive assert_is_a_string
 #' @importFrom assertive assert_is_all_of
