62abc4637f945f254d2dd4e5c8f1d25f6adece01
Michael Steinbaugh <mike@steinbaugh.com>
2018-03-20 10:39:02 -0400

    Switch `returnAsList` to `return` and use `match.arg()`

--- R/methods-plotFeatureTSNE.R
+++ R/methods-plotFeatureTSNE.R
@@ -1,37 +1,35 @@
+# FIXME Fix return documentation.
+
 #' Plot Feature t-SNE
 #'
-#' @rdname plotFeatureTSNE
 #' @name plotFeatureTSNE
 #' @author Michael Steinbaugh
 #'
 #' @inheritParams general
 #' @inheritParams plotTSNE
-#'
 #' @param features Character vector of features (e.g. gene expression, PC
 #'   scores, number of genes detected).
 #' @param legend Show legends in paneled plots. Defaults to `FALSE` because
 #'   typically these look too busy and the legends can get cut off.
-#' @param returnAsList Return plots as a list.
+#' @param return Return as "`plot`" or "`list`".
 #'
 #' @seealso [Seurat::FeaturePlot()].
 #'
-#' @return No return, only graphical output.
+#' @return Depends on the return argument.
 #'
 #' @examples
-#' load(system.file("extdata/seurat.rda", package = "bcbioSingleCell"))
-#'
-#' # seurat
-#' plotFeatureTSNE(seurat, features = "PC1")
+#' # seurat ====
+#' plotFeatureTSNE(pbmc_small, features = "PC1")
 NULL
 
 
 
 # Methods ======================================================================
 #' @rdname plotFeatureTSNE
+#' @importFrom Seurat FetchData
 #' @importFrom basejump midnightTheme
 #' @importFrom cowplot plot_grid
 #' @importFrom ggplot2 aes_string geom_point ggplot scale_color_gradient theme
-#' @importFrom Seurat FetchData
 #' @export
 setMethod(
     "plotFeatureTSNE",
@@ -41,36 +39,44 @@ setMethod(
         features,
         pointSize = 0.5,
         pointAlpha = 0.8,
-        color = ggplot2::scale_color_gradient(
+        color = scale_color_gradient(
             low = "lightgray",
-            high = "purple"),
+            high = "purple"
+        ),
         dark = FALSE,
         label = TRUE,
         labelSize = 6L,
         legend = FALSE,
-        returnAsList = FALSE) {
-        data <- cbind(
-            fetchTSNEData(object),
-            FetchData(object, vars.all = features)
-        )
+        return = c("plot", "list")
+    ) {
+        return <- match.arg(return)
+
+        tsne <- fetchTSNEData(object)
+        data <- Seurat::FetchData(object, vars.all = features)
+        assert_are_identical(rownames(tsne), rownames(data))
+        assert_are_disjoint_sets(tsne, data)
+        data <- cbind(tsne, data)
         # Remove duplicate columns
         data <- data[, unique(colnames(data))]
-        plotlist <- lapply(seq_along(features), function(a) {
+        plotlist <- lapply(features, function(feature) {
             p <- ggplot(
-                data,
+                data = data,
                 mapping = aes_string(
                     x = "tSNE1",
                     y = "tSNE2",
-                    color = features[[a]])
-            )
+                    color = feature
+                )
+            ) +
+                geom_point(
+                    alpha = pointAlpha,
+                    size = pointSize
+                ) +
+                labs(title = feature)
+
             if (isTRUE(dark)) {
                 p <- p + midnightTheme()
             }
-            p <- p +
-                geom_point(
-                    alpha = pointAlpha,
-                    size = pointSize) +
-                labs(title = features[[a]])
+
             if (isTRUE(label)) {
                 if (isTRUE(dark)) {
                     labelColor <- "white"
@@ -82,22 +88,29 @@ setMethod(
                         mapping = aes_string(
                             x = "centerX",
                             y = "centerY",
-                            label = "ident"),
+                            label = "ident"
+                        ),
                         color = labelColor,
                         size = labelSize,
-                        fontface = "bold")
+                        fontface = "bold"
+                    )
             }
+
             if (is(color, "ScaleContinuous")) {
                 p <- p + color
             }
+
             if (!isTRUE(legend)) {
                 p <- p + theme(legend.position = "none")
             }
+
             p
         })
-        if (isTRUE(returnAsList)) {
-            plotlist
-        } else {
+
+        if (return == "plot") {
             plot_grid(plotlist = plotlist, labels = NULL)
+        } else if (return == "list") {
+            plotlist
         }
-    })
+    }
+)
