5d527df337621646bae8830e9a00dccf283f3e48
Michael Steinbaugh <mike@steinbaugh.com>
2018-03-20 09:37:28 -0400

    Update documentation and working example

--- R/methods-fetchTSNEExpressionData.R
+++ R/methods-fetchTSNEExpressionData.R
@@ -3,59 +3,43 @@
 #' This gets t-SNE locations, cellular metadata, expression of genes and
 #' the geometric mean of the gene expression from a [seurat] object.
 #'
-#' @rdname fetchTSNEExpressionData
 #' @name fetchTSNEExpressionData
 #' @family t-SNE Utilities
 #' @author Rory Kirchner, Michael Steinbaugh
 #'
 #' @inheritParams general
-#'
 #' @param genes Genes identifiers (matching the rownames in the object),
 #'   of which to get expression data.
 #'
-#' @return [data.frame] containing tSNE coordinates, sample metadata, and
+#' @return `data.frame` containing tSNE coordinates, sample metadata, and
 #'   aggregate marker expression values (`mean`, `median`, and `sum`).
 #'
 #' @examples
-#' load(system.file("extdata/seurat.rda", package = "bcbioSingleCell"))
-#'
-#' genes <- counts(seurat) %>% rownames() %>% head()
+#' # seurat ====
+#' genes <- head(rownames(pbmc_small))
 #' print(genes)
-#'
-#' fetchTSNEExpressionData(seurat, genes = genes) %>%
-#'     glimpse()
+#' fetchTSNEExpressionData(pbmc_small, genes = genes) %>% glimpse()
 NULL
-
-
-
-# Constructors =================================================================
-#' @importFrom Biobase rowMedians
-#' @importFrom dplyr everything group_by select
-#' @importFrom rlang !!! !! sym syms
-#' @importFrom Seurat FetchData
-#' @importFrom tibble column_to_rownames rownames_to_column
-#' @importFrom tidyr gather
-.fetchTSNEExpressionData.seurat <- function(  # nolint
-    object,
-    genes) {
-    tsne <- fetchTSNEData(object)
-
-    # Gene aggregate math
-    data <- fetchGeneData(object, genes = genes)
-    # TODO Okay to just use S4Vectors methods here?
-    mean <- Matrix::rowMeans(data)
-    median <- rowMedians(data)
-    sum <- Matrix::rowSums(data)
-
-    cbind(tsne, mean, median, sum)
-}
 
 
 
 # Methods ======================================================================
 #' @rdname fetchTSNEExpressionData
+#' @importFrom Biobase rowMedians
 #' @export
 setMethod(
     "fetchTSNEExpressionData",
     signature("seurat"),
-    .fetchTSNEExpressionData.seurat)
+    function(object, genes) {
+        assert_is_character(genes)
+        tsne <- fetchTSNEData(object)
+
+        # Gene aggregate math
+        data <- fetchGeneData(object, genes = genes)
+        mean <- Matrix::rowMeans(data)
+        median <- rowMedians(data)
+        sum <- Matrix::rowSums(data)
+
+        cbind(tsne, mean, median, sum)
+    }
+)
