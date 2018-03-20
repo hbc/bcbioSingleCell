c0f9af678b1a89bd24da226d29244e44f1b872b4
Michael Steinbaugh <mike@steinbaugh.com>
2018-03-20 08:35:53 -0400

    Document example datasets

--- R/datasets.R
+++ R/datasets.R
@@ -1,3 +1,18 @@
+#' Minimal Seurat Markers Example
+#'
+#' @author Michael Steinbaugh
+#' @keywords internal
+"all_markers_small"
+
+
+
+#' Minimal `bcbioSingleCell` Example
+#'
+#' @author Michael Steinbaugh
+#' @keywords internal
+"bcb_small"
+
+
 #' Cell-Cycle Markers
 #'
 #' @author Michael Steinbaugh
@@ -9,3 +24,19 @@
 #'
 #' @author Michael Steinbaugh
 "cellTypeMarkers"
+
+
+
+#' Minimal Known Markers Example
+#'
+#' @author Michael Steinbaugh
+#' @keywords internal
+"known_markers_small"
+
+
+
+#' Minimal `seurat` Example
+#'
+#' @author Michael Steinbaugh
+#' @keywords internal
+"seurat_small"
