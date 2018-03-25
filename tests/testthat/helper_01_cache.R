4a2fbf0934b88e09c29a34e36a9ff72a88434f6f
Michael Steinbaugh <mike@steinbaugh.com>
2018-03-25 16:32:18 -0400

    Load internal data in extdata directory

--- tests/testthat/helper_01_cache.R
+++ tests/testthat/helper_01_cache.R
@@ -1,19 +1,15 @@
-cacheURL <- "http://bcbiosinglecell.seq.cloud"
-files <- character()
-mapply(
-    FUN = function(cacheURL, file, envir) {
-        # Download file to testthat directory
-        if (!file.exists(file)) {
-            download.file(
-                url = paste(cacheURL, file, sep = "/"),
-                destfile = file)
-        }
-        # Load R Data file
-        if (grepl("\\.rda$", file)) {
-            message(paste("Loading", file))
-            load(file, envir = envir)
-        }
-    },
-    file = files,
-    MoreArgs = list(cacheURL = cacheURL, envir = environment())
+files <- list.files(
+    path = "../../inst/extdata",
+    pattern = "*.rda",
+    full.names = TRUE
 )
+if (length(files)) {
+    mapply(
+        file = files,
+        FUN = function(file, envir) {
+            message(paste("Loading", basename(file)))
+            load(file, envir = envir)
+        },
+        MoreArgs = list(envir = environment())
+    )
+}
