acb7e096d0adcf052b1d81e8613670f5d00dacb8
Michael Steinbaugh <mike@steinbaugh.com>
2018-03-25 15:10:02 -0400

    Update NAMESPACE

--- R/bcbioSingleCell-package.R
+++ R/bcbioSingleCell-package.R
@@ -29,13 +29,13 @@
 #' @importFrom cowplot draw_plot ggdraw plot_grid
 #' @importFrom dplyr arrange bind_rows group_by group_vars left_join mutate
 #'   mutate_all mutate_if n pull select_if summarize summarize_all ungroup
-#' @importFrom edgeR calcNormFactors DGEList glmFit
+#' @importFrom edgeR calcNormFactors DGEList estimateDisp glmFit
 #' @importFrom ggplot2 aes_ aes_string coord_flip element_blank element_line
 #'   element_rect element_text expand_limits facet_wrap geom_bar geom_boxplot
 #'   geom_histogram geom_hline geom_label geom_line geom_point geom_smooth
 #'   geom_text geom_violin geom_vline ggplot ggtitle guide_colorbar guide_legend
-#'   guides labs qplot scale_radius scale_x_log10 scale_x_sqrt
-#'   scale_y_continuous scale_y_log10 scale_y_sqrt theme xlab xlim ylab
+#'   guides labs qplot scale_radius scale_x_continuous scale_y_continuous theme
+#'   xlab xlim ylab
 #' @importFrom ggridges geom_density_ridges
 #' @importFrom graphics hist
 #' @importFrom grid unit
@@ -46,7 +46,7 @@
 #' @importFrom readr read_lines read_tsv
 #' @importFrom rlang !! !!! .data abort inform sym syms warn
 #' @importFrom scales percent
-#' @importFrom stats model.matrix reorder
+#' @importFrom stats as.formula median model.matrix relevel reorder
 #' @importFrom stringr str_extract str_match str_pad str_split
 #' @importFrom tibble as_tibble column_to_rownames remove_rownames
 #'   rownames_to_column tibble
