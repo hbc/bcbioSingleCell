5e40bfba7a85d7af84dcea53308d26d20a2f862e
Michael Steinbaugh <mike@steinbaugh.com>
2018-03-30 15:37:57 -0400

    Fix ridgeline x-axis [ci skip]

--- R/internal-plotQCMetric.R
+++ R/internal-plotQCMetric.R
@@ -84,7 +84,10 @@
             scale_y_continuous(trans = trans)
     }
 
-    p <- p + theme(axis.text.x = element_text(angle = 90L, hjust = 1L))
+    # Rotate the x-axis 90 degrees
+    if (geom != "ridgeline") {
+        p <- p + theme(axis.text.x = element_text(angle = 90L, hjust = 1L))
+    }
 
     # Cutoff lines
     if (geom %in% c("boxplot", "violin")) {
