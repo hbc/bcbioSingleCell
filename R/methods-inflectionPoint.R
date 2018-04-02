c572d0c3ead84367cc13678c040661a3ead0e16f
Michael Steinbaugh <mike@steinbaugh.com>
2018-04-02 17:24:57 -0400

    Update `inflectionPoint()` code to not attempt to use a cell cutoff. This is looking too conservative...we'll likely need another method

--- R/methods-inflectionPoint.R
+++ R/methods-inflectionPoint.R
@@ -5,8 +5,6 @@
 #' @author Michael Steinbaugh
 #'
 #' @inheritParams general
-#' @param maxCells The number of cells to consider as an upper bound and to
-#'   compute the inflection point.
 #'
 #' @seealso Modified version of `dropbead::estimateCellNumber()`.
 #'
@@ -23,37 +21,29 @@ NULL
 
 
 # Constructors =================================================================
-.inflectionPoint <- function(
-    object,
-    maxCells = 100000L
-) {
+.inflectionPoint <- function(object) {
     validObject(object)
-    assertIsAnImplicitInteger(maxCells)
-
     data <- .readsPerCell(object)
     assert_is_subset("nCount", colnames(data))
-    counts <- data[["nCount"]]
-
-    # Define the upper boundary cutoff, gated by `maxCells` if necessary
-    maxCells <- min(length(counts), maxCells)
-    counts <- counts[seq_len(maxCells)]
+    counts <- sort(data[["nCount"]])
 
     # Trigonometric calculations
     xdata <- seq_along(counts) / length(counts)
     cs <- cumsum(counts / sum(counts))
-    m <- (cs[[maxCells]] - cs[[1L]]) / (maxCells - xdata[[1L]])
-    dists <- vapply(
-        X = seq_len(maxCells),
-        FUN = function(cell) {
-            sin(atan(m)) * abs(cs[[cell]] - xdata[[cell]])
+    m <- max(cs) / length(counts)
+    dists <- mcmapply(
+        xdata = xdata,
+        cs = cs,
+        MoreArgs = list(m = m),
+        FUN = function(xdata, cs, m) {
+            sin(atan(m)) * abs(cs - xdata)
         },
-        FUN.VALUE = numeric(1L)
+        SIMPLIFY = TRUE,
+        USE.NAMES = FALSE
     )
 
     # Return the inflection point as the expression value
-    inflection <- counts[which.max(dists)]
-    names(inflection) <- countsCol
-    inflection
+    counts[which.max(dists)]
 }
 
 
