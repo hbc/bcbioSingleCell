631998326a3aff3360f9cbbd21d3a1b67dc96049
Michael Steinbaugh <mike@steinbaugh.com>
2018-04-04 15:56:49 -0400

    Allow user to set the scale, but recommend using "count" as default

--- R/methods-plotViolin.R
+++ R/methods-plotViolin.R
@@ -8,10 +8,12 @@
 #'
 #' @inheritParams general
 #' @inheritParams plotDot
+#' @inheritParams ggplot2::geom_violin
 #'
 #' @examples
 #' # seurat ====
-#' genes <- slot(pbmc_small, "raw.data") %>% rownames() %>% head(2L)
+#' genes <- head(rownames(pbmc_small), 2L)
+#' print(genes)
 #'
 #' # grid
 #' plotViolin(pbmc_small, genes = genes, return = "grid")
@@ -26,74 +28,71 @@ NULL
 
 
 
-# Constructors =================================================================
-.plotViolin.seurat <- function(  # nolint
-    object,
-    genes,
-    fill = scale_fill_viridis(discrete = TRUE),
-    headerLevel = 2L,
-    return = c("grid", "list", "markdown")
-) {
-    assertIsFillScaleDiscreteOrNULL(fill)
-    assertIsAHeaderLevel(headerLevel)
-    return <- match.arg(return)
-
-    # Fetch data ===============================================================
-    ident <- slot(object, "ident")
-    data <- fetchGeneData(object, genes = genes) %>%
-        as.data.frame() %>%
-        cbind(ident) %>%
-        rownames_to_column("cell") %>%
-        as_tibble() %>%
-        gather(
-            key = "gene",
-            value = "expression",
-            !!genes
-        ) %>%
-        group_by(!!sym("gene"))
-
-    # Loop across genes ====================================================
-    plotlist <- lapply(seq_along(genes), function(a) {
-        gene <- genes[[a]]
-        data <- data[data[["gene"]] == gene, , drop = FALSE]
-        p <- ggplot(
-            data,
-            mapping = aes_string(
-                x = "ident",
-                y = "expression",
-                fill = "ident"
-            )
-        ) +
-            geom_violin(
-                color = "black",
-                scale = "width",
-                adjust = 1L,
-                trim = TRUE
-            ) +
-            labs(title = gene) +
-            guides(fill = FALSE)
-        if (is(fill, "ScaleDiscrete")) {
-            p <- p + fill
-        }
-        p
-    })
-    names(plotlist) <- genes
-
-    # Return ===============================================================
-    dynamicPlotlist(
-        plotlist,
-        return = return,
-        headerLevel = headerLevel
-    )
-}
-
-
-
 # Methods ======================================================================
 #' @rdname plotViolin
 #' @export
 setMethod(
     "plotViolin",
     signature("seurat"),
-    .plotViolin.seurat
+    function(
+        object,
+        genes,
+        scale = c("count", "width", "area"),
+        fill = scale_fill_viridis(discrete = TRUE),
+        headerLevel = 2L,
+        return = c("grid", "list", "markdown")
+    ) {
+        scale <- match.arg(scale)
+        assertIsFillScaleDiscreteOrNULL(fill)
+        assertIsAHeaderLevel(headerLevel)
+        return <- match.arg(return)
+
+        # Fetch data ===========================================================
+        ident <- slot(object, "ident")
+        data <- fetchGeneData(object, genes = genes) %>%
+            as.data.frame() %>%
+            cbind(ident) %>%
+            rownames_to_column("cell") %>%
+            as_tibble() %>%
+            gather(
+                key = "gene",
+                value = "expression",
+                !!genes
+            ) %>%
+            group_by(!!sym("gene"))
+
+        # Loop across genes ====================================================
+        plotlist <- lapply(seq_along(genes), function(a) {
+            gene <- genes[[a]]
+            data <- data[data[["gene"]] == gene, , drop = FALSE]
+            p <- ggplot(
+                data,
+                mapping = aes_string(
+                    x = "ident",
+                    y = "expression",
+                    fill = "ident"
+                )
+            ) +
+                geom_violin(
+                    color = "black",
+                    scale = scale,
+                    adjust = 1L,
+                    trim = TRUE
+                ) +
+                labs(title = gene) +
+                guides(fill = FALSE)
+            if (is(fill, "ScaleDiscrete")) {
+                p <- p + fill
+            }
+            p
+        })
+        names(plotlist) <- genes
+
+        # Return ===============================================================
+        dynamicPlotlist(
+            plotlist,
+            return = return,
+            headerLevel = headerLevel
+        )
+    }
 )
