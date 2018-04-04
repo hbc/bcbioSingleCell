8a176a34b62a101713ffbb8946a23ceba08cda56
Michael Steinbaugh <mike@steinbaugh.com>
2018-04-04 12:58:36 -0400

    Update unit test

--- tests/testthat/test_plotQuantileHeatmap.R
+++ tests/testthat/test_plotQuantileHeatmap.R
@@ -2,14 +2,9 @@ context("plotQuantileHeatmap")
 
 test_that("plotQuantileHeatmap : seurat", {
     p <- plotQuantileHeatmap(seurat_small)
-    # Check for pheatmap return
     expect_is(p, "list")
     expect_identical(
         names(p),
-        c("quantiles", "plot")
-    )
-    expect_identical(
-        names(p[["plot"]]),
         c("tree_row", "tree_col", "kmeans", "gtable")
     )
 })
