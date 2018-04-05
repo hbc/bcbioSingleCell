09fd727d850b3e51faa724b242fbdd0f6934cc7b
Michael Steinbaugh <mike@steinbaugh.com>
2018-04-04 22:37:26 -0400

    Move constructor into method

--- R/methods-cellTypesPerCluster.R
+++ R/methods-cellTypesPerCluster.R
@@ -18,60 +18,55 @@ NULL
 
 
 
-# Constructors =================================================================
-.cellTypesPerCluster <- function(
-    object,
-    min = 1L,
-    max = Inf
-) {
-    assert_are_identical(attr(object, "vars"), "cellType")
-    requiredCols <- c(
-        "cellType",  # bcbio
-        "cluster",   # Seurat
-        "geneID",    # bcbio
-        "geneName",  # bcbio
-        "avgLogFC",  # Seurat v2.1
-        "padj"       # Seurat v2.1
-    )
-    assert_is_subset(requiredCols, colnames(object))
-
-    # Note that the order is important here
-    groupCols <- c("cluster", "cellType")
-
-    data <- object %>%
-        ungroup() %>%
-        .[, unique(c(groupCols, colnames(.))), drop = FALSE] %>%
-        group_by(!!!syms(groupCols)) %>%
-        arrange(.data[["padj"]], .by_group = TRUE) %>%
-        # Use `toString()` instead of `aggregate()` for R Markdown tables
-        summarize(
-            n = n(),
-            # Genes are arranged by P value
-            geneID = toString(.data[["geneID"]]),
-            geneName = toString(.data[["geneName"]])
-        ) %>%
-        group_by(!!sym("cluster")) %>%
-        arrange(dplyr::desc(.data[["n"]]), .by_group = TRUE)
-
-    # Apply minimum and maximum gene cutoffs
-    if (is.numeric(min) && min > 1L) {
-        data <- data[data[["n"]] >= min, , drop = FALSE]
-    }
-    if (is.numeric(max) && max > 1L) {
-        data <- data[data[["n"]] <= max, , drop = FALSE]
-    }
-    assert_has_rows(data)
-
-    data
-}
-
-
-
-# Methods =====
+# Methods ======================================================================
 #' @rdname cellTypesPerCluster
 #' @export
 setMethod(
     "cellTypesPerCluster",
     signature("grouped_df"),
-    .cellTypesPerCluster
+    function(
+        object,
+        min = 1L,
+        max = Inf
+    ) {
+        assert_are_identical(attr(object, "vars"), "cellType")
+        requiredCols <- c(
+            "cellType",  # bcbio
+            "cluster",   # Seurat
+            "geneID",    # bcbio
+            "geneName",  # bcbio
+            "avgLogFC",  # Seurat v2.1
+            "padj"       # Seurat v2.1
+        )
+        assert_is_subset(requiredCols, colnames(object))
+
+        # Note that the order is important here
+        groupCols <- c("cluster", "cellType")
+
+        data <- object %>%
+            ungroup() %>%
+            .[, unique(c(groupCols, colnames(.))), drop = FALSE] %>%
+            group_by(!!!syms(groupCols)) %>%
+            arrange(.data[["padj"]], .by_group = TRUE) %>%
+            # Use `toString()` instead of `aggregate()` for R Markdown tables
+            summarize(
+                n = n(),
+                # Genes are arranged by P value
+                geneID = toString(.data[["geneID"]]),
+                geneName = toString(.data[["geneName"]])
+            ) %>%
+            group_by(!!sym("cluster")) %>%
+            arrange(dplyr::desc(.data[["n"]]), .by_group = TRUE)
+
+        # Apply minimum and maximum gene cutoffs
+        if (is.numeric(min) && min > 1L) {
+            data <- data[data[["n"]] >= min, , drop = FALSE]
+        }
+        if (is.numeric(max) && max > 1L) {
+            data <- data[data[["n"]] <= max, , drop = FALSE]
+        }
+        assert_has_rows(data)
+
+        data
+    }
 )
