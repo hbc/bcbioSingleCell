ccef7f35d48ffdf0e404897f7e206d5cda497c8f
Michael Steinbaugh <mike@steinbaugh.com>
2018-04-05 07:25:32 -0400

    Simplify internal code and add option for mean, median calculations

--- R/methods-metricsPerSample.R
+++ R/methods-metricsPerSample.R
@@ -5,6 +5,7 @@
 #' @author Michael Steinbaugh
 #'
 #' @inheritParams general
+#' @param f Mathematical function to apply. Defaults to "`sum`".
 #'
 #' @return `tbl_df` containing summary statistics.
 #'
@@ -18,23 +19,30 @@ NULL
 
 
 
-# Constructors =================================================================
-.metricsPerSample <- function(object) {
-    metrics(object) %>%
-        .[, c("sampleName", "nUMI", "nGene", "nCoding"), drop = FALSE] %>%
-        group_by(!!sym("sampleName")) %>%
-        summarize_all(sum)
-}
-
-
-
 # Methods ======================================================================
 #' @rdname metricsPerSample
 #' @export
 setMethod(
     "metricsPerSample",
-    signature("bcbioSingleCell"),
-    .metricsPerSample
+    signature("SingleCellExperiment"),
+    function(
+        object,
+        f = c("sum", "mean", "median")
+    ) {
+        f <- match.arg(f)
+        inform(paste("Calculating", f, "per sample"))
+        f <- get(f)
+        assert_is_function(f)
+        assert_is_subset("sampleName", colnames(metrics(object)))
+        metrics <- metrics(object)
+        # Summarize only the `n*` columns containing counts
+        nCols <- grep("^n[A-Z]", colnames(metrics), value = TRUE)
+        assert_is_non_empty(nCols)
+        data <- metrics[, c("sampleName", nCols)]
+        data %>%
+            group_by(!!sym("sampleName")) %>%
+            summarize_all(f)
+    }
 )
 
 
@@ -44,5 +52,5 @@ setMethod(
 setMethod(
     "metricsPerSample",
     signature("seurat"),
-    .metricsPerSample
+    getMethod("metricsPerSample", "SingleCellExperiment")
 )
