83acfe0ced3e759ac71394a91396e5ffbe060662
Michael Steinbaugh <mike@steinbaugh.com>
2018-04-06 12:43:07 -0400

    Remove duplicate imports (see reexports.R file)

--- R/bcbioSingleCell-package.R
+++ R/bcbioSingleCell-package.R
@@ -28,15 +28,14 @@
 #'   sampleYAMLMetadata uniteInterestingGroups
 #' @importFrom cowplot draw_plot ggdraw plot_grid
 #' @importFrom dplyr arrange bind_rows group_by group_vars left_join matches
-#'   mutate mutate_all mutate_if n pull select_if summarize summarize_all
-#'   ungroup
+#'   mutate mutate_all mutate_if n select_if summarize summarize_all ungroup
 #' @importFrom edgeR calcNormFactors DGEList estimateDisp glmFit
 #' @importFrom ggplot2 aes_ aes_string coord_flip element_blank element_line
 #'   element_rect element_text expand_limits facet_wrap geom_bar geom_boxplot
 #'   geom_histogram geom_hline geom_label geom_line geom_point geom_smooth
-#'   geom_text geom_violin geom_vline ggplot ggtitle guide_colorbar guide_legend
-#'   guides labs qplot scale_radius scale_x_continuous scale_y_continuous
-#'   stat_ecdf theme xlab xlim ylab
+#'   geom_text geom_violin geom_vline ggtitle guide_colorbar guide_legend guides
+#'   labs qplot scale_radius scale_x_continuous scale_y_continuous stat_ecdf
+#'   theme xlab xlim ylab
 #' @importFrom ggridges geom_density_ridges
 #' @importFrom graphics hist
 #' @importFrom grid unit
