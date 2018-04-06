6a10e740551e15609d4b449f78d48d92c8f127e2
Michael Steinbaugh <mike@steinbaugh.com>
2018-04-06 12:41:54 -0400

    Move globals

--- R/bcbioSingleCell-package.R
+++ R/bcbioSingleCell-package.R
@@ -57,10 +57,3 @@
 #' @importFrom utils globalVariables packageVersion
 #' @importFrom zingeR glmWeightedF zeroWeightsLS
 NULL
-
-
-
-globalVariables(".")
-# Trailing number is to match cellranger output
-barcodePattern <- ")_([ACGT_]{6,})(_[0-9]+)?$"
-packageVersion <- packageVersion("bcbioSingleCell")
