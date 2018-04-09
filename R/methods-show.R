06815a272a50ec0279107d8b10b65ed241b4db90
Michael Steinbaugh <mike@steinbaugh.com>
2018-04-09 06:36:05 -0400

    Improve show appearance

--- R/methods-show.R
+++ R/methods-show.R
@@ -19,15 +19,49 @@ setMethod(
     signature("bcbioSingleCell"),
     function(object) {
         validObject(object)
+
         cat(
             paste(class(object), metadata(object)[["version"]]),
-            paste("samples:", nrow(sampleData(object))),
-            paste("cells:", ncol(object)),
-            paste0(metadata(object)[["level"]], ": ", nrow(object)),
-            paste("organism:", metadata(object)[["organism"]]),
-            paste("upload dir:", metadata(object)[["uploadDir"]]),
-            paste("upload date:", metadata(object)[["runDate"]]),
-            paste("R load date:", metadata(object)[["date"]]),
+            paste("Samples:", nrow(sampleData(object))),
+            paste("Cells:", ncol(object)),
+            paste0(
+                sub(
+                    pattern = "^([a-z])",
+                    replacement = "\\U\\1",
+                    x = metadata(object)[["level"]],
+                    perl = TRUE
+                ),
+                ": ",
+                nrow(object)
+            ),
+            paste("Organism:", metadata(object)[["organism"]]),
+            sep = "\n"
+        )
+
+        # rowRanges
+        m <- metadata(object)[["rowRangesMetadata"]]
+        if (is.data.frame(m)) {
+            cat(
+                paste(
+                    "AnnotationHub:",
+                    m[m[["name"]] == "id", "value", drop = TRUE]
+                ),
+                paste(
+                    "Ensembl Release:",
+                    m[m[["name"]] == "ensembl_version", "value", drop = TRUE]
+                ),
+                paste(
+                    "Genome Build:",
+                    m[m[["name"]] == "genome_build", "value", drop = TRUE]
+                ),
+                sep = "\n"
+            )
+        }
+
+        cat(
+            paste("Upload Dir:", metadata(object)[["uploadDir"]]),
+            paste("Upload Date:", metadata(object)[["runDate"]]),
+            paste("R Load Date:", metadata(object)[["date"]]),
             sep = "\n"
         )
     }
