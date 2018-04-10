de92ed9bb51aa2084b42b7ab0c5243aa25ac757a
Michael Steinbaugh <mike@steinbaugh.com>
2018-04-09 23:12:26 -0400

    Support for labeling UMI per cell cutoffs

--- R/internal-labelBarcodeRanks.R
+++ R/internal-labelBarcodeRanks.R
@@ -5,9 +5,7 @@
     point = c("knee", "inflection")
 ) {
     point <- match.arg(point)
-    ranks <- barcodeRanks(object)
-    inflection <- ranks[["inflection"]]
-    knee <- ranks[["knee"]]
+
 
     if (point == "inflection") {
         if (geom %in% c("boxplot", "violin")) {
@@ -58,47 +56,23 @@
 ) {
     point <- match.arg(point)
     ranks <- barcodeRanksPerSample(object)
-    color <- viridis(length(ranks))
-    for (i in seq_along(ranks)) {
-        if (point == "inflection") {
-            inflection <- ranks[[i]][["inflection"]]
-            if (geom %in% c("boxplot", "violin")) {
-                xintercept <- NULL
-                yintercept <- inflection
-            } else {
-                xintercept <- inflection
-                yintercept <- NULL
-            }
-            p <- p +
-                .qcCutoffLine(
-                    xintercept = xintercept,
-                    yintercept = yintercept,
-                    linetype = "solid",
-                    alpha = 0.5,
-                    size = 0.75,
-                    color = color[[i]]
-                )
-        }
-        if (point == "knee") {
-            knee <- ranks[[i]][["knee"]]
-            if (geom %in% c("boxplot", "violin")) {
-                xintercept <- NULL
-                yintercept <- knee
-            } else {
-                xintercept <- knee
-                yintercept <- NULL
-            }
-            p <- p +
-                .qcCutoffLine(
-                    xintercept = xintercept,
-                    yintercept = yintercept,
-                    linetype = "solid",
-                    alpha = 0.5,
-                    size = 0.75,
-                    color = color[[i]]
-                )
-        }
-    }
+    sampleNames <- as.character(sampleData(object)[, "sampleName"])
+
+    points <- lapply(seq_along(ranks), function(x) {
+        ranks[[x]][[point]]
+    })
+    points <- unlist(points)
+    names(points) <- names(ranks)
+
+    labels <- paste(sampleNames, points, sep = " = ")
+
+    # FIXME Need to figure out how to replace scale cleanly or suppress
+    # the message
+    p <- p + scale_color_viridis(
+        discrete = TRUE,
+        breaks = sampleNames,
+        labels = labels
+    )
 
     p
 }
