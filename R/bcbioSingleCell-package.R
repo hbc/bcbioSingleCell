5cc1759fde119bcc61037a2f4c3443b598d00a49
Michael Steinbaugh <mike@steinbaugh.com>
2018-05-24 11:12:23 -0400

    Update NAMESPACE

--- R/bcbioSingleCell-package.R
+++ R/bcbioSingleCell-package.R
@@ -57,9 +57,9 @@
 #'   readSampleData readTx2gene readYAMLSampleData sanitizeSampleData
 #'   uniteInterestingGroups
 #' @importFrom cowplot draw_plot ggdraw plot_grid
-#' @importFrom dplyr arrange bind_rows desc filter group_by group_vars left_join
-#'   matches mutate mutate_all mutate_if n rename select select_if slice
-#'   summarize summarize_all ungroup
+#' @importFrom dplyr arrange bind_rows desc everything filter group_by
+#'   group_vars left_join matches mutate mutate_all mutate_if n rename select
+#'   select_if slice summarize summarize_all ungroup
 #' @importFrom ggplot2 aes_ aes_string coord_flip element_blank element_line
 #'   element_rect element_text expand_limits facet_wrap geom_bar geom_boxplot
 #'   geom_histogram geom_hline geom_line geom_point geom_smooth
