dc6cbec5e2f7a155ddc09c018ec27f9141314abb
Michael Steinbaugh <mike@steinbaugh.com>
2018-06-04 11:22:50 -0400

    Import `bcbioBase::minimalSampleData()`

--- R/bcbioSingleCell-package.R
+++ R/bcbioSingleCell-package.R
@@ -9,17 +9,6 @@
 #' @importClassesFrom Seurat seurat
 #' @importClassesFrom SingleCellExperiment SingleCellExperiment
 #'
-#' @importFrom Biobase rowMedians sampleNames
-#' @importFrom GenomicFeatures genes makeTxDbFromGFF transcripts
-#' @importFrom Matrix colSums readMM rowMeans rowSums
-#' @importFrom Matrix.utils aggregate.Matrix
-#' @importFrom S4Vectors DataFrame aggregate as.data.frame as.matrix
-#'   complete.cases mcols metadata metadata<- na.omit
-#' @importFrom Seurat Convert CreateSeuratObject FetchData GetDimReduction
-#' @importFrom SingleCellExperiment SingleCellExperiment isSpike isSpike<-
-#'   spikeNames
-#' @importFrom SummarizedExperiment assay assayNames assays assays<- colData
-#'   rowData rowRanges rowRanges<-
 #' @importFrom assertive.base assert_are_identical
 #' @importFrom assertive.files assert_all_are_dirs assert_all_are_existing_files
 #' @importFrom assertive.numbers assert_all_are_greater_than_or_equal_to
@@ -52,13 +41,15 @@
 #'   stripTranscriptVersions theme_midnight theme_paperwhite tx2geneFromGFF
 #' @importFrom bcbioBase assertFormalInterestingGroups bcbio_geom_abline
 #'   bcbio_geom_label bcbio_geom_label_average bcbio_geom_label_repel flatFiles
-#'   prepareSummarizedExperiment readDataVersions readLog readProgramVersions
-#'   readSampleData readTx2gene readYAMLSampleData sanitizeSampleData
-#'   uniteInterestingGroups
+#'   minimalSampleData prepareSummarizedExperiment readDataVersions readLog
+#'   readProgramVersions readSampleData readTx2gene readYAMLSampleData
+#'   sanitizeSampleData sampleDirs uniteInterestingGroups
+#' @importFrom Biobase rowMedians sampleNames
 #' @importFrom cowplot draw_plot ggdraw plot_grid
 #' @importFrom dplyr arrange bind_rows desc everything filter group_by
 #'   group_vars left_join matches mutate mutate_all mutate_if n rename select
 #'   select_if slice summarize summarize_all ungroup
+#' @importFrom GenomicFeatures genes makeTxDbFromGFF transcripts
 #' @importFrom ggplot2 aes_ aes_string coord_flip element_blank element_line
 #'   element_rect element_text expand_limits facet_wrap geom_bar geom_boxplot
 #'   geom_histogram geom_hline geom_line geom_point geom_smooth
@@ -71,16 +62,25 @@
 #' @importFrom jsonlite read_json
 #' @importFrom knitr kable
 #' @importFrom magrittr %>% set_colnames set_names set_rownames
+#' @importFrom Matrix colSums readMM rowMeans rowSums
+#' @importFrom Matrix.utils aggregate.Matrix
 #' @importFrom methods .hasSlot as as<- getMethod is new show slot slot<-
 #'   validObject
 #' @importFrom parallel mclapply mcmapply
 #' @importFrom pbapply pblapply
 #' @importFrom readr read_lines read_tsv
 #' @importFrom rlang !! !!! sym syms UQ
+#' @importFrom S4Vectors DataFrame aggregate as.data.frame as.matrix
+#'   complete.cases mcols metadata metadata<- na.omit
 #' @importFrom scales percent pretty_breaks
+#' @importFrom Seurat Convert CreateSeuratObject FetchData GetDimReduction
+#' @importFrom SingleCellExperiment SingleCellExperiment isSpike isSpike<-
+#'   spikeNames
 #' @importFrom stats ecdf fitted median model.matrix predict relevel reorder
 #'   smooth.spline
 #' @importFrom stringr str_extract str_match str_pad str_split
+#' @importFrom SummarizedExperiment assay assayNames assays assays<- colData
+#'   rowData rowRanges rowRanges<-
 #' @importFrom tibble as_tibble column_to_rownames has_rownames remove_rownames
 #'   rownames_to_column tibble
 #' @importFrom tidyr gather
