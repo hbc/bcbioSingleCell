9ed577e9b8f828a04be85ab56ab4d89199d3aa8f
Michael Steinbaugh <mike@steinbaugh.com>
2018-06-04 16:55:42 -0400

    Update unit tests

--- tests/testthat/test_data_functions.R
+++ tests/testthat/test_data_functions.R
@@ -27,12 +27,16 @@ test_that("fetchPCAData", {
         list(
             sampleID = "factor",
             nGene = "integer",
-            nUMI = "numeric",
+            nUMI = "integer",
+            nCoding = "integer",
+            nMito = "integer",
+            log10GenesPerUMI = "numeric",
+            mitoRatio = "numeric",
             orig.ident = "factor",
             res.0.8 = "character",  # factor
-            res.1 = "character",  # factor
             ident = "factor",
             sampleName = "factor",
+            index = "factor",
             interestingGroups = "factor",
             PC1 = "numeric",
             PC2 = "numeric",
@@ -48,20 +52,24 @@ test_that("fetchPCAData", {
         ) %>%
         tibble::column_to_rownames(.)
     target <- data.frame(
-        sampleID = factor("SeuratProject"),
-        nGene = 47L,
-        nUMI = 70L,
-        orig.ident = factor("SeuratProject"),
-        res.0.8 = "0",
-        res.1 = "0",
-        ident = factor("0", levels = c("0", "1", "2", "3")),
-        sampleName = factor("SeuratProject"),
-        interestingGroups = factor("SeuratProject"),
-        PC1 = 0.443,
-        PC2 = -1.575,
-        centerX = -1.176,
-        centerY = -1.683,
-        row.names = "ATGCCAGAACGACT",
+        sampleID = factor("pbmc4k_1"),
+        nGene = 2785L,
+        nUMI = 12147L,
+        nCoding = 11413L,
+        nMito = 394L,
+        log10GenesPerUMI = 0.843,
+        mitoRatio = 0.032,
+        orig.ident = factor("pbmc4k"),
+        res.0.8 = "3",
+        ident = factor("3", levels = c("0", "1", "2", "3", "4", "5")),
+        sampleName = factor("pbmc4k"),
+        index = factor("1"),
+        interestingGroups = factor("pbmc4k"),
+        PC1 = 2.44,
+        PC2 = -2.471,
+        centerX = 3.03,
+        centerY = -2.471,
+        row.names = "pbmc4k_1_AAACCTGCAGGCGATA",
         stringsAsFactors = FALSE
     )
     # Use `glimpse()` to compare the rounded numbers
@@ -79,12 +87,16 @@ test_that("fetchTSNEData", {
         list(
             sampleID = "factor",
             nGene = "integer",
-            nUMI = "numeric",
+            nUMI = "integer",
+            nCoding = "integer",
+            nMito = "integer",
+            log10GenesPerUMI = "numeric",
+            mitoRatio = "numeric",
             orig.ident = "factor",
             res.0.8 = "character",
-            res.1 = "character",
             ident = "factor",
             sampleName = "factor",
+            index = "factor",
             interestingGroups = "factor",
             tSNE_1 = "numeric",
             tSNE_2 = "numeric",
@@ -100,20 +112,24 @@ test_that("fetchTSNEData", {
         ) %>%
         tibble::column_to_rownames(.)
     target <- data.frame(
-        sampleID = factor("SeuratProject"),
-        nGene = 47L,
-        nUMI = 70L,
-        orig.ident = factor("SeuratProject"),
-        res.0.8 = "0",
-        res.1 = "0",
-        ident = factor("0", levels = c("0", "1", "2", "3")),
-        sampleName = factor("SeuratProject"),
-        interestingGroups = factor("SeuratProject"),
-        tSNE_1 = 14.422,
-        tSNE_2 = 8.336,
-        centerX = -0.332,
-        centerY = 18.76,
-        row.names = "ATGCCAGAACGACT",
+        sampleID = factor("pbmc4k_1"),
+        nGene = 2785L,
+        nUMI = 12147L,
+        nCoding = 11413L,
+        nMito = 394L,
+        log10GenesPerUMI = 0.843,
+        mitoRatio = 0.032,
+        orig.ident = factor("pbmc4k"),
+        res.0.8 = "3",
+        ident = factor("3", levels = c("0", "1", "2", "3", "4", "5")),
+        sampleName = factor("pbmc4k"),
+        index = factor("1"),
+        interestingGroups = factor("pbmc4k"),
+        tSNE_1 = 4.55,
+        tSNE_2 = 2.186,
+        centerX = 4.034,
+        centerY = 1.805,
+        row.names = "pbmc4k_1_AAACCTGCAGGCGATA",
         stringsAsFactors = FALSE
     )
     expect_equal(subset, target)
@@ -133,12 +149,16 @@ test_that("fetchTSNEExpressionData", {
         list(
             sampleID = "factor",
             nGene = "integer",
-            nUMI = "numeric",
+            nUMI = "integer",
+            nCoding = "integer",
+            nMito = "integer",
+            log10GenesPerUMI = "numeric",
+            mitoRatio = "numeric",
             orig.ident = "factor",
             res.0.8 = "character",
-            res.1 = "character",
             ident = "factor",
             sampleName = "factor",
+            index = "factor",
             interestingGroups = "factor",
             tSNE_1 = "numeric",
             tSNE_2 = "numeric",
@@ -156,23 +176,27 @@ test_that("fetchTSNEExpressionData", {
     # The round function will coerce integers to numerics. This is the rationale
     # for the `as.numeric()` usage below.
     target <- data.frame(
-        sampleID = factor("SeuratProject"),
-        nGene = 47L,
-        nUMI = 70L,
-        orig.ident = factor("SeuratProject"),
-        res.0.8 = "0",
-        res.1 = "0",
-        ident = factor("0", levels = c("0", "1", "2", "3")),
-        sampleName = factor("SeuratProject"),
-        interestingGroups = factor("SeuratProject"),
-        tSNE_1 = 14.422,
-        tSNE_2 = 8.336,
-        centerX = -0.332,
-        centerY = 18.76,
-        mean = 0.333,
-        median = 0L,
-        sum = 2L,
-        row.names = "ATGCCAGAACGACT",
+        sampleID = factor("pbmc4k_1"),
+        nGene = 2785L,
+        nUMI = 12147L,
+        nCoding = 11413L,
+        nMito = 394L,
+        log10GenesPerUMI = 0.843,
+        mitoRatio = 0.032,
+        orig.ident = factor("pbmc4k"),
+        res.0.8 = "3",
+        ident = factor("3", levels = c("0", "1", "2", "3", "4", "5")),
+        sampleName = factor("pbmc4k"),
+        index = factor("1"),
+        interestingGroups = factor("pbmc4k"),
+        tSNE_1 = 4.55,
+        tSNE_2 = 2.186,
+        centerX = 4.034,
+        centerY = 1.805,
+        mean = 7.833,
+        median = 2L,
+        sum = 47L,
+        row.names = "pbmc4k_1_AAACCTGCAGGCGATA",
         stringsAsFactors = FALSE
     )
     expect_equal(subset, target)
@@ -240,8 +264,8 @@ test_that("interestingGroups<- : seurat", {
         interestingGroups(seurat_small),
         "sampleName"
     )
-    x <- seurat_small
-    expect_error(interestingGroups(x) <- "sampleName")
+    x <- Seurat::pbmc_small
+    expect_error(interestingGroups(Seurat::pbmc_small) <- "sampleName")
 })
 
 
