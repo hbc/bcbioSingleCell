7340bfa9a804383b596b32a731fbc00e2089e12b
Michael Steinbaugh <mike@steinbaugh.com>
2018-06-04 14:19:29 -0400

    Reorganize data-raw scripts

--- data-raw/03_seurat.R
+++ data-raw/03_seurat.R
@@ -1,8 +1,8 @@
 # seurat_small
-# 2018-05-14
+# 2018-06-04
 library(devtools)
-load_all()
 library(Seurat)
+load_all()
 
 # seurat_small =================================================================
 dims_use <- seq_len(20L)
