2df415ab5f7949473f53bcf1594a3c73f24ebee4
Michael Steinbaugh <mike@steinbaugh.com>
2018-06-04 16:16:44 -0400

    Update NAMESPACE

--- R/bcbioSingleCell-package.R
+++ R/bcbioSingleCell-package.R
@@ -47,8 +47,8 @@
 #' @importFrom Biobase rowMedians sampleNames
 #' @importFrom cowplot draw_plot ggdraw plot_grid
 #' @importFrom dplyr arrange bind_rows desc everything filter group_by
-#'   group_vars left_join matches mutate mutate_all mutate_if n rename select
-#'   select_if slice summarize summarize_all ungroup
+#'   group_vars left_join matches mutate mutate_all mutate_at mutate_if n rename
+#'   select select_if slice summarize summarize_all ungroup
 #' @importFrom GenomicFeatures genes makeTxDbFromGFF transcripts
 #' @importFrom ggplot2 aes_ aes_string coord_flip element_blank element_line
 #'   element_rect element_text expand_limits facet_wrap geom_bar geom_boxplot
