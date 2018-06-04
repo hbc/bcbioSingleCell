5d16eff4a39e00465230f4756884e9ff5b33a517
Michael Steinbaugh <mike@steinbaugh.com>
2018-06-04 13:50:00 -0400

    Resave indrops_small example data

--- data-raw/01_indrops_small.R
+++ data-raw/01_indrops_small.R
@@ -1,72 +1,78 @@
 # indrops_small (harvard-indrop-v3)
-# 2018-05-15
+# 2018-06-04
+library(assertive)
 library(devtools)
-load_all()
 library(tidyverse)
 library(Matrix)
+load_all()
 
 # Include the top 500 genes (rows) and cells (columns)
-uploadDir <- "inst/extdata/indrops"
-sampleName <- "multiplexed-AAAAAAAA"
+upload_dir <- "inst/extdata/indrops"
+sample <- "multiplexed-AAAAAAAA"
 
-matFile <- file.path(
-    uploadDir,
-    sampleName,
-    paste0(sampleName, ".mtx")
+counts_file <- file.path(
+    upload_dir,
+    sample,
+    paste0(sample, ".mtx")
 )
-rownamesFile <- file.path(
-    uploadDir,
-    sampleName,
-    paste0(sampleName, ".mtx.rownames")
+rownames_file <- file.path(
+    upload_dir,
+    sample,
+    paste0(sample, ".mtx.rownames")
 )
-colnamesFile <- file.path(
-    uploadDir,
-    sampleName,
-    paste0(sampleName, ".mtx.colnames")
+colnames_file <- file.path(
+    upload_dir,
+    sample,
+    paste0(sample, ".mtx.colnames")
 )
-barcodesFile <- file.path(
-    uploadDir,
-    sampleName,
-    paste0(sampleName, "-barcodes.tsv")
+barcodes_file <- file.path(
+    upload_dir,
+    sample,
+    paste0(sample, "-barcodes.tsv")
+)
+assert_all_are_existing_files(
+    c(counts_file, rownames_file, colnames_file, barcodes_file)
 )
 
 # Prepare the sparse matrix
-mat <- readMM(matFile)
-rownames <- read_lines(rownamesFile)
-colnames <- read_lines(colnamesFile)
-stopifnot(identical(nrow(mat), length(rownames)))
-stopifnot(identical(ncol(mat), length(colnames)))
-rownames(mat) <- rownames
-colnames(mat) <- colnames
+counts <- readMM(counts_file)
+rownames <- read_lines(rownames_file)
+colnames <- read_lines(colnames_file)
+stopifnot(identical(nrow(counts), length(rownames)))
+stopifnot(identical(ncol(counts), length(colnames)))
+rownames(counts) <- rownames
+colnames(counts) <- colnames
 
 # Subset the matrix to include only the top genes and cells
-countsPerGene <- rowSums(mat) %>%
+top_genes <- rowSums(counts) %>%
     sort(decreasing = TRUE) %>%
     head(n = 500L)
-genes <- sort(names(countsPerGene))
-countsPerCell <- colSums(mat) %>%
+genes <- sort(names(top_genes))
+
+top_cells <- colSums(counts) %>%
     sort(decreasing = TRUE) %>%
     head(n = 500L)
-cells <- sort(names(countsPerCell))
-mat <- mat[genes, cells]
+cells <- sort(names(top_cells))
+
+counts <- counts[genes, cells]
 
 # Update the `barcodes.tsv` file to match
-barcodes <- read_tsv(barcodesFile, col_names = FALSE)
-match <- match(x = colnames(mat), table = barcodes[[1L]])
+barcodes <- read_tsv(barcodes_file, col_names = FALSE)
+match <- match(x = colnames(counts), table = barcodes[[1L]])
 stopifnot(!any(is.na(match)))
 barcodes <- barcodes[match, ]
-stopifnot(identical(colnames(mat), barcodes[[1L]]))
+stopifnot(identical(colnames(counts), barcodes[[1L]]))
 
 # Write update files to disk
-writeMM(mat, file = matFile)
-write_lines(rownames(mat), path = rownamesFile)
-write_lines(colnames(mat), path = colnamesFile)
-write_tsv(barcodes, path = barcodesFile, col_names = FALSE)
+writeMM(counts, file = counts_file)
+write_lines(rownames(counts), path = rownames_file)
+write_lines(colnames(counts), path = colnames_file)
+write_tsv(barcodes, path = barcodes_file, col_names = FALSE)
 
 # indrops_small ================================================================
 indrops_small <- bcbioSingleCell(
-    uploadDir = uploadDir,
-    sampleMetadataFile = file.path(uploadDir, "metadata.csv"),
+    uploadDir = upload_dir,
+    sampleMetadataFile = file.path(upload_dir, "metadata.csv"),
     organism = "Homo sapiens",
     ensemblRelease = 90L
 )
@@ -80,5 +86,8 @@ indrops_small <- filterCells(
     maxMitoRatio = Inf,
     minCellsPerGene = 0
 )
+
+# Require 500 cells, 500 genes
+stopifnot(identical(dim(indrops_small), c(500L, 500L)))
 
 use_data(indrops_small, compress = "xz", overwrite = TRUE)
