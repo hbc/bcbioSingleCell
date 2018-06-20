e206713be94f4501f953a2c3b750ffa09eb206f2
Michael Steinbaugh <mike@steinbaugh.com>
2018-06-20 11:44:36 -0400

    Don't attempt to generate empty metadata for seurat objects

--- R/methods-sampleData.R
+++ R/methods-sampleData.R
@@ -35,10 +35,14 @@ setMethod(
     signature("SingleCellExperiment"),
     function(
         object,
-        clean = TRUE,
         interestingGroups,
+        clean = TRUE,
         return = c("DataFrame", "data.frame", "kable")
     ) {
+        object <- as(object, "SingleCellExperiment")
+        if (missing(interestingGroups)) {
+            interestingGroups <- bcbioBase::interestingGroups(object)
+        }
         assert_is_a_bool(clean)
         return <- match.arg(return)
 
@@ -56,9 +60,6 @@ setMethod(
             data <- data[, setdiff, drop = FALSE]
         } else {
             # Include `interestingGroups` column, if not NULL
-            if (missing(interestingGroups)) {
-                interestingGroups <- bcbioBase::interestingGroups(object)
-            }
             if (length(interestingGroups)) {
                 data <- uniteInterestingGroups(data, interestingGroups)
             }
@@ -85,77 +86,7 @@ setMethod(
 setMethod(
     "sampleData",
     signature("seurat"),
-    function(
-        object,
-        clean = TRUE,
-        interestingGroups,
-        return = c("DataFrame", "data.frame", "kable")
-    ) {
-        return <- match.arg(return)
-
-        # Use stashed sampleData, if created with bcbioSingleCell
-        data <- metadata(object)[["sampleData"]]
-        # Otherwise, generate on the fly
-        if (is.null(data)) {
-            data <- slot(object, "meta.data")
-            assert_is_data.frame(data)
-            # Create priority columns from `orig.ident`, if necessary
-            if (!"sampleName" %in% colnames(data)) {
-                data[["sampleName"]] <- data[["orig.ident"]]
-            }
-            # Remove columns that map to cells and aren't unique per sample
-            blacklist <- paste(
-                c(
-                    "^cellularBarcode$",
-                    "^orig\\.ident$",
-                    "^phase$",
-                    "^res[.0-9]+$"
-                ),
-                collapse = "|"
-            )
-            data <- data %>%
-                remove_rownames() %>%
-                .[, !grepl(x = colnames(.), pattern = blacklist)] %>%
-                mutate_if(is.character, as.factor) %>%
-                select_if(is.factor) %>%
-                mutate_all(droplevels) %>%
-                unique()
-            assert_has_no_duplicates(data[["sampleName"]])
-            rownames(data) <- makeNames(data[["sampleName"]], unique = TRUE)
-            data
-        }
-
-        # Only return factor columns, if desired
-        if (isTRUE(clean)) {
-            data <- data[, vapply(data, is.factor, logical(1L)), drop = FALSE]
-            # Drop remaining blacklisted columns
-            setdiff <- setdiff(colnames(data), bcbioBase::metadataBlacklist)
-            data <- data[, setdiff, drop = FALSE]
-        } else {
-            # Include `interestingGroups` column, if not NULL
-            if (missing(interestingGroups)) {
-                interestingGroups <- bcbioBase::interestingGroups(object)
-                if (is.null(interestingGroups)) {
-                    interestingGroups <- "sampleName"
-                }
-            }
-            if (length(interestingGroups)) {
-                data <- uniteInterestingGroups(data, interestingGroups)
-            }
-        }
-
-        # Arrange rows by `sampleName` column, if defined
-        if ("sampleName" %in% colnames(data)) {
-            data <- data[order(data[["sampleName"]]), , drop = FALSE]
-        }
-
-        # Return
-        if (return == "kable") {
-            kable(as.data.frame(data), row.names = FALSE)
-        } else {
-            as(data, return)
-        }
-    }
+    getMethod("sampleData", "SingleCellExperiment")
 )
 
 
