ffd4adc82d1d570c9ab09ac6b60d9b35d2134cb0
Michael Steinbaugh <mike@steinbaugh.com>
2018-06-21 09:37:09 -0400

    Update travis_success.sh

--- travis_success.sh
+++ travis_success.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-    Rscript -e 'lintr::lint_package()'
-    Rscript -e 'covr::codecov()'
-    R CMD BiocCheck .
-fi
+Rscript -e 'lintr::lint_package()'
+Rscript -e 'covr::codecov()'
+R CMD BiocCheck .
