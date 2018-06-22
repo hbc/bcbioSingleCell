5e10b974b1d6bfe3591d3fcad34bc4da054266fd
Michael Steinbaugh <mike@steinbaugh.com>
2018-06-22 12:12:56 -0400

    Consistently put rownames operations first

--- R/internal-counts.R
+++ R/internal-counts.R
@@ -6,26 +6,27 @@
 
     sampleStem <- basename(sampleDir)
     matrixFile <- file.path(sampleDir, paste0(sampleStem, ".mtx"))
+    rowFile <- paste0(matrixFile, ".rownames")  # features
     colFile <- paste0(matrixFile, ".colnames")  # barcodes
-    rowFile <- paste0(matrixFile, ".rownames")  # transcripts
-    assert_all_are_existing_files(c(matrixFile, colFile, rowFile))
+    assert_all_are_existing_files(c(matrixFile, rowFile, colFile))
 
     # Attempt to load the column and rowname files first. If they're empty,
     # skip loading the MatrixMarket file, which will error otherwise. The bcbio
     # pipeline will output empty files for very low quality samples with no
     # cells that pass filtering.
-    colnames <- read_lines(colFile)
     rownames <- read_lines(rowFile)
-    if (!length(colnames) || !length(rownames)) {
-        warning(paste(sampleID, "does not contain any cells"))
+    colnames <- read_lines(colFile)
+    if (!length(rownames) || !length(colnames)) {
         return(NULL)
     }
 
     counts <- readMM(matrixFile)
-    assert_are_identical(length(colnames), ncol(counts))
+
     assert_are_identical(length(rownames), nrow(counts))
-    colnames(counts) <- colnames
+    assert_are_identical(length(colnames), ncol(counts))
     rownames(counts) <- rownames
+    colnames(counts) <- colnames
+
     counts
 }
 
@@ -69,8 +70,15 @@
         giveCsparse = TRUE
     )
 
-    rownames(counts) <- h5[["genes"]]
-    colnames(counts) <- h5[["barcodes"]]
+    rownames <- h5[["genes"]]
+    colnames <- h5[["barcodes"]]
+    # Move the multiplexed sample index number to the beginning
+    colnames <- sub("^([ACGT]+)-(\\d+)$", "\\2-\\1", colnames)
+    assert_are_identical(length(rownames), nrow(counts))
+    assert_are_identical(length(colnames), ncol(counts))
+    rownames(counts) <- rownames
+    colnames(counts) <- colnames
+
     counts
 }
 
@@ -107,15 +115,11 @@
     assert_is_a_string(genomeBuild)
 
     matrixFile <- file.path(countsDir, genomeBuild, "matrix.mtx")
-    colFile <- file.path(dirname(matrixFile), "barcodes.tsv")
     rowFile <- file.path(dirname(matrixFile), "genes.tsv")
-    assert_all_are_existing_files(c(matrixFile, colFile, rowFile))
+    colFile <- file.path(dirname(matrixFile), "barcodes.tsv")
+    assert_all_are_existing_files(c(matrixFile, rowFile, colFile))
 
-    # `barcodes.tsv` is not tab delimited
-    colnames <- read_lines(colFile)
-    # Move the multiplexed sample index number to the beginning for easier
-    # downstream grep matching
-    colnames <- sub("^([ACGT]+)-(\\d+)$", "\\2-\\1", colnames)
+    counts <- readMM(matrixFile)
 
     # `genes.tsv` is tab delimited
     rownames <- read_tsv(
@@ -126,11 +130,17 @@
     assert_has_rows(rownames)
     rownames <- pull(rownames, "geneID")
 
-    counts <- readMM(matrixFile)
-    assert_are_identical(length(colnames), ncol(counts))
+    # `barcodes.tsv` is not tab delimited
+    colnames <- read_lines(colFile)
+    # Move the multiplexed sample index number to the beginning for easier
+    # downstream grep matching
+    colnames <- sub("^([ACGT]+)-(\\d+)$", "\\2-\\1", colnames)
+
     assert_are_identical(length(rownames), nrow(counts))
-    colnames(counts) <- colnames
+    assert_are_identical(length(colnames), ncol(counts))
     rownames(counts) <- rownames
+    colnames(counts) <- colnames
+
     counts
 }
 
@@ -150,8 +160,8 @@
     pipeline <- match.arg(pipeline)
     format <- match.arg(format)
     assert_is_a_bool(filtered)
-
     message(paste("Reading counts in", format, "format"))
+
     list <- mcmapply(
         sampleDir = sampleDirs,
         sampleID = names(sampleDirs),
