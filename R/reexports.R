f1090ddc29bca1ba8cea7a000cc3732758840bef
Michael Steinbaugh <mike@steinbaugh.com>
2018-07-05 14:28:59 -0400

    Simplify color palette reexports

--- R/reexports.R
+++ R/reexports.R
@@ -12,9 +12,21 @@ ggplot2::ggplot
 #' @export
 ggplot2::scale_color_gradient
 
+#' @importFrom ggplot2 scale_color_gradientn
+#' @export
+ggplot2::scale_color_gradientn
+
 #' @importFrom ggplot2 scale_color_hue
 #' @export
 ggplot2::scale_color_hue
+
+#' @importFrom ggplot2 scale_color_viridis_c
+#' @export
+ggplot2::scale_color_viridis_c
+
+#' @importFrom ggplot2 scale_color_viridis_d
+#' @export
+ggplot2::scale_color_viridis_d
 
 #' @importFrom ggplot2 scale_colour_gradient
 #' @export
@@ -28,9 +40,25 @@ ggplot2::scale_colour_gradientn
 #' @export
 ggplot2::scale_colour_hue
 
+#' @importFrom ggplot2 scale_colour_viridis_c
+#' @export
+ggplot2::scale_colour_viridis_c
+
+#' @importFrom ggplot2 scale_colour_viridis_d
+#' @export
+ggplot2::scale_colour_viridis_d
+
 #' @importFrom ggplot2 scale_fill_hue
 #' @export
 ggplot2::scale_fill_hue
+
+#' @importFrom ggplot2 scale_fill_viridis_c
+#' @export
+ggplot2::scale_fill_viridis_c
+
+#' @importFrom ggplot2 scale_fill_viridis_d
+#' @export
+ggplot2::scale_fill_viridis_d
 
 
 
@@ -43,25 +71,3 @@ R.utils::gzip
 #' @importFrom tibble glimpse
 #' @export
 tibble::glimpse
-
-
-
-#' @importFrom viridis inferno
-#' @export
-viridis::inferno
-
-#' @importFrom viridis scale_color_viridis
-#' @export
-viridis::scale_color_viridis
-
-#' @importFrom viridis scale_colour_viridis
-#' @export
-viridis::scale_colour_viridis
-
-#' @importFrom viridis scale_fill_viridis
-#' @export
-viridis::scale_fill_viridis
-
-#' @importFrom viridis viridis
-#' @export
-viridis::viridis
