ac4e8ffe2b7c2a3520d1af79f1cd2db7308db3b6
Michael Steinbaugh <mike@steinbaugh.com>
2018-07-31 17:56:22 -0400

    Move `.useGeneName()` check to assert

--- R/assert-internal.R
+++ R/assert-internal.R
@@ -56,3 +56,13 @@
         }
     }
 }
+
+
+
+# Figure out which gene ID type is in use (geneID or geneName)
+# Return TRUE if we're working with symbols (not recommended but used by Seurat)
+.useGeneName <- function(object) {
+    geneName <- as.character(gene2symbol(object)[["geneName"]])
+    assert_is_non_empty(geneName)
+    any(geneName %in% rownames(object))
+}
