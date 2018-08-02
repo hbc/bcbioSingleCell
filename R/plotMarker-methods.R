0aad63fce5fa5a8592d1aadc040b7a1ba8876fd1
Michael Steinbaugh <mike@steinbaugh.com>
2018-08-02 15:44:06 -0400

    Use `plotMarker()` as primary method

--- R/plotMarker-methods.R
+++ R/plotMarker-methods.R
@@ -94,192 +94,16 @@ NULL
 
 
 
-# Dimensionality reduction (t-SNE, UMAP) plot constructor
-.plotMarkerReduction <- function(
-    object,
-    genes,
-    reduction = c("TSNE", "UMAP"),
-    expression = c("mean", "median", "sum"),
-    color = getOption("bcbio.discrete.color", NULL),
-    pointSize = getOption("bcbio.pointSize", 0.75),
-    pointAlpha = getOption("bcbio.pointAlpha", 0.8),
-    pointsAsNumbers = FALSE,
-    label = getOption("bcbio.label", TRUE),
-    labelSize = getOption("bcbio.labelSize", 6L),
-    dark = getOption("bcbio.dark", FALSE),
-    grid = getOption("bcbio.grid", FALSE),
-    legend = getOption("bcbio.legend", TRUE),
-    aspectRatio = getOption("bcbio.aspectRatio", 1L),
-    title = TRUE
-) {
-    assert_is_character(genes)
-    assert_has_no_duplicates(genes)
-    assert_is_subset(genes, rownames(object))
-    reduction <- match.arg(reduction)
-    expression <- match.arg(expression)
-    # Legacy support for `color = "auto"`
-    if (identical(color, "auto")) {
-        color <- NULL
-    }
-    assertIsColorScaleContinuousOrNULL(color)
-    assert_is_a_number(pointSize)
-    assert_is_a_number(pointAlpha)
-    assert_is_a_bool(pointsAsNumbers)
-    assert_is_a_bool(label)
-    assert_is_a_number(labelSize)
-    assert_is_a_bool(dark)
-    assert_is_a_bool(legend)
-    assert_is_any_of(title, c("character", "logical", "NULL"))
-    if (is.character(title)) {
-        assert_is_a_string(title)
-    }
-
-    # Fetch dimensional reduction coordinates
-    if (reduction == "TSNE") {
-        fun <- fetchTSNEExpressionData
-        dimCols <- c("tSNE_1", "tSNE_2")
-    } else if (reduction == "UMAP") {
-        fun <- fetchUMAPExpressionData
-        dimCols <- c("UMAP1", "UMAP2")
-    }
-    data <- fun(object = object, genes = genes)
-
-    if (!isTRUE(.useGene2symbol(object))) {
-        g2s <- gene2symbol(object)
-        if (length(g2s)) {
-            g2s <- g2s[genes, , drop = FALSE]
-            genes <- make.unique(g2s[["geneName"]])
-        }
-    }
-    genes <- sort(unique(genes))
-
-    requiredCols <- c(
-        "centerX",
-        "centerY",
-        "mean",
-        "median",
-        "ident",
-        "sum",
-        dimCols
-    )
-    assert_is_subset(requiredCols, colnames(data))
-
-    p <- ggplot(
-        data = data,
-        mapping = aes(
-            x = !!sym(dimCols[[1L]]),
-            y = !!sym(dimCols[[2L]]),
-            color = !!sym(expression)
-        )
-    )
-
-    # Titles
-    subtitle <- NULL
-    if (isTRUE(title)) {
-        if (is_a_string(genes)) {
-            title <- genes
-        } else {
-            title <- NULL
-            subtitle <- genes
-            # Limit to the first 5 markers
-            if (length(subtitle) > 5L) {
-                subtitle <- c(subtitle[1L:5L], "...")
-            }
-            subtitle <- toString(subtitle)
-        }
-    } else if (identical(title, FALSE)) {
-        title <- NULL
-    }
-    p <- p + labs(title = title, subtitle = subtitle)
-
-    # Customize legend
-    if (isTRUE(legend)) {
-        if (is_a_string(genes)) {
-            guideTitle <- "expression"
-        } else {
-            guideTitle <- expression
-        }
-        p <- p + guides(color = guide_colorbar(title = guideTitle))
-    } else {
-        p <- p + guides(color = "none")
-    }
-
-    if (isTRUE(pointsAsNumbers)) {
-        if (pointSize < 4L) pointSize <- 4L
-        p <- p +
-            geom_text(
-                mapping = aes(
-                    x = !!sym(dimCols[[1L]]),
-                    y = !!sym(dimCols[[2L]]),
-                    label = !!sym("ident"),
-                    color = !!sym(expression)
-                ),
-                alpha = pointAlpha,
-                size = pointSize
-            )
-    } else {
-        p <- p +
-            geom_point(
-                alpha = pointAlpha,
-                size = pointSize
-            )
-    }
-
-    if (isTRUE(label)) {
-        if (isTRUE(dark)) {
-            labelColor <- "white"
-        } else {
-            labelColor <- "black"
-        }
-        p <- p +
-            geom_text(
-                mapping = aes(
-                    x = !!sym("centerX"),
-                    y = !!sym("centerY"),
-                    label = !!sym("ident")
-                ),
-                color = labelColor,
-                size = labelSize,
-                fontface = "bold"
-            )
-    }
-
-    # Color palette
-    if (isTRUE(dark)) {
-        theme <- theme_midnight
-        if (is.null(color)) {
-            color <- darkMarkerColors
-        }
-    } else {
-        theme <- theme_paperwhite
-        if (is.null(color)) {
-            color <- lightMarkerColors
-        }
-    }
-    p <- p +
-        theme(
-            aspect_ratio = aspectRatio,
-            grid = grid
-        )
-
-    if (is(color, "ScaleContinuous")) {
-        p <- p + color
-    }
-
-    p
-}
-
-
-
 # Methods ======================================================================
 #' @rdname plotMarker
 #' @export
 setMethod(
-    "plotMarkerTSNE",
+    "plotMarker",
     signature("SingleCellExperiment"),
     function(
         object,
         genes,
+        reducedDim = c("TSNE", "UMAP"),
         expression = c("mean", "median", "sum"),
         color = getOption("bcbio.discrete.color", NULL),
         pointSize = getOption("bcbio.pointSize", 0.75),
@@ -293,22 +117,180 @@ setMethod(
         aspectRatio = getOption("bcbio.aspectRatio", 1L),
         title = TRUE
     ) {
-        .plotMarkerReduction(
+        assert_is_character(genes)
+        assert_has_no_duplicates(genes)
+        assert_is_subset(genes, rownames(object))
+        reducedDim <- match.arg(reducedDim)
+        expression <- match.arg(expression)
+        # Legacy support for `color = "auto"`
+        if (identical(color, "auto")) {
+            color <- NULL
+        }
+        assertIsColorScaleContinuousOrNULL(color)
+        assert_is_a_number(pointSize)
+        assert_is_a_number(pointAlpha)
+        assert_is_a_bool(pointsAsNumbers)
+        assert_is_a_bool(label)
+        assert_is_a_number(labelSize)
+        assert_is_a_bool(dark)
+        assert_is_a_bool(legend)
+        assert_is_any_of(title, c("character", "logical", "NULL"))
+        if (is.character(title)) {
+            assert_is_a_string(title)
+        }
+
+        # Fetch reduced dimension data
+        fun <- get(paste0("fetch", reducedDim, "ExpressionData"))
+        assert_is_function(fun)
+        data <- fun(object = object, genes = genes)
+        dimCols <- colnames(data)[seq_len(2L)]
+
+        if (!isTRUE(.useGene2symbol(object))) {
+            g2s <- gene2symbol(object)
+            if (length(g2s)) {
+                g2s <- g2s[genes, , drop = FALSE]
+                genes <- make.unique(g2s[["geneName"]])
+            }
+        }
+        genes <- sort(unique(genes))
+
+        requiredCols <- c(
+            dimCols,
+            "x",
+            "y",
+            "centerX",
+            "centerY",
+            "mean",
+            "median",
+            "ident",
+            "sum"
+        )
+        assert_is_subset(requiredCols, colnames(data))
+
+        p <- ggplot(
+            data = data,
+            mapping = aes(
+                x = !!sym("x"),
+                y = !!sym("y"),
+                color = !!sym(expression)
+            )
+        )
+
+        # Titles
+        subtitle <- NULL
+        if (isTRUE(title)) {
+            if (is_a_string(genes)) {
+                title <- genes
+            } else {
+                title <- NULL
+                subtitle <- genes
+                # Limit to the first 5 markers
+                if (length(subtitle) > 5L) {
+                    subtitle <- c(subtitle[1L:5L], "...")
+                }
+                subtitle <- toString(subtitle)
+            }
+        } else if (identical(title, FALSE)) {
+            title <- NULL
+        }
+        p <- p +
+            labs(
+                x = dimCols[[1L]],
+                y = dimCols[[2L]],
+                title = title,
+                subtitle = subtitle
+            )
+
+        # Customize legend
+        if (isTRUE(legend)) {
+            if (is_a_string(genes)) {
+                guideTitle <- "expression"
+            } else {
+                guideTitle <- expression
+            }
+            p <- p + guides(color = guide_colorbar(title = guideTitle))
+        } else {
+            p <- p + guides(color = "none")
+        }
+
+        if (isTRUE(pointsAsNumbers)) {
+            if (pointSize < 4L) pointSize <- 4L
+            p <- p +
+                geom_text(
+                    mapping = aes(
+                        x = !!sym("x"),
+                        y = !!sym("y"),
+                        label = !!sym("ident"),
+                        color = !!sym(expression)
+                    ),
+                    alpha = pointAlpha,
+                    size = pointSize
+                )
+        } else {
+            p <- p +
+                geom_point(
+                    alpha = pointAlpha,
+                    size = pointSize
+                )
+        }
+
+        if (isTRUE(label)) {
+            if (isTRUE(dark)) {
+                labelColor <- "white"
+            } else {
+                labelColor <- "black"
+            }
+            p <- p +
+                geom_text(
+                    mapping = aes(
+                        x = !!sym("centerX"),
+                        y = !!sym("centerY"),
+                        label = !!sym("ident")
+                    ),
+                    color = labelColor,
+                    size = labelSize,
+                    fontface = "bold"
+                )
+        }
+
+        # Color palette
+        if (isTRUE(dark)) {
+            theme <- theme_midnight
+            if (is.null(color)) {
+                color <- darkMarkerColors
+            }
+        } else {
+            theme <- theme_paperwhite
+            if (is.null(color)) {
+                color <- lightMarkerColors
+            }
+        }
+        p <- p +
+            theme(
+                aspect_ratio = aspectRatio,
+                grid = grid
+            )
+
+        if (is(color, "ScaleContinuous")) {
+            p <- p + color
+        }
+
+        p
+    }
+)
+
+
+
+#' @rdname plotMarker
+#' @export
+setMethod(
+    "plotMarkerTSNE",
+    signature("SingleCellExperiment"),
+    function(object, ...) {
+        plotMarker(
             object = object,
-            genes = genes,
-            reduction = "TSNE",
-            expression = expression,
-            color = color,
-            pointSize = pointSize,
-            pointAlpha = pointAlpha,
-            pointsAsNumbers = pointsAsNumbers,
-            label = label,
-            labelSize = labelSize,
-            dark = dark,
-            grid = grid,
-            legend = legend,
-            aspectRatio = aspectRatio,
-            title = title
+            reducedDim = "TSNE",
+            ...
         )
     }
 )
@@ -320,38 +302,11 @@ setMethod(
 setMethod(
     "plotMarkerUMAP",
     signature("SingleCellExperiment"),
-    function(
-        object,
-        genes,
-        expression = c("mean", "median", "sum"),
-        color = getOption("bcbio.discrete.color", NULL),
-        pointSize = getOption("bcbio.pointSize", 0.75),
-        pointAlpha = getOption("bcbio.pointAlpha", 0.8),
-        pointsAsNumbers = FALSE,
-        label = getOption("bcbio.label", TRUE),
-        labelSize = getOption("bcbio.labelSize", 6L),
-        dark = getOption("bcbio.dark", FALSE),
-        grid = getOption("bcbio.grid", FALSE),
-        legend = getOption("bcbio.legend", TRUE),
-        aspectRatio = getOption("bcbio.aspectRatio", 1L),
-        title = TRUE
-    ) {
-        .plotMarkerReduction(
+    function(object, ...) {
+        plotMarker(
             object = object,
-            genes = genes,
-            reduction = "UMAP",
-            expression = expression,
-            color = color,
-            pointSize = pointSize,
-            pointAlpha = pointAlpha,
-            pointsAsNumbers = pointsAsNumbers,
-            label = label,
-            labelSize = labelSize,
-            dark = dark,
-            grid = grid,
-            legend = legend,
-            aspectRatio = aspectRatio,
-            title = title
+            reducedDim = "UMAP",
+            ...
         )
     }
 )
