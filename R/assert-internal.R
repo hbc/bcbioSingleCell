707dee22e4dc7a54a13fe33ab36ecace915f65e8
Michael Steinbaugh <mike@steinbaugh.com>
2018-08-02 19:15:09 -0400

    Simplify assertive checks

--- R/assert-internal.R
+++ R/assert-internal.R
@@ -1,9 +1,3 @@
-.assertHasIdent <- function(object) {
-    assert_is_subset("ident", colnames(colData(object)))
-}
-
-
-
 .isAggregate <- function(object, stop = FALSE) {
     logical <- "aggregate" %in% colnames(object)
     if (
@@ -13,58 +7,4 @@
         stop("`aggregate` column is required")
     }
     logical
-}
-
-
-
-.isSanitizedMarkers <- function(
-    object,
-    package = "Seurat"
-) {
-    package <- match.arg(package)
-
-    # General checks ===========================================================
-    if (!is(object, "grouped_df")) {
-        return(FALSE)
-    } else if (
-        is.null(attr(object, "vars")) ||
-        attr(object, "vars") != "cluster"
-    ) {
-        return(FALSE)
-    } else if (!"geneID" %in% colnames(object)) {
-        return(FALSE)
-    }
-
-    # Package-specific checks ==================================================
-    if (package == "Seurat") {
-        # Check for `Seurat::FindAllMarkers()` return.
-        # These columns are output in an inconsistent format, so we'll sanitize
-        # into lowerCamelCase.
-        seuratBlacklist <- c(
-            "avg_diff",   # Legacy, now "avg_logFC"
-            "avg_logFC",  # Renamed in v2.1
-            "gene",
-            "p_val",      # We'll rename to pvalue, matching DESeq2
-            "p_val_adj",  # New in v2.1, we'll rename to padj, matching DESeq2
-            "pct.1",
-            "pct.2"
-        )
-        if (any(seuratBlacklist %in% colnames(object))) {
-            return(FALSE)
-        } else {
-            return(TRUE)
-        }
-    }
-}
-
-
-
-# Determine whether we should use stashed gene-to-symbol mappings
-.useGene2symbol <- function(object) {
-    geneName <- as.character(
-        suppressWarnings(
-            gene2symbol(object)[["geneName"]]
-        )
-    )
-    any(geneName %in% rownames(object))
 }
