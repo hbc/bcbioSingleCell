d244d4d3ddf4f4a352adfabf80fe52461cc1a8af
Michael Steinbaugh <mike@steinbaugh.com>
2018-08-02 15:53:31 -0400

    Export `plotFeature()`

--- R/plotFeature-methods.R
+++ R/plotFeature-methods.R
@@ -26,147 +26,14 @@ NULL
 
 
 
-# Constructors =================================================================
-.plotFeatureReduction <- function(
-    object,
-    features,
-    reduction = c("TSNE", "UMAP"),
-    color = getOption("bcbio.discrete.color", NULL),
-    pointSize = getOption("bcbio.pointSize", 0.75),
-    pointAlpha = getOption("bcbio.pointAlpha", 0.75),
-    label = getOption("bcbio.label", TRUE),
-    labelSize = getOption("bcbio.labelSize", 6L),
-    dark = getOption("bcbio.dark", FALSE),
-    grid = getOption("bcbio.grid", FALSE),
-    legend = getOption("bcbio.legend", FALSE),
-    aspectRatio = getOption("bcbio.aspectRatio", 1L)
-) {
-    assert_is_character(features)
-    # Legacy support for `color = "auto"`
-    if (identical(color, "auto")) {
-        warning("Use `NULL` instead of `\"auto\"` for `color`")
-        color <- NULL
-    }
-    assertIsColorScaleContinuousOrNULL(color)
-    reduction <- match.arg(reduction)
-
-    if (isTRUE(dark)) {
-        fill <- "black"
-    } else {
-        fill <- "white"
-    }
-
-    # Get reduced dimension coordinates (t-SNE and UMAP are supported)
-    if (reduction == "TSNE") {
-        fxn <- fetchTSNEData
-        dimCols <- c("tSNE_1", "tSNE_2")
-    } else if (reduction == "UMAP") {
-        fxn <- fetchUMAPData
-        dimCols <- c("UMAP1", "UMAP2")
-    }
-    data <- fxn(object)
-
-    # If the features are not defined, attempt to merge all reduced dims
-    # information before stopping
-    if (!all(features %in% colnames(data))) {
-        reducedDimsData <- do.call(
-            what = cbind,
-            args = reducedDims(object)
-        )
-        stopifnot(identical(rownames(data), rownames(reducedDimsData)))
-        data <- cbind(data, reducedDimsData) %>%
-            # Ensure columns are unique
-            .[, unique(colnames(.))]
-    }
-    assert_is_subset(features, colnames(data))
-
-    plotlist <- lapply(features, function(feature) {
-        p <- ggplot(
-            data = data,
-            mapping = aes(
-                x = !!sym(dimCols[[1L]]),
-                y = !!sym(dimCols[[2L]]),
-                color = !!sym(feature)
-            )
-        ) +
-            geom_point(
-                alpha = pointAlpha,
-                size = pointSize
-            ) +
-            labs(title = feature)
-
-        if (isTRUE(label)) {
-            if (isTRUE(dark)) {
-                labelColor <- "white"
-            } else {
-                labelColor <- "black"
-            }
-            p <- p +
-                geom_text(
-                    mapping = aes(
-                        x = !!sym("centerX"),
-                        y = !!sym("centerY"),
-                        label = !!sym("ident")
-                    ),
-                    color = labelColor,
-                    size = labelSize,
-                    fontface = "bold"
-                )
-        }
-
-        if (isTRUE(dark)) {
-            theme <- theme_midnight
-            if (is.null(color)) {
-                color <- darkMarkerColors
-            }
-        } else {
-            theme <- theme_paperwhite
-            if (is.null(color)) {
-                color <- lightMarkerColors
-            }
-        }
-        p <- p +
-            theme(
-                aspect_ratio = aspectRatio,
-                grid = grid
-            )
-
-        if (is(color, "ScaleContinuous")) {
-            p <- p + color
-        }
-
-        if (!isTRUE(legend)) {
-            p <- p + guides(color = "none")
-        }
-
-        p
-    })
-
-    # Return ===================================================================
-    if (length(features) > 1L) {
-        plot_grid(plotlist = plotlist) +
-            theme(
-                plot.background = element_rect(
-                    color = NA,
-                    fill = fill
-                )
-            )
-    } else {
-        plotlist[[1L]]
-    }
-}
-
-
-
 # Methods ======================================================================
-#' @rdname plotFeature
-#' @export
 setMethod(
-    "plotFeatureTSNE",
+    "plotFeature",
     signature("SingleCellExperiment"),
     function(
         object,
         features,
+        reducedDim = c("TSNE", "UMAP"),
         color = getOption("bcbio.discrete.color", NULL),
         pointSize = getOption("bcbio.pointSize", 0.75),
         pointAlpha = getOption("bcbio.pointAlpha", 0.75),
@@ -177,20 +44,116 @@ setMethod(
         legend = getOption("bcbio.legend", FALSE),
         aspectRatio = getOption("bcbio.aspectRatio", 1L)
     ) {
-        .plotFeatureReduction(
-            object = object,
-            features = features,
-            reduction = "TSNE",
-            color = color,
-            pointSize = pointSize,
-            pointAlpha = pointAlpha,
-            label = label,
-            labelSize = labelSize,
-            dark = dark,
-            grid = grid,
-            legend = legend,
-            aspectRatio = aspectRatio
-        )
+        assert_is_character(features)
+        # Legacy support for `color = "auto"`
+        if (identical(color, "auto")) {
+            warning("Use `NULL` instead of `\"auto\"` for `color`")
+            color <- NULL
+        }
+        assertIsColorScaleContinuousOrNULL(color)
+        reducedDim <- match.arg(reducedDim)
+
+        if (isTRUE(dark)) {
+            fill <- "black"
+        } else {
+            fill <- "white"
+        }
+
+        data <- fetchReducedDimData(object, reducedDim = reducedDim)
+        axes <- colnames(data)[seq_len(2L)]
+
+        # If the features are not defined, attempt to merge all reduced dims
+        # information before stopping
+        if (!all(features %in% colnames(data))) {
+            reducedDimsData <- do.call(
+                what = cbind,
+                args = reducedDims(object)
+            )
+            stopifnot(identical(rownames(data), rownames(reducedDimsData)))
+            data <- cbind(data, reducedDimsData) %>%
+                # Ensure columns are unique
+                .[, unique(colnames(.))]
+        }
+        assert_is_subset(features, colnames(data))
+
+        plotlist <- lapply(features, function(feature) {
+            p <- ggplot(
+                data = data,
+                mapping = aes(
+                    x = !!sym("x"),
+                    y = !!sym("y"),
+                    color = !!sym(feature)
+                )
+            ) +
+                geom_point(
+                    alpha = pointAlpha,
+                    size = pointSize
+                ) +
+                labs(
+                    x = axes[[1L]],
+                    y = axes[[2L]],
+                    title = feature
+                )
+
+            if (isTRUE(label)) {
+                if (isTRUE(dark)) {
+                    labelColor <- "white"
+                } else {
+                    labelColor <- "black"
+                }
+                p <- p +
+                    geom_text(
+                        mapping = aes(
+                            x = !!sym("centerX"),
+                            y = !!sym("centerY"),
+                            label = !!sym("ident")
+                        ),
+                        color = labelColor,
+                        size = labelSize,
+                        fontface = "bold"
+                    )
+            }
+
+            if (isTRUE(dark)) {
+                theme <- theme_midnight
+                if (is.null(color)) {
+                    color <- darkMarkerColors
+                }
+            } else {
+                theme <- theme_paperwhite
+                if (is.null(color)) {
+                    color <- lightMarkerColors
+                }
+            }
+            p <- p +
+                theme(
+                    aspect_ratio = aspectRatio,
+                    grid = grid
+                )
+
+            if (is(color, "ScaleContinuous")) {
+                p <- p + color
+            }
+
+            if (!isTRUE(legend)) {
+                p <- p + guides(color = "none")
+            }
+
+            p
+        })
+
+        # Return ===================================================================
+        if (length(features) > 1L) {
+            plot_grid(plotlist = plotlist) +
+                theme(
+                    plot.background = element_rect(
+                        color = NA,
+                        fill = fill
+                    )
+                )
+        } else {
+            plotlist[[1L]]
+        }
     }
 )
 
@@ -200,8 +163,14 @@ setMethod(
 #' @export
 setMethod(
     "plotFeatureTSNE",
-    signature("seurat"),
-    getMethod("plotFeatureTSNE", "SingleCellExperiment")
+    signature("SingleCellExperiment"),
+    function(object, ...) {
+        plotFeature(
+            object = object,
+            reducedDim = "TSNE",
+            ...
+        )
+    }
 )
 
 
@@ -211,36 +180,34 @@ setMethod(
 setMethod(
     "plotFeatureUMAP",
     signature("SingleCellExperiment"),
-    function(
-        object,
-        features,
-        color = getOption("bcbio.discrete.color", NULL),
-        pointSize = getOption("bcbio.pointSize", 0.75),
-        pointAlpha = getOption("bcbio.pointAlpha", 0.75),
-        label = getOption("bcbio.label", TRUE),
-        labelSize = getOption("bcbio.labelSize", 6L),
-        dark = getOption("bcbio.dark", FALSE),
-        grid = getOption("bcbio.grid", FALSE),
-        legend = getOption("bcbio.legend", FALSE),
-        aspectRatio = getOption("bcbio.aspectRatio", 1L)
-    ) {
-        .plotFeatureReduction(
+    function(object, ...) {
+        plotFeature(
             object = object,
-            features = features,
-            reduction = "UMAP",
-            color = color,
-            pointSize = pointSize,
-            pointAlpha = pointAlpha,
-            label = label,
-            labelSize = labelSize,
-            dark = dark,
-            grid = grid,
-            legend = legend,
-            aspectRatio = aspectRatio
+            reducedDim = "UMAP",
+            ...
         )
     }
 )
 
+
+
+
+
+#' @rdname plotFeature
+#' @export
+setMethod(
+    "plotFeature",
+    signature("seurat"),
+    getMethod("plotFeature", "SingleCellExperiment")
+)
+
+#' @rdname plotFeature
+#' @export
+setMethod(
+    "plotFeatureTSNE",
+    signature("seurat"),
+    getMethod("plotFeatureTSNE", "SingleCellExperiment")
+)
 
 
 #' @rdname plotFeature
