266cc83d2ac6b695b60ea45e8b9427bb29e0593a
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-09-12 13:58:24 -0400

    Resave indrops_small

--- data-raw/indrops.R
+++ data-raw/indrops.R
@@ -1,7 +1,3 @@
-# FIXME Resave example without `sampleData` slotted in metadata.
-
-
-
 # inDrops example data
 # Using harvard-indrop-v3 barcodes
 # 2018-09-12
@@ -10,10 +6,8 @@ library(tidyverse)
 library(Seurat)
 library(Matrix)
 
-
-
 # Minimal example bcbio upload directory =======================================
-# Include the top 500 genes (rows) and cells (columns)
+# Include the top 500 genes (rows) and cells (columns).
 upload_dir <- "inst/extdata/indrops"
 sample <- "multiplexed-AAAAAAAA"
 
@@ -42,7 +36,7 @@ stopifnot(all(file.exists(
     c(counts_file, rownames_file, colnames_file, barcodes_file)
 )))
 
-# Prepare the sparse matrix
+# Prepare the sparse matrix.
 counts <- readMM(counts_file)
 rownames <- read_lines(rownames_file)
 colnames <- read_lines(colnames_file)
@@ -51,7 +45,7 @@ stopifnot(identical(ncol(counts), length(colnames)))
 rownames(counts) <- rownames
 colnames(counts) <- colnames
 
-# Subset the matrix to include only the top genes and cells
+# Subset the matrix to include only the top genes and cells.
 top_genes <- Matrix::rowSums(counts) %>%
     sort(decreasing = TRUE) %>%
     head(n = 500L)
@@ -64,29 +58,27 @@ cells <- sort(names(top_cells))
 
 counts <- counts[genes, cells]
 
-# Update the `barcodes.tsv` file to match
+# Update the `barcodes.tsv` file to match.
 barcodes <- read_tsv(barcodes_file, col_names = FALSE)
 match <- match(x = colnames(counts), table = barcodes[[1L]])
 stopifnot(!any(is.na(match)))
 barcodes <- barcodes[match, ]
 stopifnot(identical(colnames(counts), barcodes[[1L]]))
 
-# Write update files to disk
+# Write update files to disk.
 writeMM(counts, file = counts_file)
 write_lines(rownames(counts), path = rownames_file)
 write_lines(colnames(counts), path = colnames_file)
 write_tsv(barcodes, path = barcodes_file, col_names = FALSE)
 
-
-
-# bcbioRNASeq object ===========================================================
+# bcbioSingleCell object =======================================================
 bcb <- bcbioSingleCell(
     uploadDir = upload_dir,
     sampleMetadataFile = file.path(upload_dir, "metadata.csv"),
     organism = "Homo sapiens",
     ensemblRelease = 90L
 )
-# Apply example filtering without excluding any cells
+# Apply example filtering without excluding any cells.
 bcb <- filterCells(bcb)
 stopifnot(identical(dim(bcb), c(500L, 500L)))
 
