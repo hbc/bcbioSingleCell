7b90e3cbb5aedb9af6a4c21f8286f777dcd11ea8
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-09-12 13:50:40 -0400

    Improve the combine method

--- R/combine-methods.R
+++ R/combine-methods.R
@@ -1,55 +1,43 @@
-# FIXME Export this in basejump package for `SummarizedExperiment`.
-
-
-
-#' Combine Multiple `SingleCellExperiment ` Objects
-#'
-#' @note We're attempting to make this as strict as possible, requiring:
-#'
-#' - Rows (genes) across objects must be identical.
-#' - Organism and genome build must be identical.
-#' - rowRanges metadata must be identical.
-#' - colData (cellular barcode metrics) must contain the same columns.
-#' - sampleData must contain the same columns.
+#' Combine Multiple Objects
 #'
 #' @name combine
 #' @author Michael Steinbaugh
 #' @importFrom BiocGenerics combine
+#' @importMethodsFrom basejump combine
 #' @export
 #'
-#' @inheritParams BiocGenerics::combine
+#' @inherit basejump::combine
 #'
-#' @param metadata `character`. Metadata slot names that must be identical
-#'   between the datasets.
-#'
-#' @seealso `help("merge.Matrix", "Matrix.utils")`.
+#' @seealso
+#' - [BiocGenerics::combine()].
+#' - `help("merge.Matrix", "Matrix.utils")`.
 #'
 #' @return `SingleCellExperiment`.
 #'
 #' @examples
 #' # For this simple working example, let's duplicate our minimal dataset,
-#' # modify the cell barcodes, and combine
+#' # modify the cell barcodes, and combine.
 #'
 #' x <- indrops_small
 #' validObject(x)
 #' sampleData(x)
 #' sampleNames(x)
 #'
-#' # Here we're faking a distinct replicate, just as an example
+#' # Here we're faking a distinct replicate, just as an example.
 #' y <- indrops_small
-#' # Modify the cellular barcodes prefix
+#' # Modify the cellular barcodes prefix.
 #' colnames(y) <- gsub(
 #'     pattern = "^multiplexed_AAAAAAAA",
 #'     replacement = "multiplexed_CCCCCCCC",
 #'     x = colnames(y)
 #' )
-#' # Modify the column data
+#' # Modify the column data.
 #' colData(y)[["description"]] <- factor("multiplexed-CCCCCCCC")
 #' colData(y)[["sampleID"]] <- factor("multiplexed_CCCCCCCC")
 #' colData(y)[["sampleName"]] <- factor("rep_2")
 #' colData(y)[["sequence"]] <- factor("GGGGGGGG")
 #' colData(y)[["revcomp"]] <- factor("CCCCCCCC")
-#' # Modify the sample data
+#' # Modify the sample data.
 #' sampleData(y)[["sampleName"]] <- factor("rep_2")
 #' sampleData(y)[["description"]] <- factor("multiplexed-CCCCCCCC")
 #' sampleData(y)[["sequence"]] <- factor("GGGGGGGG")
@@ -59,7 +47,7 @@
 #' sampleData(y)
 #' sampleNames(y)
 #'
-#' # Combine two SingleCellExperiment objects
+#' # Combine two SingleCellExperiment objects.
 #' c <- combine(x, y)
 #' print(c)
 #' sampleNames(c)
@@ -95,54 +83,18 @@ setMethod(
     ) {
         assert_is_character(metadata)
 
-        # Coerce the objects to SingleCellExperiment.
-        x <- as(x, "SingleCellExperiment")
-        y <- as(y, "SingleCellExperiment")
-
-        # Currently we're being strict and requiring that the rows (features)
-        # are identical, otherwise zero counts may be misleading.
-        assert_are_identical(rownames(x), rownames(y))
-
-        # Require that there are no duplicate cells.
-        assert_are_disjoint_sets(colnames(x), colnames(y))
-
-        # Require that specific metadata is identical.
-        assert_are_identical(
-            x = metadata(x)[metadata],
-            y = metadata(y)[metadata]
+        # Use our SummarizedExperiment method to combine.
+        assert_are_identical(class(x), class(y))
+        Class <- "RangedSummarizedExperiment"  # nolint
+        rse <- combine(
+            x = as(object = x, Class = Class),
+            y = as(object = y, Class = Class)
         )
+        counts <- counts(rse)
+        colData <- colData(rse)
+        rowRanges <- rowRanges(rse)
 
-        # Counts ---------------------------------------------------------------
-        # Check that count matrices are identical format, then combine.
-        assert_are_identical(
-            x = class(counts(x)),
-            y = class(counts(y))
-        )
-        counts <- cbind(counts(x), counts(y))
-
-        # Row data -------------------------------------------------------------
-        # Require that the gene annotations are identical.
-        assert_are_identical(
-            x = rowRanges(x),
-            y = rowRanges(y)
-        )
-        rowRanges <- rowRanges(x)
-
-        # Column data ----------------------------------------------------------
-        assert_are_set_equal(
-            x = colnames(colData(x)),
-            y = colnames(colData(y))
-        )
-        cols <- intersect(
-            x = colnames(colData(x)),
-            y = colnames(colData(y))
-        )
-        colData <- rbind(
-            colData(x)[, cols, drop = FALSE],
-            colData(y)[, cols, drop = FALSE]
-        )
-
-        # Sample data ----------------------------------------------------------
+        # Update cell2sample mappings ------------------------------------------
         assert_are_set_equal(
             x = colnames(sampleData(x)),
             y = colnames(sampleData(y))
@@ -155,8 +107,6 @@ setMethod(
             sampleData(x)[, cols, drop = FALSE],
             sampleData(y)[, cols, drop = FALSE]
         )
-
-        # cell2sample ----------------------------------------------------------
         cell2sample <- .mapCellsToSamples(
             cells = colnames(counts),
             samples = rownames(sampleData)
