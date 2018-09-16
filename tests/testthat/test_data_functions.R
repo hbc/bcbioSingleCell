7d0855da6c8c74b49781119319f1ccfbe5028303
Michael Steinbaugh <mike@steinbaugh.com>
2018-09-16 13:52:23 -0400

    Update unit test

--- tests/testthat/test_data_functions.R
+++ tests/testthat/test_data_functions.R
@@ -2,15 +2,15 @@ context("Data Functions")
 
 
 
-# aggregateReplicates ==========================================================
-test_that("aggregateReplicates", {
-    x <- aggregateReplicates(indrops_small)
+# aggregateCols ==========================================================
+test_that("aggregateCols", {
+    x <- aggregateCols(indrops_small)
     expect_s4_class(x, "SingleCellExperiment")
     expect_identical(
         dim(x),
         c(500L, 500L)
     )
-    map <- metadata(x)[["aggregateReplicates"]]
+    map <- metadata(x)[["aggregateCols"]]
     expect_is(map, "factor")
     expect_identical(length(map), 500L)
     expect_identical(length(levels(map)), 500L)
