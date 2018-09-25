ce5ac4d160b40d246de5595384d123c4a5f27e39
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-09-25 17:22:01 -0400

    Draft update improving cellranger sample loading

--- R/import-internal.R
+++ R/import-internal.R
@@ -4,16 +4,21 @@
         assert_all_are_dirs(sampleDirs)
         assert_has_names(sampleDirs)
         message("Importing counts...")
-        list <- mapply(
-            sampleID = names(sampleDirs),
-            dir = sampleDirs,
-            FUN = function(sampleID, dir) {
-                counts <- .import.bcbio.mtx(sampleID, dir)
+
+        # FIXME
+        stop("BIND THE SAMPLE IDs AFTER")
+
+        list <- lapply(
+            X = sampleDirs,
+            FUN = function(dir) {
+                .import.bcbio.mtx(dir)
             }
         )
+
         # Remove any empty items in list, which can result from low quality
         # samples with empty matrices in bcbio pipeline.
         list <- Filter(Negate(is.null), list)
+
         # Bind the matrices.
         do.call(cbind, list)
     }
@@ -61,17 +66,15 @@
 # This may be advantagenous to loading the giant combined matrix because we
 # can parallelize with BiocParallel.
 .import.bcbio.mtx <-  # nolint
-    function(sampleID, dir) {
-        assert_is_a_string(sampleID)
-        assertAllAreValidNames(sampleID)
+    function(dir) {
         assert_is_a_string(dir)
         assert_all_are_dirs(dir)
 
         # Require that all of the files exist, even if they are empty.
-        matrixFile <- file.path(dir, paste0(basename(dir), ".mtx"))
-        rowFile <- paste0(matrixFile, ".rownames")
-        colFile <- paste0(matrixFile, ".colnames")
-        assert_all_are_existing_files(c(matrixFile, rowFile, colFile))
+        file <- file.path(dir, paste0(basename(dir), ".mtx"))
+        rownamesFile <- paste0(file, ".rownames")
+        colnamesFile <- paste0(file, ".colnames")
+        assert_all_are_existing_files(c(file, rownamesFile, colnamesFile))
 
         # Attempt to load the column and rowname files first. If they're empty,
         # skip loading the MatrixMarket file, which will error otherwise. The
@@ -79,54 +82,64 @@
         # with no cells that pass filtering.
 
         # Import Genes/transcripts (features).
-        rownames <- read_lines(rowFile)
+        rownames <- read_lines(rownamesFile)
 
         # Import cellular barcodes.
-        colnames <- read_lines(colFile)
+        colnames <- read_lines(colnamesFile)
 
         if (!length(rownames) > 0L || !length(colnames) > 0L) {
-            message(paste("Skipped", sampleID))
+            message(paste("Skipped", basename(dir)))
             return(NULL)
         }
 
         # Import counts.
-        counts <- readMM(matrixFile)
-        assert_is_all_of(counts, "sparseMatrix")
+        counts <- readMM(file)
 
         assert_are_identical(length(rownames), nrow(counts))
         assert_are_identical(length(colnames), ncol(counts))
 
-        # Ensure rows are valid.
-        rownames(counts) <- rownames %>%
-            makeNames(unique = TRUE)
+        rownames(counts) <- rownames
+        colnames(counts) <- colnames
 
-        # Prefix cell barcodes with sample ID and make valid.
-        colnames(counts) <- colnames %>%
-            paste(sampleID, ., sep = "_") %>%
-            makeNames(unique = TRUE)
+        message(paste0("Imported ", basename(dir)))
 
-        message(paste0("Imported ", sampleID))
         counts
     }
 
 
 
 # Cell Ranger ==================================================================
-# FIXME Need to add a simple mode, like `Seurat::Read10X()`.
-
 .import.cellranger <-  # nolint
-    function(
-        sampleDirs,
-        format = c("mtx", "h5"),
-        filtered = TRUE
-    ) {
-        assert_all_are_dirs(sampleDirs)
-        assert_has_names(sampleDirs)
+    function(sampleFiles) {
+        assert_all_are_existing_files(sampleFiles)
+        assert_has_names(sampleFiles)
 
-        format <- match.arg(format)
-        assert_is_a_bool(filtered)
+        if (all(grepl("\\.mtx$", sampleFiles))) {
+            fun <- .import.cellranger.mtx
+        } else if (all(grepl("\\.h5$", sampleFiles))) {
+            fun <- .import.cellranger.h5
+        }
 
-        message(paste("Reading counts in", format, "format"))
+        list <- mapply(
+            sampleID = names(sampleFiles),
+            file = sampleFiles,
+            FUN = function(sampleID, file) {
+                counts <- fun(file)
+                rownames(counts) <-
+                    makeNames(rownames(counts), unique = TRUE)
+                colnames(counts) <-
+                    makeNames(
+                        paste(sampleID, colnames(counts), sep = "_"),
+                        unique = TRUE
+                    )
+                counts
+            },
+            SIMPLIFY = FALSE,
+            USE.NAMES = TRUE
+        )
+
+        # Bind the matrices.
+        do.call(cbind, list)
     }
 
 
@@ -134,32 +147,18 @@
 # Import Cell Ranger HDF5 Counts
 # @seealso `cellrangerRkit::get_matrix_from_h5()`
 .import.cellranger.h5 <-  # nolint
-    function(sampleDir, filtered = TRUE) {
-        assert_all_are_dirs(sampleDir)
-        assert_is_a_string(sampleDir)
-        assert_is_a_bool(filtered)
-
-        if (isTRUE(filtered)) {
-            prefix <- "filtered"
-        } else {
-            prefix <- "raw"
-        }
-
-        file <- file.path(
-            sampleDir,
-            "outs",
-            paste0(prefix, "_gene_bc_matrices_h5.h5")
-        )
+    function(file) {
+        assert_is_a_string(file)
         assert_all_are_existing_files(file)
-        message(file)
 
+        # Get the genome build, which we need to pass as "name".
         genomeBuild <- names(h5dump(file, load = FALSE))
         assert_is_a_string(genomeBuild)
 
-        # name e.g. "/GRCh38/data"
+        # Use genome build name (e.g. "/GRCh38/data").
         h5 <- h5read(file = file, name = genomeBuild)
 
-        # Want `Csparse` instead of `Tsparse` generally.
+        # Want `Csparse` not `Tsparse` matrix.
         counts <- sparseMatrix(
             i = h5[["indices"]] + 1L,
             p = h5[["indptr"]],
@@ -170,10 +169,13 @@
 
         rownames <- h5[["genes"]]
         colnames <- h5[["barcodes"]]
+
         # Move the multiplexed sample index number to the beginning.
         colnames <- sub("^([ACGT]+)-(\\d+)$", "\\2-\\1", colnames)
+
         assert_are_identical(length(rownames), nrow(counts))
         assert_are_identical(length(colnames), ncol(counts))
+
         rownames(counts) <- rownames
         colnames(counts) <- colnames
 
@@ -185,56 +187,34 @@
 # Import Cell Ranger Sparse Counts
 # Matrix Market Exchange (MEX/MTX) format.
 .import.cellranger.mtx <-
-    function(sampleDir, filtered = TRUE) {
-        assert_is_a_string(sampleDir)
-        assert_all_are_dirs(sampleDir)
-        assert_is_a_bool(filtered)
+    function(file) {
+        assert_is_a_string(file)
+        assert_all_are_existing_files(file)
 
-        if (isTRUE(filtered)) {
-            prefix <- "filtered"
-        } else {
-            prefix <- "raw"
-        }
+        # Locate required sidecar files.
+        barcodesFile <- file.path(dirname(file), "barcodes.tsv")
+        genesFile <- file.path(dirname(file), "genes.tsv")
+        assert_all_are_existing_files(c(barcodesFile, genesFile))
 
-        countsDir <- file.path(
-            sampleDir,
-            "outs",
-            paste0(prefix, "_gene_bc_matrices")
-        )
-        message(countsDir)
-        assert_all_are_dirs(countsDir)
-
-        genomeBuild <- list.dirs(
-            path = countsDir,
-            full.names = FALSE,
-            recursive = FALSE
-        )
-        assert_is_a_string(genomeBuild)
-
-        matrixFile <- file.path(countsDir, genomeBuild, "matrix.mtx")
-        rowFile <- file.path(dirname(matrixFile), "genes.tsv")
-        colFile <- file.path(dirname(matrixFile), "barcodes.tsv")
-        assert_all_are_existing_files(c(matrixFile, rowFile, colFile))
-
-        counts <- readMM(matrixFile)
-
-        # `genes.tsv` is tab delimited
+        # `genes.tsv` is tab delimited.
         rownames <- read_tsv(
-            file = rowFile,
+            file = genesFile,
             col_names = c("geneID", "geneName"),
             col_types = "cc"
-        )
-        assert_has_rows(rownames)
-        rownames <- pull(rownames, "geneID")
+        ) %>%
+            pull("geneID")
 
-        # `barcodes.tsv` is not tab delimited
-        colnames <- read_lines(colFile)
-        # Move the multiplexed sample index number to the beginning for easier
-        # downstream grep matching
-        colnames <- sub("^([ACGT]+)-(\\d+)$", "\\2-\\1", colnames)
+        # `barcodes.tsv` is not tab delimited.
+        # Move the multiplexed sample index number to the beginning for logical
+        # sorting and consistency with bcbio approach.
+        colnames <- read_lines(colFile) %>%
+            sub("^([ACGT]+)-(.+)$", "\\2-\\1", .)
+
+        counts <- readMM(file)
 
         assert_are_identical(length(rownames), nrow(counts))
         assert_are_identical(length(colnames), ncol(counts))
+
         rownames(counts) <- rownames
         colnames(counts) <- colnames
 
