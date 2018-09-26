e5fd6848e124c42d26c5705203dcfe58762e2786
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-09-26 11:30:18 -0400

    Import `makeDimnames()`

--- R/bcbioSingleCell-package.R
+++ R/bcbioSingleCell-package.R
@@ -29,8 +29,8 @@
 #'   basejump_geom_abline basejump_geom_label basejump_geom_label_average
 #'   basejump_geom_label_repel camel cell2sample emptyRanges gene2symbol
 #'   hasRownames import interestingGroups interestingGroups<-
-#'   initializeDirectory makeGRangesFromEnsembl makeGRangesFromGFF makeNames
-#'   makeSingleCellExperiment makeTx2geneFromGFF mapCellsToSamples
+#'   initializeDirectory makeDimnames makeGRangesFromEnsembl makeGRangesFromGFF
+#'   makeNames makeSingleCellExperiment makeTx2geneFromGFF mapCellsToSamples
 #'   matchArgsToDoCall matchInterestingGroups markdownHeader markdownPlotlist
 #'   methodFormals minimalSampleData prepareTemplate printString
 #'   sanitizeSampleData separator standardizeCall stripTranscriptVersions
