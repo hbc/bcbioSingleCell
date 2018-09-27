d3623025673da9d3e270e9ae5e9dd22a1ccc5e9a
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-09-27 08:36:17 -0400

    Simplify `nCount` handling

--- R/barcodes-internal.R
+++ R/barcodes-internal.R
@@ -1,4 +1,4 @@
-#' Bind Cellular Barcodes
+#' Counts per Cellular Barcode
 #'
 #' @author Michael Steinbaugh
 #' @keywords internal
@@ -6,25 +6,26 @@
 #'
 #' @param list `list`. Cellular barcodes per sample.
 #'
-#' @return `data.frame`.
-.bindCellularBarcodes <- function(list) {
+#' @return `integer`.
+.nCount <- function(list) {
     assert_is_list(list)
     assert_has_names(list)
-    mapply(
-        sampleID = names(list),
-        x = list,
-        FUN = function(x, sampleID) {
-            assert_are_identical(colnames(x), c("barcode", "n"))
-            # Add the sampleID as a column
-            x[["sampleID"]] <- as.factor(sampleID)
-            x
-        },
-        USE.NAMES = TRUE,
-        SIMPLIFY = FALSE
-    ) %>%
-        bind_rows() %>%
-        mutate(
-            rowname = paste(!!sym("sampleID"), !!sym("barcode"), sep = "_")
-        ) %>%
-        as("DataFrame")
+    assert_is_integer(list[[1L]])
+    assert_has_names(list[[1L]])
+    if (length(list) > 1L) {
+        # Prefix with `sampleID` when handling multiple samples.
+        mapply(
+            sampleID = names(list),
+            x = list,
+            FUN = function(x, sampleID) {
+                # Prefix the barcode with `sampleID`.
+                names(x) <- paste(sampleID, names(x), sep = "_")
+                x
+            },
+            USE.NAMES = TRUE,
+            SIMPLIFY = TRUE
+        )
+    } else {
+        list[[1L]]
+    }
 }
