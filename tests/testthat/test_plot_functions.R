0400a8b8682fa227a865ba1d593bcfe6ee5bc9d8
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-09-28 11:03:55 -0400

    Parameterize unit tests

--- tests/testthat/test_plot_functions.R
+++ tests/testthat/test_plot_functions.R
@@ -3,44 +3,47 @@ context("Plot Functions")
 
 
 # plotQC =======================================================================
-test_that("plotQC : grid", {
-    # Example dataset doesn't have a cellular barcode cutoff because we removed
-    # the bcbio commands log file (which conflicts with Travis CI)
-    p <- plotQC(indrops_small, return = "grid")
-    expect_is(p, "ggplot")
-})
+with_parameters_test_that(
+    "plotQC", {
+        # Grid
+        # Example dataset doesn't have a cellular barcode cutoff because we
+        # removed the bcbio commands log file (which conflicts with Travis CI).
+        p <- plotQC(object, return = "grid")
+        expect_is(p, "ggplot")
 
-test_that("plotQC : list", {
-    p <- plotQC(indrops_small, return = "list")
-    expect_identical(
-        names(p),
-        c(
-            "Cell Counts",
-            "Reads per Cell",
-            "UMIs per Cell",
-            "Genes per Cell",
-            "UMIs vs. Genes",
-            "Novelty",
-            "Mito Ratio",
-            "Zeros vs. Depth"
+        # List
+        x <- plotQC(object, return = "list")
+        # "Cell Counts" only returns for filtered datasets.
+        # "Reads per Cell" only returns for bcbioSingleCell.
+        names <- c(
+            "genesPerCell",
+            "mitoRatio",
+            "novelty",
+            "umisPerCell",
+            "umisVsGenes",
+            "zerosVsDepth"
         )
-    )
-})
+        expect_true(all(names %in% names(x)))
 
-test_that("plotQC : markdown", {
-    output <- capture.output(plotQC(indrops_small, return = "markdown"))
-    sep <- c("", "", "")
-    expect_identical(
-        head(output, 3L),
-        c("", "", "## Filtered quality control metrics {.tabset}")
+        # Markdown
+        output <- capture.output(plotQC(indrops_small, return = "markdown"))
+        sep <- c("", "", "")
+        expect_identical(
+            head(output, 3L),
+            c("", "", "## Quality control metrics {.tabset}")
+        )
+    },
+    object = list(
+        bcbioSingleCell = indrops_small,
+        CellRanger = cellranger_small
     )
-})
+)
 
 
 
 # plotReadsPerCell =============================================================
 # Example dataset doesn't have a cellular barcode cutoff because we removed the
-# bcbio commands log file (which conflicts with Travis CI)
+# bcbio commands log file (which conflicts with Travis CI).
 test_that("plotReadsPerCell", {
     # Histogram
     x <- plotReadsPerCell(indrops_small, geom = "histogram")
