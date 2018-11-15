6d92d2e9b1f3407bd68a2a9b13e334393f783254
Michael Steinbaugh <michael.steinbaugh@constellationpharma.com>
2018-11-15 15:49:30 -0500

    Update unit tests to only test against bcbioSingleCell

--- tests/testthat/test_bcbioSingleCell.R
+++ tests/testthat/test_bcbioSingleCell.R
@@ -1,8 +1,7 @@
-context("S4 Classes")
+context("S4 class")
 
 
 
-# bcbioSingleCell ==============================================================
 test_that("bcbioSingleCell", {
     uploadDir <- system.file("extdata/indrops", package = "bcbioSingleCell")
 
@@ -24,24 +23,4 @@ test_that("bcbioSingleCell", {
         organism = "Homo sapiens"
     )
     expect_s4_class(x, "bcbioSingleCell")
-})
-
-
-
-# CellRanger ===================================================================
-test_that("CellRanger", {
-    uploadDir = system.file("extdata/cellranger", package = "bcbioSingleCell")
-
-    # Minimal mode, with no metadata or annotations.
-    # This is fast but doesn't slot a lot of useful info.
-    x <- CellRanger(uploadDir = uploadDir)
-    expect_s4_class(x, "CellRanger")
-
-    # Automatic organism annotations from AnnotationHub.
-    x <- CellRanger(
-        uploadDir = uploadDir,
-        organism = "Homo sapiens",
-        ensemblRelease = 87L
-    )
-    expect_s4_class(x, "CellRanger")
 })
--- tests/testthat/test_plots.R
+++ tests/testthat/test_plots.R
@@ -1,45 +1,39 @@
 context("Plots")
 
-data(indrops, cellranger, envir = environment())
+data(indrops, envir = environment())
 
 
 
 # plotQC =======================================================================
-with_parameters_test_that(
-    "plotQC", {
-        # Grid
-        # Example dataset doesn't have a cellular barcode cutoff because we
-        # removed the bcbio commands log file (which conflicts with Travis CI).
-        p <- plotQC(object, return = "grid")
-        expect_s3_class(p, "ggplot")
+test_that("plotQC", {
+    object <- indrops
 
-        # List
-        x <- plotQC(object, return = "list")
-        # "Cell Counts" only returns for filtered datasets.
-        # "Reads per Cell" only returns for bcbioSingleCell.
-        names <- c(
-            "genesPerCell",
-            "mitoRatio",
-            "novelty",
-            "umisPerCell",
-            "umisVsGenes",
-            "zerosVsDepth"
-        )
-        expect_true(all(names %in% names(x)))
+    # Grid. Example dataset doesn't have a cellular barcode cutoff because we
+    # removed the bcbio commands log file (which conflicts with Travis CI).
+    p <- plotQC(object, return = "grid")
+    expect_s3_class(p, "ggplot")
 
-        # Markdown
-        output <- capture.output(plotQC(indrops, return = "markdown"))
-        sep <- c("", "", "")
-        expect_identical(
-            head(output, 3L),
-            c("", "", "## Quality control metrics {.tabset}")
-        )
-    },
-    object = list(
-        bcbioSingleCell = indrops,
-        CellRanger = cellranger
+    # List. "Cell Counts" only returns for filtered datasets. "Reads per Cell"
+    # only returns for bcbioSingleCell.
+    x <- plotQC(object, return = "list")
+    names <- c(
+        "genesPerCell",
+        "mitoRatio",
+        "novelty",
+        "umisPerCell",
+        "umisVsGenes",
+        "zerosVsDepth"
     )
-)
+    expect_true(all(names %in% names(x)))
+
+    # Markdown.
+    output <- capture.output(plotQC(indrops, return = "markdown"))
+    sep <- c("", "", "")
+    expect_identical(
+        head(output, 3L),
+        c("", "", "## Quality control metrics {.tabset}")
+    )
+})
 
 
 
