eaafb9ec7b23ab60a451962236f3d8b59b805acb
Michael Steinbaugh <mike@steinbaugh.com>
2019-04-11 09:20:22 -0400

    Ensure functions necessary for R Markdown QC get reexported

---
+++ R/reexports.R
@@ -0,0 +1,19 @@
+#' @importFrom basejump assignAndSaveData
+#' @export
+basejump::assignAndSaveData
+
+#' @importFrom basejump basenameSansExt
+#' @export
+basejump::basenameSansExt
+
+#' @importFrom basejump export
+#' @export
+basejump::export
+
+#' @importFrom basejump markdownHeader
+#' @export
+basejump::markdownHeader
+
+#' @importFrom basejump SaveData
+#' @export
+basejump::saveData
