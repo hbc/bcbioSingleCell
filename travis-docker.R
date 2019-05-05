5566fa08bd8020d65ec1ea497a82e3a51a1ab673
Michael Steinbaugh <mike@steinbaugh.com>
2019-05-05 14:19:59 -0400

    Update Travis CI configuration

--- travis-docker.R
+++ travis-docker.R
@@ -1,5 +1,20 @@
+#!/usr/bin/env Rscript
+
 Sys.setenv(TZ = "America/New_York")
+options(
+    deparse.max.lines = 3L,
+    error = quote(rlang::entrace()),
+    rlang_backtrace_on_error = "full",
+    showErrorCalls = TRUE,
+    showWarnCalls = TRUE,
+    warn = 1L,
+    warnPartialMatchAttr = TRUE,
+    warnPartialMatchDollar = TRUE,
+    warning.length = 8170L
+)
+
 sessioninfo::session_info()
+
 rcmdcheck::rcmdcheck(
     path = ".",
     args = c(
@@ -16,9 +31,16 @@ rcmdcheck::rcmdcheck(
 )
 
 if (packageVersion("base") >= "3.6") {
+    # Run `BiocCheck::usage()` to get supported flags.
     BiocCheck::BiocCheck(
         package = ".",
-        `quit-with-status` = FALSE
+        `new-package` = TRUE,
+        `no-check-R-ver` = TRUE,
+        `no-check-bioc-help` = TRUE,
+        `no-check-remotes` = TRUE,
+        `no-check-version-num` = TRUE,
+        `no-check-vignettes` = TRUE,
+        `quit-with-status` = TRUE
     )
     lintr::lint_package()
 }
