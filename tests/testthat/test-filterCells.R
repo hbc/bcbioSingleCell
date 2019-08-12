ee7c37dabe832eaa0fc44c07d11bc01003b9ee6c
Michael Steinbaugh <mike@steinbaugh.com>
2019-08-12 15:17:31 -0400

    Simplify unit tests

--- tests/testthat/test-filterCells.R
+++ tests/testthat/test-filterCells.R
@@ -1,2 +1,48 @@
 context("filterCells")
 
+indrops <- calculateMetrics(indrops)
+
+## Expecting an object with the same dimensions by default.
+test_that("No filtering", {
+    x <- filterCells(indrops)
+    expect_s4_class(x, "bcbioSingleCell")
+    expect_identical(dim(x), dim(indrops))
+})
+
+with_parameters_test_that(
+    "Parameterized cutoff tests", {
+        args[["object"]] <- indrops
+        x <- do.call(what = filterCells, args = args)
+        expect_s4_class(x, "bcbioSingleCell")
+        expect_s4_class(metadata(x)[["filterCells"]], "SimpleList")
+        expect_identical(metadata(x)[["subset"]], TRUE)
+        expect_identical(dim(x), dim)
+    },
+    ## Refer to the quality control R Markdown for actual recommended cutoffs.
+    ## These are skewed, and designed to work with our minimal dataset.
+    args = list(
+        list(minCounts = 2000L),
+        list(maxCounts = 2500L),
+        list(minFeatures = 45L),
+        list(maxFeatures = 49L),
+        list(maxMitoRatio = 0.1),
+        list(minNovelty = 0.5),
+        list(minCellsPerFeature = 95L)
+    ),
+    dim = list(
+        c(50L, 35L),
+        c(50L, 88L),
+        c(50L, 95L),
+        c(50L, 81L),
+        c(50L, 22L),
+        c(50L, 81L),
+        c(45L, 100L)
+    )
+)
+
+test_that("Expected cutoff failure", {
+    expect_error(
+        filterCells(indrops, minCounts = Inf),
+        "No cells passed"
+    )
+})
