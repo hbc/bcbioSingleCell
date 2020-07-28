a4f7e1b6a754adfc8137bf3382108c17a4e0aef0
Michael Steinbaugh <mike@steinbaugh.com>
2020-07-27 20:09:41 -0400

    Simplify parameterization

--- tests/testthat/test-plotReadsPerCell.R
+++ tests/testthat/test-plotReadsPerCell.R
@@ -2,12 +2,10 @@ context("plotReadsPerCell")
 
 ## Example dataset doesn't have a cellular barcode cutoff because we removed the
 ## bcbio commands log file (which conflicts with Travis CI).
-
-geom <- eval(formals(`plotReadsPerCell,bcbioSingleCell`)[["geom"]])
-with_parameters_test_that(
-    "geom", {
+test_that("geom", {
+    for (geom in eval(formals(`plotReadsPerCell,bcbioSingleCell`)[["geom"]])) {
         x <- plotReadsPerCell(bcb, geom = geom)
         expect_is(x, "ggplot")
-    },
+    }
     geom = geom
-)
+})
