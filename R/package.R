a617ab45b2ac777d2a431f2b057394d92de59575
Michael Steinbaugh <mike@steinbaugh.com>
2021-02-22 12:20:11 -0500

    Consolidate imports

--- R/package.R
+++ R/package.R
@@ -5,4 +5,42 @@
 #'
 #' @aliases NULL
 #' @keywords internal
+#'
+#' @importClassesFrom SingleCellExperiment SingleCellExperiment
+#'
+#' @importMethodsFrom basejump coerce
+#'
+#' @importFrom AcidPlots acid_geom_abline acid_geom_label
+#'   acid_geom_label_average acid_geom_label_repel
+#' @importFrom BiocParallel bplapply bpmapply
+#' @importFrom IRanges DataFrameList
+#' @importFrom S4Vectors DataFrame SimpleList cbind do.call lapply metadata
+#'   metadata<-
+#' @importFrom SummarizedExperiment assayNames assay assays assays<- colData
+#'   colData<- rowData rowData<- rowRanges rowRanges<-
+#' @importFrom basejump as_tibble calculateMetrics camelCase cell2sample counts
+#'   detectLanes droplevels emptyRanges formalsList import importSampleData
+#'   interestingGroups interestingGroups<- leftJoin makeDimnames
+#'   makeGRangesFromEnsembl makeGRangesFromGFF makeLabel makeNames
+#'   makeSingleCellExperiment mapCellsToSamples markdownPlots
+#'   matchInterestingGroups metrics metricsCols minimalSampleData printString
+#'   realpath sampleData sampleNames separator showSlotInfo standardizeCall
+#' @importFrom bcbioBase getBarcodeCutoffFromCommands getGTFFileFromYAML
+#'   getLevelFromCommands getSampleDataFromYAML getUMITypeFromCommands
+#'   importDataVersions importProgramVersions projectDir runDate sampleDirs
+#' @importFrom cli cat_line cli_alert cli_alert_success cli_alert_warning cli_h1
+#'   cli_h2 cli_text
+#' @importFrom ggplot2 aes facet_wrap geom_boxplot geom_histogram geom_step
+#'   geom_violin ggplot labs scale_x_continuous scale_y_continuous stat_ecdf
+#' @importFrom ggridges geom_density_ridges
+#' @importFrom goalie allAreDirectories allAreFiles areDisjointSets areSetEqual
+#'   assert hasLength hasNames hasRownames hasValidDimnames isADirectory isAFile
+#'   isAURL isAny isCharacter isDirectory isFile isFlag isGGScale isInt isString
+#'   isSubset validate validateClasses
+#' @importFrom graphics hist
+#' @importFrom methods as as<- is new setClass show slot slot<- validObject
+#'   .hasSlot
+#' @importFrom rlang !! sym syms
+#' @importFrom stringr str_extract
+#' @importFrom utils capture.output packageName packageVersion
 "_PACKAGE"
