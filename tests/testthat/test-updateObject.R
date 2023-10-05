db3a968c23cb98a6ff3861bba272a7377cddd779
Michael Steinbaugh <mike@steinbaugh.com>
2023-10-05 09:27:33 -0400

    Use cacheDir

--- tests/testthat/test-updateObject.R
+++ tests/testthat/test-updateObject.R
@@ -4,7 +4,7 @@ test_that("bcbioSingleCell", {
 })
 
 test_that("v0.1 update", {
-    invalid <- import(file.path("cache", "bcbioSingleCell_0.1.0.rds"))
+    invalid <- import(file.path(cacheDir, "bcbioSingleCell_0.1.0.rds"))
     valid <- updateObject(invalid)
     expect_s4_class(valid, "bcbioSingleCell")
 })
